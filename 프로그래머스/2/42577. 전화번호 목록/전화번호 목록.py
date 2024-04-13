8376b32720e48918464a66968d695f467ab1b263
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-13 19:52:23 +0900

    [level 2] Title: 전화번호 목록, Time: 85.94 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,8 @@
 def solution(phone_book):
     phone_book.sort()
+    # 2중 for문 대신 이렇게 비교하기
     for i in range(len(phone_book)-1):
+        # 슬라이싱으로 접두어 부분만 비교하기
         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
     return True
