e1f346cf5dfc192c921fa0bae33e9a22204ed52f
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-13 19:04:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.53 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,4 @@
+import collections
 def solution(participant, completion):
-    pcount = {}
-    
-    for p in participant:
-        # 딕셔너리 만들기
-        if p in pcount:
-            # 이미 있으면
-            pcount[p] +=1 # 1증가
-        else:
-            pcount[p] = 1 # 1넣기
-    
-    for c in completion:
-        if pcount[c] == 1:
-            # 1명인 경우 지우기
-            del pcount[c]
-        else:
-            pcount[c] -=1 # 2명 이상이면 하나 지우기
-    
-    # 남은 키값이 정답
-    answer = list(pcount.keys())[0]
-            
-  
-    return answer
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
