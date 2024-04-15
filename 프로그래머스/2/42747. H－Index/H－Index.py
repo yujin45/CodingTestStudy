884e4e3da8404810624b3ba9f6308711bde96b2c
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-15 20:08:43 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,16 @@
 def solution(citations):
+    answer = 0
     citations.sort(reverse=True)
-    for idx , citation in enumerate(citations):
-        if idx >= citation:
-            return idx
-    return len(citations)
+    
+    for i in range(len(citations)):
+        h = citations[i]
+        count = 0
+        for j in range(len(citations)):
+            if h <= citations[j]:
+                count+=1
+            else:
+                break
+        if h == count:
+            return h
+
+print(solution([2,5,4,4,6, 0]))
