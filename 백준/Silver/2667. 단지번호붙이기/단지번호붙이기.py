b3f5172c25f774a0037e01f270bc41b0d18d10c0
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-19 00:36:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31180 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,32 @@
-# 풀이 참고
 n = int(input())
-
-graph =[]
-
+graph = []
 for i in range(n):
     graph.append(list(map(int, input().strip())))
-    
-home = 0 # 방문할 집 수
-nums = [] # 단지들 당 집 수
 
+home = 0
+nums = []
 def dfs(x, y):
     global home
-    if x < 0 or y<0 or x>=n or y>=n:
-        # 범위 밖이면 false
+    if x <0 or y <0 or x>=n or y>=n:
         return False
-    if graph[x][y] == 1:
-        # 집이 있으면
-        home +=1 # 집 1개 증가
-        graph[x][y] = 0 # 갔으니까 0으로 바꿔주기
+
+    if graph[x][y]==1:
+        home +=1
+        graph[x][y]=0
         dfs(x-1, y)
-        dfs(x, y-1)
         dfs(x+1, y)
-        dfs(x, y+1) 
-        # 상하좌우 재귀로 돌아주기
+        dfs(x, y-1)
+        dfs(x, y+1)
         return True
-    return False # 집 없으면 false
+
+    return False
 
 for i in range(n):
     for j in range(n):
-        if dfs(i, j) == True:
-            # 집이 있음
-            # 재귀 다 돌고 home에 개수 있을 것
+        if dfs(i, j)==True:
             nums.append(home)
-            home = 0 # 초기화
-
-nums.sort() # 오름차순 정렬      
-print(len(nums))     
+            home = 0
+nums.sort()
+print(len(nums))
 for i in nums:
-    print(i)    
+    print(i)
