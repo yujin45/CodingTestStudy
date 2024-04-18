8a18c35b471b26e5a27076304f30656356c80fe0
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-19 00:54:10 +0900

    [Silver II] Title: 유기농 배추, Time: 260 ms, Memory: 31420 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,37 +1,39 @@
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10000000)
+T = int(input())
 
-T = int(input()) # 테스트 케이스 수
 count = 0
-def dfs(x, y):   
+def dfs(x, y):
+    # 여기에서 X는 행, Y는 열
     if x <0 or y<0 or x>=N or y>=M:
         return False
-    if maps[x][y] == 1:
-        # 있으면 재귀 상하좌우
-        maps[x][y] =0
+    if graph[x][y] == 1:
+        # 배추 있으면 지렁이도 있음
+        # home+=1 이거랑 비슷한 문제
+        graph[x][y] = 0 # 체크했으니 0으로
         dfs(x-1, y)
-        dfs(x, y-1)
         dfs(x+1, y)
+        dfs(x, y-1)
         dfs(x, y+1)
         return True
     return False
 
 for _ in range(T):
-    # 가로 길이, 세로 길이, 배추 개수 
     M, N, K = map(int, input().split())
-    # 열, 행
-    
-    maps= [[0]*(M) for _ in range(N)]
-    # N행 M열
+    #열, 행, 배추 위치 개수
+    graph = [[0]*(M) for _ in range(N)]
+
+
     for _ in range(K):
-        X, Y = map(int, input().split())
-        #  X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)
-        maps[Y][X] = 1
-    
+        x, y = map(int, input().split())
+        # X(0 ≤ X ≤ M-1) 가로 열
+        # y 행
+        graph[y][x] = 1 # 배추 심기
+
+    count = 0
     for i in range(N):
         for j in range(M):
-            if dfs(i,j) == True:
-                count +=1
-        
+            if dfs(i, j) == True:
+                # 지렁이 있는 곳
+                count+=1
     print(count)
-    count = 0
