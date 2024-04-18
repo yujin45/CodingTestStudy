5779eed74222e872e08ba4c64f0e767f85ea1834
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-04-18 21:05:31 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,24 +1,20 @@
-# 전날과 완전 똑같으면 안됨
-#  각 종류별로 최대 1가지 의상만 착용
-#  하루에 최소 한 개의 의상은 입습니다.
-
 
 def solution(clothes):
-    hash = {}
-    for c in clothes:
-        if c[1] not in hash:
-        # 키 없으면 만들어주기
-            hash[c[1]] = list()
-        hash[c[1]].append(c[0])
-        
-    #print(hash)
-    # 키포인트
-    # # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수 (N+1)(M+1)
+    closet ={}
+    # 리스트 2가지 요소로 가지고 올 수 있다
+    # 기억해야 할 것
+    # 1. for문 인자값 2개 받아올 수 있다.
+    # 2. [리스트]는 더하기 연산이 가능하다 [a]+[b] = [a, b]
+    for name, kind in clothes:
+        if kind in closet.keys():
+            # closet에 키가 이미 있으면
+            closet[kind]+=[name]
+        else:
+            closet[kind] = [name]
     
-    
+    # A종류가 N개 B 종류가 M개일 때의 경우의 수 = (N+1)(M+1)
     answer = 1
-    for key, value in hash.items():
-        #print(value)
-        answer *= (len(value) + 1)
-        
-    return answer -1
+    for kind in closet.keys():
+        answer *= (len(closet[kind]) +1)
+    # 아무것도 안 입었을 때 빼주기
+    return answer-1
