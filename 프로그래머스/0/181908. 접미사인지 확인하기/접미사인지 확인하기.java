473c6a11e11ec526b9ccc20f48c89d6d99cb1459
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-05-29 17:11:00 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        for(int i = 0; i< my_string.length(); i++){
+            String sub = my_string.substring(i, my_string.length());
+            if(sub.equals(is_suffix)){
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
