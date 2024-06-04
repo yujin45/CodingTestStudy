573d47f9f8696d0ce157a1ce65b4c21968eea24c
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-06-04 16:22:26 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.54 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,41 +1,45 @@
+// 전부 스택으로 구현하기
 import java.util.Stack;
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        Stack<Integer> stack = new Stack<>();
-        int count =0;
-        // board에서 move 위치에 있는 가장 위에 있는 인형 꺼내오기
-        for(int m : moves){
-            int doll = 0;
-        B: for(int i = 0; i<board.length; i++){
-                if(board[i][m-1]!=0){
-                    // 인형 있으면 잡아오기 
-                    doll = board[i][m-1];
-                    // 잡아오고 for문 나와야 했어!!!!! 그리고 뽑은 곳 0으로 만들어줘야 했어!!
-                    board[i][m-1] = 0;
-                    break B;
-                }
-            }
-            // 인형 없으면 doll =0으로 자동 설정 됨
-            // 꺼내온 인형 바구니에 쌓기
-            // 쌓으려는데 맨 위가 똑같은 애면 없애버리고 개수 +2 세기
-            // (아니면 마지막에 pickUp한 수 - 바구니 개수)
-            if(doll!=0){ // 인형 있었으면
-                //System.out.println(stack.toString());
-                //System.out.println("doll: " + doll);
-                if(!stack.isEmpty() && doll == stack.peek()){
-                    // 스택 안 비어 있으면 그리고 peek이 같으면
-                    //System.out.println("doll, peek :" + doll +" " + stack.peek());
-                    stack.pop();
-                    count+=2;
-                }else{
-                    stack.push(doll);
-                }
-                //System.out.println(stack.toString());
-            }
-            
+        // 1. 각 열에 대한 스택을 생성
+        Stack<Integer> [] lanes = new Stack[board.length];
+        for(int i = 0; i<lanes.length; i++){
+            lanes[i] = new Stack<>();// 스택 배열에 생성
         }
-        return count;
+         
+        // 2. board를 역순으로 탐색하며, 각 열의 인형을 lanes에 추가
+        for(int i = board.length -1; i>=0; i--){
+            // 아래 행부터 돌기
+            for(int j = 0; j<board[i].length; j++){
+                if(board[i][j]>0){
+                    // 0 아니면 인형 있는 것
+                    lanes[j].push(board[i][j]);
+                }
+            }
+        }
         
+        // 3. 인형을 담을 bucket 생성
+        Stack<Integer> bucket = new Stack<>();
+        // 4. 사라진 인형의 총 개수 저장 변수 초기화
+        int answer =  0;
+        
+        // 5. moves 순회하며, 각 열에서 인형 뽑아 bucket에 추가
+        for(int move : moves){
+            if(!lanes[move-1].isEmpty()){
+                // 해당 열 인형 있는 경우
+                int doll = lanes[move-1].pop(); // 꺼내기
+                // 6. 바구니에 인형이 있고, 가장 위에 있는 인형과 같은 경우
+                if(!bucket.isEmpty() && bucket.peek() == doll){
+                    bucket.pop(); // 바구니 인형 빼고
+                    answer +=2; //2개 증가
+                }else{
+                    // 7. 바구니 안에 인형 없거나, 가장 위 인형과 다른 경우
+                    bucket.push(doll); // 인형 넣기
+                }
+            }
+        }
+        return answer;
         
     }
 }
