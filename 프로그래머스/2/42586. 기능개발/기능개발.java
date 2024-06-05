60ff5df98df089c36144ae044d8872fe3a550af7
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-06-05 15:24:13 +0900

    [level 2] Title: 기능개발, Time: 2.00 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,29 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> countList = new ArrayList<>(); // 배포 개수
-        int days = 0;
+        ArrayList<Integer> countList = new ArrayList<>();
+        int days=0;
+        int index=0; 
         int count = 0;
-        int index = 0;
-        
-        while(index < progresses.length) {
-            // 새로운 days 구하기
-            if(((100 - progresses[index]) % speeds[index]) != 0) {
-                // 나누어 떨어지지 않으면 
-                days = ((100 - progresses[index]) / speeds[index]) + 1; 
-            } else {
-                days = ((100 - progresses[index]) / speeds[index]);
+        // 인덱스로 확인, 마지막 항목까지 검사
+        while(index < progresses.length){
+            // days 설정하기
+            if((100 - progresses[index]) % speeds[index] !=0){
+                // 나머지가 생기면
+                days = (100 - progresses[index]) /speeds[index] +1;
+            }else{
+                days = (100 - progresses[index]) /speeds[index];
             }
-            
-            // 같은 기간에 배포할 작업 개수 세기
+            // days 확인하기 
             count = 0;
-            while(index < progresses.length && (progresses[index] + speeds[index] * days) >= 100) {
-                count++;
+            while((index < progresses.length) && ( (progresses[index] + speeds[index]*days)>=100)){
+                // 작업 완료될 경우
                 index++;
+                count++;
             }
             countList.add(count);
+            
         }
-        
         return countList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
