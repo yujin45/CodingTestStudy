d33b2f8f7a7b804520e01bca78ba16ca8004b75c
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-06-13 17:08:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,3 +1,12 @@
 import math
-def solution(n):
+def solution1(n):
+    # math의 제곱근 구해서 보는 방법
     return 1 if math.sqrt(n).is_integer() else 2
+
+def solution2(n):
+    # 0.5승 해서 보는 방법
+    return 1 if (n ** 0.5).is_integer() else 2
+
+def solution(n):
+    # 정수 제곱근만 반환하는 math.isqrt 써서 제곱하면 원래수 나오는지 확인
+    return 1 if math.isqrt(n)**2==n else 2
