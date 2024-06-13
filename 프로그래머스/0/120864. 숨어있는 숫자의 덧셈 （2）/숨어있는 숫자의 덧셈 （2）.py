ba3872d84a380807b7c047e86c60b872677826d9
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-06-13 20:14:27 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    # 문자 하나씩 체크가 아니라 숫자 연속으로 나오면 연속을 하나로 봄
+    #return sum([int(k) for k in my_string if k.isdigit()])
+    num_list = []
+    num = '0'
+    for m in my_string:
+        if m.isdigit():
+            num+=m
+        else:
+            num_list.append(int(num))
+            num = '0'
+    num_list.append(int(num))
+    return sum(num_list)
