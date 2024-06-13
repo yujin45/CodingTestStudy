ee549d7e4e3bfece1eca036b12d80bf26b6e804d
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-06-13 23:24:41 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,4 +1,16 @@
 def solution(id_pw, db):
+    database = dict(db)
+    pw = database.setdefault(id_pw[0], "fail")
+    if pw =="fail":
+        return "fail"
+    elif pw == id_pw[1]:
+        return "login"
+    else:
+        return "wrong pw"
+    
+    return "fail"
+
+def solution2(id_pw, db):
     answer = ''
     for id_, pw in db:
         if id_ == id_pw[0]:
