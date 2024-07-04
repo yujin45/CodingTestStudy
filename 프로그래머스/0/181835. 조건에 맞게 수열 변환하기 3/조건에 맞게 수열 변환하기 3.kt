5361aff3bf176502c9092e469561fb5fe7b819c8
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-04 21:14:59 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 18.39 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(arr: IntArray, k: Int): IntArray {
+        return if(k % 2 ==1 ) arr.map{it * k }.toIntArray()
+        else arr.map{it +k }.toIntArray()
+    }
+}
