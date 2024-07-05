0555451615884bf01c976d8957f33152ecb82a6c
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 16:13:37 +0900

    [level 0] Title: ad 제거하기, Time: 7.35 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
@@ -1,5 +1,7 @@
 class Solution {
     fun solution(strArr: Array<String>): Array<String> {
+        return strArr.filterNot { it.contains("ad") }.toTypedArray()
+        return strArr.filter { !it.contains("ad") }.toTypedArray()
         // 내 코드 : 순회하며 확인
         var answer = mutableListOf<String>()
         for(s in strArr){
