41c55b42a0c6370ff5055e29b8cf702e908b90c6
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 15:14:46 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 26.82 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.kt
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.kt
@@ -1,5 +1,10 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
+        // take함수
+        return num_list.sorted().take(5).toIntArray()
+	      // 4까지 포함
+		    return num_list.sorted().slice(0 .. 4).toIntArray()
+			   // 5는 미포함
         return num_list.sorted().slice(0 until 5).toIntArray()
     }
 }
