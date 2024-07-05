18eaa5f1c189541eacef6c37cd9d16a7ff37d981
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 21:04:39 +0900

    [level 0] Title: n개 간격의 원소들, Time: 5.82 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.kt
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.kt
@@ -1,5 +1,8 @@
 class Solution {
     fun solution(num_list: IntArray, n: Int): IntArray {
+        // 다른 사람 풀이
+        return num_list.filterIndexed { index, _ -> index % n == 0 }.toIntArray()
+        // 내 풀이
         return num_list.toList().chunked(n).map{it.first()}.toIntArray()
     }
 }
