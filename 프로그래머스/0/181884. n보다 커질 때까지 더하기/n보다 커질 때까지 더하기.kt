30ceec9e213cd4d2ef5259fe13e121ceac62b11d
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 20:49:10 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -1,5 +1,8 @@
 class Solution {
     fun solution(numbers: IntArray, n: Int): Int {
+        // 다른 사람 코드: 누적합 이용 n 넘지 않을 때까지 더하기
+        return numbers.fold(0) { acc, i -> if (n >= acc) acc + i else acc }
+        
         var answer: Int = 0
         for(num in numbers){
             answer += num
