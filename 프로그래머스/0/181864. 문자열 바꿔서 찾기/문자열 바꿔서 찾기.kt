a6a45e0084e8905004823f2c0cbdc679fbe8e02d
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 15:52:51 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 13.21 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.kt
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.kt
@@ -1,5 +1,16 @@
 class Solution {
     fun solution(myString: String, pat: String): Int {
+        // map, joinToString 사용
+        val convertedString = myString.map{
+            when(it){
+                'A' -> 'B'
+                'B' -> 'A'
+                else -> it
+            }
+        }.joinToString("")
+        return if(pat in convertedString) 1 else 0
+        
+        // 내 코드
         // 하나씩 바꿔주기
         var reverse : String = ""
         for(m in myString){
