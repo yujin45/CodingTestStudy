07b0bd0452c4bc90457bf37928a71b0f4935c82e
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-07-05 14:47:25 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 19.12 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.kt
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.kt
@@ -1,5 +1,8 @@
 class Solution {
     fun solution(arr: IntArray, delete_list: IntArray): IntArray {
+        // filter 이용 방법
+        return arr.filter{it !in delete_list}.toIntArray()
+        // for루프 이용한 방법
         val result = mutableListOf<Int>()
         for (num in arr){
             if(num !in delete_list){
