c28fd4979f36b04ad8e7fe10eee755b95dfb6384
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-01 12:40:06 +0900

    [level 1] Title: 평균 구하기, Time: 20.84 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(arr: IntArray): Double {
+        return arr.average()
+    }
+}
+
+class Solution1 {
+    fun solution(arr: IntArray): Double {
+        return arr.sum().toDouble()/arr.size
+    }
+}
