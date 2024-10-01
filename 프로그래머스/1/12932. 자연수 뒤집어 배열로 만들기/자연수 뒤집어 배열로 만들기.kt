24d086cacd30cd89fcbd84f8b7dfe739b4b5f713
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-01 13:19:06 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 10.15 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -1,9 +1,17 @@
 class Solution {
     fun solution(n: Long): IntArray {
-        
         return n.toString()
-            .reversed()
-            .map{it.toString().toInt()}
-            .toIntArray()
+            .map { it.toString().toInt() }  // n을 문자열로 변환한 후, 각 문자를 Int로 변환하여 리스트로 만듦
+            .reversed()                      // 리스트를 역순으로 뒤집음
+            .toIntArray()                    // 리스트를 IntArray로 변환하여 반환
+    }
+}
+
+class Solution1 {
+    fun solution(n: Long): IntArray {
+        return n.toString()
+            .reversed()                  // 문자열을 뒤집음
+            .map { it.toString().toInt() } // 각 문자를 Int로 변환
+            .toIntArray()                 // List를 IntArray로 변환
     }
 }
