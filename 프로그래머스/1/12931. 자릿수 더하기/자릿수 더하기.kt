7d29b24fa3e922457322af4e2115743f25b88562
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-01 13:11:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,6 +1,20 @@
-class Solution {
+class Solution1 {
     fun solution(n: Int): Int {
+    
+        return n.toString().map{ it.toString().toInt()}.sum()
+    }
+}
+
+class Solution {
+    fun solution(N: Int): Int {
+        var sum = 0
+        var number = N
         
-        return n.toString().map{ char -> char.toString().toInt()}.sum()
+        while (number > 0) {
+            sum += number % 10  // 마지막 자릿수를 더함
+            number /= 10        // 마지막 자릿수를 제거
+        }
+        
+        return sum
     }
 }
