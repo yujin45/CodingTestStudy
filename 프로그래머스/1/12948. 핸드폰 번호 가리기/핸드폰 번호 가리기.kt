1fa6527897e954b05097c562c2977d80e95a04b2
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-01 15:33:29 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 22.65 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,8 +1,16 @@
-class Solution {
+class Solution1 {
     fun solution(phone_number: String): String {
         val starLen =  phone_number.length - 4
         val star = "*".repeat(starLen)
         
         return star + phone_number.substring(starLen, phone_number.length) 
+    }
+}
+
+class Solution {
+    fun solution(phone_number: String): String {
+        val hidden = "*".repeat(phone_number.length - 4)  // 앞 부분은 *로 대체
+        val lastFour = phone_number.takeLast(4)  // 마지막 4자리만 가져옴
+        return hidden + lastFour
     }
 }
