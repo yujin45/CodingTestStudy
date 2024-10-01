296efcfd495e2d40c6516f509bce0cfbefdf9329
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-01 15:04:52 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 27.50 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,5 +1,12 @@
 class Solution {
     fun solution(numbers: IntArray): Int {
+        return (0..9).filter{it !in numbers}.sum()
+    }
+}
+
+
+class Solution1 {
+    fun solution(numbers: IntArray): Int {
         var sum: Int = 0
         val range = (0..9)
         range.map{it -> if(it !in numbers) sum += it}
