cf4c7f65613ec95bae1ac154a0b3e4150b3b6ad3
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-10-09 22:48:51 +0900

    [level 2] Title: 피로도, Time: 68.13 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,32 +1,16 @@
-# 피로도 0 이상의 정수
-# "최소 필요 피로도">= 항상 "소모 피로도"
-# 서로 다른 던전의 ["최소 필요 피로도", "소모 피로도"]가 서로 같을 수 있습니다. => 중복
-# 순서
+from itertools import permutations
 
-import itertools
 def solution(k, dungeons):
-    answer = -1
-    
-    d_list = list(itertools.permutations(dungeons, len(dungeons)))
-    #print(d_list)
-    maxe = 0
-    count =0
-    ori_k = k
-    for d in d_list:
-        #print("d", d)
-        count =0
-        k = ori_k
-        for i in range(len(d)):
-            #print("d[i][0] >= k:", d[i][0] , k)
-            if d[i][0] <= k:
-                #print()
-                # 동굴 입장 조건
+    perm = list(permutations(dungeons, len(dungeons)))
+    max_count =0
+    for duns in perm:
+        count = 0
+        now = k
+        for need, minus in duns:
+            if now >= need:
+                now -= minus
                 count+=1
-                k -=d[i][1]
-        if count > maxe:
-            maxe = count
-    answer = maxe
-    return answer
-
-#a=solution(50, [[40, 10], [30, 20], [30,20]])
-#print(a)
+            else:
+                break
+        max_count = max(count, max_count)
+    return max_count 
