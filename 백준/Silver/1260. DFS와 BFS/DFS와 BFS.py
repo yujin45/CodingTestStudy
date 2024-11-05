d345ba2a176feb85369f2cda597632326b59addb
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-11-06 07:34:24 +0900

    [Silver II] Title: DFS와 BFS, Time: 408 ms, Memory: 39536 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,54 @@
+from collections import deque
+
+# 정점 개수: N
+# 간선 개수: M
+# 탐색 시작 정점: V번 (1부터 시작)
 N, M, V = map(int, input().split())
 
+# 번호 탐색 위해 0 포함해서 N+1
+visited_bfs = [False]*(N+1)
+visited_dfs = [False]*(N+1)
+# 인접 행렬로 구현
 graph = [[0]*(N+1) for _ in range(N+1)]
 
-visited1 = [0]*(N+1)
-visited2 = visited1.copy()
+
+def bfs(graph, visited, start_v):
+    result = []
+    queue = deque([start_v])
+    visited[start_v]= True
+    while queue:
+        v = queue.popleft()
+        result.append(v)
+        for i in range(N+1):
+            if graph[v][i] == 1 and not visited[i]:
+                queue.append(i) # 연결 노드 추가
+                visited[i]= True
+                
+    return result    
+        
+dfs_ret =[]        
+def dfs(graph, visited, v):
+    visited[v] = True
+    dfs_ret.append(v)
+    for i in range(N+1):
+        if graph[v][i] == 1 and not visited[i]:
+            dfs(graph, visited, i)
+
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
-    # 양방향 노드 이어주기
+    # 두 정점의 번호, 여러 간선 o, 양방향
+    v1, v2 = map(int, input().split())
+    graph[v1][v2] = 1
+    graph[v2][v1] = 1
 
-def dfs(V):
-    visited1[V] = 1 # 방문처리
-    print(V, end = ' ')
-    for i in range(1, N+1):
-        if graph[V][i] ==1 and visited1[i] ==0:
-            #방문x인 곳 자식 노드 타고 들어가기
-            dfs(i)
-            
-            
-def bfs(V):
-    queue = [V] # 큐에 넣기
-    visited2[V] = 1 # 방문 표시
-    while queue:
-        #큐에 있으면 일단 pip
-        V = queue.pop(0)
-        print(V, end = ' ')
-        for i in range(1, N+1):
-            if graph[V][i] ==1 and visited2[i] ==0:
-                # 큐에 넣기
-                queue.append(i)
-                visited2[i] = 1
-                
-            
-dfs(V)
-print()
-bfs(V)
-                               
+dfs(graph, visited_dfs, V)
+#print(dfs_ret)
+#print(bfs(graph, visited_bfs, 1))
+bfs_ret = bfs(graph, visited_bfs, V)
 
-  
+print(' '.join(map(str, dfs_ret)))
+print(' '.join(map(str, bfs_ret)))
 
-    
+
+
+
