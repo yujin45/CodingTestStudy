3c7a496ca320517fc1e1eacf26f883e6df2e8343
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-18 16:58:40 +0900

    [level 0] Title: 문자열 출력하기, Time: 205.55 ms, Memory: 47.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+    bw.write(br.readLine())
+    
+    br.close()
+    bw.close()
+}
