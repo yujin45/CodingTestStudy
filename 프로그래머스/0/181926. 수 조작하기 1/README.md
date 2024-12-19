66b4f465bacd3ba907d001b3284088a3ec6239f0
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-19 15:14:36 +0900

    [level 0] Title: 수 조작하기 1, Time: 20.57 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
 
 ### 성능 요약
 
-메모리: 62.1 MB, 시간: 4.39 ms
+메모리: 62.5 MB, 시간: 20.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 19일 15:12:17
+2024년 12월 19일 15:14:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -1,5 +1,15 @@
 class Solution {
     fun solution(n: Int, control: String): Int {
+        val adjustments = mapOf('w' to 1, 's' to -1, 'd' to 10, 'a' to -10)
+        
+        return control.fold(n){acc, c ->
+            acc + (adjustments[c] ?: 0)
+        }
+    }
+}
+
+class Solution2 {
+    fun solution(n: Int, control: String): Int {
         var n = n
         for(c in control){
             when(c){
