263066874888186ddea79e9f56cae2f69d260f59
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-19 15:40:19 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.09 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
         for((i, j) in queries){
             val temp = arr[i]
@@ -6,6 +6,17 @@ class Solution {
             arr[j] = temp
         }
         
+        return arr
+    }
+}
+
+class Solution {
+    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
+        queries.forEach { (i, j) ->
+            val temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
         return arr
     }
 }
