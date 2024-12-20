31344b83b216b2e2f8b800dd71133b549a2e278f
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-20 16:44:22 +0900

    [level 0] Title: 배열 만들기 1, Time: 30.77 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.kt
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -1,15 +1,5 @@
 class Solution {
     fun solution(n: Int, k: Int): IntArray {
-        // 다른 사람 코드
-        return (k..n step k).toList().toIntArray()
-        return (1..n).filter{ it % k == 0 }.toIntArray()
-        // 내 코드
-        var answer = mutableListOf<Int>()
-        for(num in 1 .. n){
-            if(num %k == 0){
-                answer.add(num)
-            }
-        }
-        return answer.toIntArray()
+        return (1..n).filter{ it % k == 0}.toIntArray()
     }
 }
