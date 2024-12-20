e18c98ca7c7b39dba0c1549e29d2dcbb1d9a09b6
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-20 17:37:47 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
@@ -1,11 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-         return num_list.indexOfFirst { it < 0 }
-        
         for(i in num_list.indices){
-            if(num_list[i] < 0){
-                return i
-            }
+            if(num_list[i]<0) return i
         }
         return -1
     }
