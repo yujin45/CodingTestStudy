4114e2eec8f2f8fbbadd602f897857628a5b3f46
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-20 21:23:49 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
-        // 마치치 못한 일들만 담기
-        return todo_list.filterIndexed{ index, _ -> !finished[index]}.toTypedArray()
-        
+        return todo_list.filterIndexed{i, v -> finished[i].not()}.toTypedArray()
     }
 }
