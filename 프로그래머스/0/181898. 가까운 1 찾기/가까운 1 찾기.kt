0563dea54a87987165b0f8cfddfc4cefff8db609
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-20 17:11:07 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(arr: IntArray, idx: Int): Int {
+        for(i in idx until arr.size){
+            if(arr[i] == 1) return i
+        }
+        return -1
+    }
+}
