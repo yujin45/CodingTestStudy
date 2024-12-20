4aecfad8e78e0d9a6e6a5f70fbb8996e34abc02e
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-20 15:32:42 +0900

    [level 0] Title: 배열 만들기 5, Time: 23.52 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.kt
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.kt
@@ -1,5 +1,10 @@
 class Solution {
     fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
+        return intStrs.map{it.substring(s, s+l).toInt()}.filter{it > k}.toIntArray()
+    }
+}
+class Solution2 {
+    fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
         return intStrs.mapNotNull{
             val num = it.substring(s, s+l).toInt()
             if(num > k ) num else null
