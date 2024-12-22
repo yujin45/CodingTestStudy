0ad8113371777fa669b612e1b31179cc9f615d06
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 08:47:58 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 17.94 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
@@ -1,8 +1,5 @@
 class Solution {
     fun solution(myString: String, pat: String): Int {
-        // 대소문자를 구분하지 않음이 포인트!
-        var lowerMyString = myString.lowercase()
-        var lowerPat = pat.lowercase()
-        return if(lowerPat in lowerMyString) 1 else 0
+        return if(myString.contains(pat, true)) 1 else 0
     }
 }
