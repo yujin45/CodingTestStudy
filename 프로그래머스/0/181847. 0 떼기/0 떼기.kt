387c88529af3b309ac5bda2202803bae22ce1aad
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 21:03:43 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.kt
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.kt
@@ -1,5 +1,11 @@
 class Solution {
     fun solution(n_str: String): String {
+        return n_str
+            .dropWhile { it == '0' }
+    }
+}
+class Solution1 {
+    fun solution(n_str: String): String {
         if(n_str[0]!='0') return n_str
         var flag = true
         val sb = StringBuilder()
@@ -12,3 +18,30 @@ class Solution {
         return sb.toString()
     }
 }
+
+/*
+테스트 1 〉	통과 (0.04ms, 64.7MB)
+테스트 2 〉	통과 (0.01ms, 62.9MB)
+테스트 3 〉	통과 (0.05ms, 62.1MB)
+테스트 4 〉	통과 (0.05ms, 61.4MB)
+테스트 5 〉	통과 (0.01ms, 63.1MB)
+테스트 6 〉	통과 (0.02ms, 62.6MB)
+테스트 7 〉	통과 (0.06ms, 62.3MB)
+테스트 8 〉	통과 (0.03ms, 62.1MB)
+테스트 9 〉	통과 (0.03ms, 63.6MB)
+테스트 10 〉	통과 (0.01ms, 63.5MB)
+*/
+class Solution2 {
+    fun solution(n_str: String): String {
+        return n_str.trimStart('0')
+    }
+}
+/*
+테스트 1 〉	통과 (24.04ms, 66.2MB)
+테스트 2 〉	통과 (20.89ms, 66.1MB)
+테스트 3 〉	통과 (21.81ms, 65MB)
+테스트 4 〉	통과 (20.45ms, 67.9MB)
+테스트 5 〉	통과 (20.25ms, 66MB)
+테스트 6 〉	통과 (20.31ms, 66.4MB)
+테스트 7 〉	통과 (22.24ms, 65.6MB)
+*/
