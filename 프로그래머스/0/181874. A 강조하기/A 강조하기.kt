c07a115ac270e9694c00f0c73e25763f07a2f919
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 10:29:03 +0900

    [level 0] Title: A 강조하기, Time: 12.42 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
@@ -1,8 +1,5 @@
 class Solution {
     fun solution(myString: String): String {
-        // a만 대문자로 아닌 것은 소문자로 
-        // 1 전체를 소문자로 변환
-        // 2 a만 대문자로 변환
         return myString.lowercase().replace("a", "A")
     }
 }
