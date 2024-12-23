76f65478af7713dfd305b6a2308adea4b6218cf9
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 21:04:41 +0900

    [level 0] Title: 0 떼기, Time: 0.06 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.kt
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.kt
@@ -1,9 +1,27 @@
 class Solution {
+    fun solution(nStr: String) = nStr.toInt().toString()
+}
+
+class Solution0 {
     fun solution(n_str: String): String {
         return n_str
             .dropWhile { it == '0' }
     }
 }
+
+/*
+테스트 1 〉	통과 (0.02ms, 63.4MB)
+테스트 2 〉	통과 (0.02ms, 62.2MB)
+테스트 3 〉	통과 (0.02ms, 64.9MB)
+테스트 4 〉	통과 (0.02ms, 64.1MB)
+테스트 5 〉	통과 (0.02ms, 63.8MB)
+테스트 6 〉	통과 (0.03ms, 63.5MB)
+테스트 7 〉	통과 (0.02ms, 65.1MB)
+테스트 8 〉	통과 (0.02ms, 61.2MB)
+테스트 9 〉	통과 (0.03ms, 62.7MB)
+테스트 10 〉	통과 (0.03ms, 61.8MB)
+*/
+
 class Solution1 {
     fun solution(n_str: String): String {
         if(n_str[0]!='0') return n_str
