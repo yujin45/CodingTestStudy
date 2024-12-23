15311a98d228f54f57256c45ec22ad5793d4c7b2
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 10:22:36 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.09 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.kt
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.kt
@@ -1,32 +1,7 @@
 class Solution {
     fun solution(strArr: Array<String>): Array<String> {
-        // gpt 추천 풀이
-        return strArr.mapIndexed{ index, str ->
-            if(index %2 ==0 ){
-                str.lowercase()
-            }else{
-                str.uppercase()
-            }
-        }.toTypedArray()
-        
-        // 다른 사람 풀이, 인덱스를 map으로 처리
-        return strArr.indices
-                    .map{ if(it%2==0){
-                        strArr[it].lowercase()
-                    }else{
-                        strArr[it].uppercase()
-                    }}.toTypedArray()
-        
-        // 내 풀이
-        var result = mutableListOf<String>()
-        for(i in strArr.indices){
-            if(i%2==0){
-                // 짝수는 소문자
-                result.add(strArr[i].lowercase())
-            }else{
-                result.add(strArr[i].uppercase())
-            }
-        }
-        return result.toTypedArray()
+        strArr.forEachIndexed{i, v ->
+        strArr[i] = if (i % 2 == 0) v.lowercase() else v.uppercase()}
+        return strArr
     }
 }
