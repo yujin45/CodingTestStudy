30cf50980fa205cecf8157b48e30f4cb27f2035a
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-23 14:01:13 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 48.81 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.kt
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.kt
@@ -1,6 +1,9 @@
 class Solution {
     fun solution(myString: String): Array<String> {
         var answer: Array<String> = arrayOf<String>()
-        return myString.split("x").filterNot{it==""}.sorted().toTypedArray()
+        return myString.split("x")
+            .filter{it.isNotEmpty()}
+            .sorted()
+            .toTypedArray()
     }
 }
