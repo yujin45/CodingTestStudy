cd460992f64a6bcb4d01f6ffcae34b6f9efd3ab1
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-26 23:42:01 +0900

    [Silver V] Title: 숫자 카드, Time: 420 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
 N = int(input())
 cards = set(map(int, input().split())) # 상근이 카드
 M = int(input())
@@ -5,8 +8,8 @@ check_cards = list(map(int, input().split())) # 체크해야 하는 것
 
 for i in range(M):
     if check_cards[i] in cards:
-        print(1, end=" ")
+        write("1 ")
     else:
-        print(0, end=" ")
+        write("0 ")
     
                    
