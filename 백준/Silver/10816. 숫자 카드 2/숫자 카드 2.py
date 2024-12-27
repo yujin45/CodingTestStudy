6abeeda030e0cb076ff31d7cddd591caba98e461
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-28 00:24:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 560 ms, Memory: 146432 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,15 +1,13 @@
 import sys
 from collections import Counter
 input = sys.stdin.readline
+write = sys.stdout.write
 
-# 입력 처리
-_ = input()  # 첫 번째 입력 (N), 필요 없으므로 무시
+_ = input()
 having = map(int, input().split())
 M = int(input().strip())
 check_cards = map(int, input().split())
 
-# 카운터 생성
 having_counter = Counter(having)
 
-# 결과 출력 (join 대신 직접 출력으로 메모리 절약)
-print(' '.join(str(having_counter[c]) for c in check_cards))
+write(' '.join(str(having_counter[c]) for c in check_cards))
