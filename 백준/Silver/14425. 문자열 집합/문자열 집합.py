6e3454725d745fbcfbb1ea4d8fb59dd3ea5be67d
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-27 15:58:00 +0900

    [Silver IV] Title: 문자열 집합, Time: 88 ms, Memory: 37672 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
+
+N, M = map(int, input().split())
+textList = set([input() for _ in range(N)])
+cnt = 0
+
+for i in range(M):
+    subText = input()
+    if subText in textList:
+        cnt+=1
+write(str(cnt)+"\n")
