ed81f066507f474f756c97d6bdca165ebed2d976
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-27 12:47:42 +0900

    [Silver V] Title: 숫자 카드, Time: 380 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 write = sys.stdout.write
 
-N = int(input())
-cards = set(map(int, input().split()))
-M = int(input())
-check_cards = list(map(int, input().split()))
+N = input()
+cards = set(map(int, input().split(" ")))
+M = input()
+check_cards = list(map(int, input().split(" ")))
 
 result = ['1' if card in cards else '0' for card in check_cards]
 write(' '.join(result) + '\n')
