7ff230f652d732b8639addb5b3a4ed4d9b4230d8
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-28 22:40:06 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 468 ms, Memory: 293268 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
@@ -1,16 +1,17 @@
 fun main() {
-    val br = System.`in`.bufferedReader()
-    val bw = System.out.bufferedWriter()
+    val str = readln() // 입력받은 문자열
+    val strSet = HashSet<String>() // 부분 문자열을 저장할 HashSet
+    var count = 0 // 부분 문자열의 개수
 
-    val inp = br.readLine()
-    val arr = mutableListOf<String>()
+    // step = substring 시 잘라낼 문자열의 크기
+    (1..str.length).forEach { step ->
+        // substring 시 인덱스를 초과하지 않게끔 length - step까지만 반복문을 돌린다.
+        for (index in 0..str.length - step)
+            strSet.add(str.substring(index, index + step))
 
-    for (start in 0..inp.length) {
-        for (end in start + 1..inp.length) {
-            arr.add(inp.substring(start, end))
-        }
+        count += strSet.size
+        strSet.clear()
     }
-    bw.write(arr.distinct().size.toString())
-    bw.flush()
-    bw.close()
+
+    println(count)
 }
