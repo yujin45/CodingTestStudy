5064f1781213e7c799a37663f11dfba46eff4325
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-28 22:13:23 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 828 ms, Memory: 564440 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
@@ -1,19 +1,16 @@
-
 fun main() {
     val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
+    val bw = System.out.bufferedWriter()
 
-    val s = br.readLine()
-    val n = s.length
-    val hashSet = hashSetOf<String>()
-    for(i in 0 until n){
-        for(j in (i+1) until (n+1)){
-            hashSet.add(s.substring(i, j))
+    val inp = br.readLine()
+    val arr = mutableListOf<String>()
+
+    for (start in 0..inp.length) {
+        for (end in start + 1..inp.length) {
+            arr.add(inp.substring(start, end))
         }
     }
-    bw.write("${hashSet.size}")
+    bw.write(arr.distinct().size.toString())
     bw.flush()
-    br.close()
     bw.close()
-
 }
