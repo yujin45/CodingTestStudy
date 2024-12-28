8ea7d73dc95f38a1b2adf957a1e622fdbc5f51ac
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-28 21:37:07 +0900

    [Silver IV] Title: 대칭 차집합, Time: 676 ms, Memory: 98996 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.kt
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.kt
@@ -1,4 +1,3 @@
-
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.`out`.bufferedWriter()
@@ -7,8 +6,14 @@ fun main() {
     val aSet = HashSet<Int>(br.readLine().split(" ").map { it.toInt() })
     val bSet = HashSet<Int>(br.readLine().split(" ").map { it.toInt() })
 
-    val c = aSet.union(bSet) - aSet.intersect(bSet)
-    bw.write("${c.size}")
+    var count = 0
+    for(element in aSet){
+        if(element !in bSet) count++
+    }
+    for(element in bSet){
+        if(element !in aSet) count++
+    }
+    bw.write("$count")
     bw.flush()
     br.close()
     bw.close()
