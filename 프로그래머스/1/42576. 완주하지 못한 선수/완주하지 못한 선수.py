fed6c9c7dfc2282773aa7482ad917eb8715e8482
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-28 23:44:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.93 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,4 @@
-import collections
+from collections import Counter
+
 def solution(participant, completion):
-    answer = ''
-    # participant -1 = completion
-    # 동명이인 있을 수 있음
-    
-    participant_dict = collections.Counter(participant)
-    #print(participant_dict)
-    for c in completion:
-        participant_dict[c]-=1
-    for k in participant_dict.keys():
-        if participant_dict[k] == 1:
-            answer = k
-    
-    return answer
+    return next(iter((Counter(participant) - Counter(completion))))
