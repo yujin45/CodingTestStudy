cd6ac83543ff914d9c402c36e65f6de9dc66b6a4
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-30 19:33:06 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,33 +1,24 @@
-
 def solution(genres, plays):
-    answer = []
-    #1.  속한 노래가 많이 재생된 장르를 먼저 수록합니다
-    ## 속한 노래 재생 횟수 합해서 장르 정렬
-    hash = {}
-    hash_g = {}
-    for i in range(len(plays)):
-        if genres[i] in hash:
-            hash[genres[i]] += plays[i]
-            hash_g[genres[i]] +=[(plays[i], i)]
-        else:
-            hash[genres[i]] =  plays[i]
-            hash_g[genres[i]] =[(plays[i], i)]
+    music = {}
+    gens = {}
+    for i in range(len(genres)):
+        gen = genres[i]
+        if gen not in music:
+            music[gen] = []
         
-    #print(hash)
-    # 딕셔너리를 내림차순으로 정렬하여 튜플 리스트로 변환
-    sorted_data = sorted(hash.items(), key=lambda x: x[1], reverse=True)
-    #print(sorted_data)
-    # 정렬된 결과에서 키만 추출하여 리스트로 변환
-    sorted_keys = [item[0] for item in sorted_data]
-    #print(sorted_keys)
-    #print(hash_g[sorted_keys[0]])
-    #장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-    for k in sorted_keys:
-        # 내림, 올림
-        sorted_data = sorted(hash_g[k], key=lambda x: (-x[0], x[1]))
-        #print(sorted_data)
-        
-        answer+= [item[1] for item in sorted_data][:2]
+        music[gen].append([plays[i], i])
+        gens[gen] = gens.setdefault(gen, 0) + plays[i]
 
-    
+    gen_list = [(k, v) for k, v in gens.items()]
+    gen_list.sort(key = lambda x : -x[1],)
+
+    answer = []
+    for k, _ in gen_list:
+        check = music[k]
+        check.sort(key = lambda x : (-x[0], x[1]))
+        check=[v for _, v in check]
+        if len(check) >=2:
+            answer+= check[:2]
+        else:
+            answer += check[:1]
     return answer
