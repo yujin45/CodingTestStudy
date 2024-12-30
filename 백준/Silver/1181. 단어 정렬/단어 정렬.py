b156a6bf5b5eb5c9dec53633fd2899b5435e04c9
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-30 21:41:18 +0900

    [Silver V] Title: 단어 정렬, Time: 72 ms, Memory: 39508 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -9,5 +9,5 @@ for _ in range(n):
     voca_set.add(input().strip())
 voca_list = list(voca_set)
 voca_list.sort(key= lambda x: (len(x), x))
-for v in voca_list:
-    print(v)
+
+print('\n'.join(voca_list))
