919ebae53389c4b36999199b268f91e255064a97
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-30 22:22:58 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2388 ms, Memory: 112344 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuilder()
+    val n = br.readLine().toInt()
+    val list = ArrayList<Pair<Int, Int>>()
+    for(i in 0 until n){
+        val (x, y) = br.readLine().split(" ").map{it.toInt()}
+        list.add(Pair(x, y))
+    }
+    list.sortWith(compareBy({it.first}, {it.second}))
+    for(l in list){
+        println("${l.first} ${l.second}")
+    }
+}
