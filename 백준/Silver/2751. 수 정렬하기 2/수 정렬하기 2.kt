f2571c2e6d797475fe3d5b9b620d4d0bd232bf1c
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-30 21:11:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1260 ms, Memory: 162692 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -1,9 +1,11 @@
+
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val n = br.readLine().toInt()
     val list = ArrayList<Int>()
-    
+
     for(i in 0 until n) {
         list.add(br.readLine().toInt())
     }
