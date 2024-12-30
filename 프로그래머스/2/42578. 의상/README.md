87a123f21bd0e964aad9642efdeb3b274958a679
yujin45 <113690378+yujin45@users.noreply.github.com>
2024-12-30 18:18:53 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.11 ms
+메모리: 10.1 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 17일 00:36:34
+2024년 12월 30일 18:18:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,20 +1,9 @@
-
+from collections import Counter
 def solution(clothes):
-    closet ={}
-    # 리스트 2가지 요소로 가지고 올 수 있다
-    # 기억해야 할 것
-    # 1. for문 인자값 2개 받아올 수 있다.
-    # 2. [리스트]는 더하기 연산이 가능하다 [a]+[b] = [a, b]
-    for name, kind in clothes:
-        if kind in closet.keys():
-            # closet에 키가 이미 있으면
-            closet[kind]+=[name]
-        else:
-            closet[kind] = [name]
-    
-    # A종류가 N개 B 종류가 M개일 때의 경우의 수 = (N+1)(M+1)
-    answer = 1
-    for kind in closet.keys():
-        answer *= (len(closet[kind]) +1)
-    # 아무것도 안 입었을 때 빼주기
-    return answer-1
+    ckinds = [ckind[1] for ckind in clothes]
+    ckinds_dict = Counter(ckinds)
+    value_list = list(ckinds_dict.values())
+    ret = 1
+    for c in value_list:
+        ret *= (c+1)
+    return ret-1
