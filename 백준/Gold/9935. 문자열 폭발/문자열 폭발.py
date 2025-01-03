575927af02cfc60a19e54e58e750b34b4f70106b
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-03 23:19:59 +0900

    [Gold IV] Title: 문자열 폭발, Time: 288 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -6,13 +6,15 @@ original = input().strip()
 boom = input().strip()
 stack = []
 boom_size = len(boom)
+last_char = boom[-1]  # 폭발 문자열의 마지막 문자
 
-for i in range(len(original)):
-    #print(original[i])
-    stack.append(original[i])
-    if len(stack)>=boom_size and ''.join(stack[-boom_size:]) == boom:
-        for _ in range(boom_size):
-            stack.pop()
+for char in original:
+    stack.append(char)
+    # 스택의 마지막 문자가 폭발 문자열의 마지막 문자와 같을 때만 검사
+    if char == last_char and ''.join(stack[-boom_size:]) == boom:
+        del stack[-boom_size:]  # 슬라이싱 삭제
+
+# 결과 출력
 if not stack:
     print("FRULA")
 else:
