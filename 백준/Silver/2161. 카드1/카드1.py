33416d4edbd357fac196b175d33ac6facdbb9f95
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-04 06:59:43 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,15 +1,13 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
 n = int(input())
-deque = deque(range(1, n+1))
+deque = [i for i in range(1, n+1)]
 ret = []
 while deque:
-    ret.append(deque.popleft()) # 버리기
+    ret.append(deque.pop(0)) # 버리기
     if deque:
-        temp = deque.popleft() # 위의 것 뽑아서
+        temp = deque.pop(0) # 위의 것 뽑아서
         deque.append(temp) # 제일 아래로 두기
     #print(ret, deque)
 print(' '.join(map(str, ret)))
