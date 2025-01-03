dcbea9892c4db7d3c6ad28b2cab3b3ee8621b465
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-03 22:26:40 +0900

    [Silver IV] Title: 좋은 단어, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+ret =0
+for _ in range(n):
+    user_input = input().strip()
+    # 홀수개면 무조건 좋은 단어 안됨
+    if(len(user_input)%2==1):
+        continue
+    stack.clear()
+    for char in user_input:
+        if len(stack)==0:
+            stack.append(char)
+        elif stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    if len(stack)==0:
+        ret +=1
+
+print(ret)
