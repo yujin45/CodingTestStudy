ff5b81042a5ede66c7bf358bd780518c532774fc
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-05 20:42:07 +0900

    [Silver I] Title: 트럭, Time: 64 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -6,19 +6,25 @@ input = sys.stdin.readline
 n, w, L = map(int, input().split())
 trucks = deque(map(int, input().split()))
 
-queue = deque([0 for _ in range(w)])
 time = 0
+bridge_weight = 0  # 다리 위 총 무게
+bridge = deque([0] * w)  # 다리 상태 (현재 다리 위의 트럭들)
 
-while trucks:
-    truck_w = trucks[0]
-    queue.popleft()  # 앞에 빼고
-    if(sum(queue) + truck_w <= L):
-        # 적재 가능
-        queue.append(trucks.popleft()) # 올리기
-    else:
-        # 적재 불가능
-        queue.append(0)
+while trucks or bridge_weight > 0:
+    # 시간 경과
+    time += 1
 
-    time+=1
-# 마지막 적재한 상태이므로 마지막 것이 나가기 위해 w
-print(time+w)
+    # 다리에서 트럭 내리기
+    exited_truck = bridge.popleft()
+    bridge_weight -= exited_truck
+
+    if trucks:
+        # 다음 트럭이 다리에 올라갈 수 있는지 확인
+        if bridge_weight + trucks[0] <= L:
+            next_truck = trucks.popleft()
+            bridge.append(next_truck)
+            bridge_weight += next_truck
+        else:
+            bridge.append(0)  # 트럭이 못 올라가면 빈 공간 유지
+
+print(time)
