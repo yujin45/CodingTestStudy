abf4835f29afff8216d1628f89856625fa88649a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-07 22:19:25 +0900

    [Silver V] Title: 아파트, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.py
+++ 백준/Silver/32979. 아파트/아파트.py
@@ -11,17 +11,10 @@ b_calls = list(map(int, input().split()))
 
 # 결과 저장 리스트
 results = []
-
+idx = 0
 for b_j in b_calls:
-    # 패배자 위치 계산
-    idx = (b_j-1) % len(hand_apartment)
-    loser = hand_apartment[idx]
-    #print(idx, (b_j-1), len(hand_apartment), loser)
-    #결과 추가
-    results.append(loser)
-    # 손 아파트 회전
-    hand_apartment.rotate(-(b_j-1)) # 왼쪽으로 |n|칸 이동. 큐여서
-
+    idx = (idx + (b_j-1)) % len(hand_apartment)
+    results.append(hand_apartment[idx])
 
 print(' '.join(map(str, results)))
 
