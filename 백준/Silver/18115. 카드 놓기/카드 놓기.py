99429a9c503ff8903ca14828ab72626cd635809d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-08 23:15:47 +0900

    [Silver III] Title: 카드 놓기, Time: 668 ms, Memory: 176236 KB -BaekjoonHub

---
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())  # 1~n 정수 카드
+cards = deque(i for i in range(n, 0, -1))
+
+command = list(map(int, input().split()))
+ret = deque()
+for card in range(1, n + 1):
+    cmd = command.pop()
+    if cmd == 1:
+        ret.appendleft(card)
+    elif cmd == 2:
+        ret.insert(1, card)
+    elif cmd == 3:
+        ret.append(card)
+
+print(' '.join(map(str, ret)))
