dc1a0f281373f563204d1233033254aa78999af7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-10 07:16:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35392 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,54 +1,38 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-# 정점 개수: N
-# 간선 개수: M
-# 탐색 시작 정점: V번 (1부터 시작)
-N, M, V = map(int, input().split())
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for neighbor in sorted(graph[v]):
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited)
 
-# 번호 탐색 위해 0 포함해서 N+1
-visited_bfs = [False]*(N+1)
-visited_dfs = [False]*(N+1)
-# 인접 행렬로 구현
-graph = [[0]*(N+1) for _ in range(N+1)]
-
-
-def bfs(graph, visited, start_v):
-    result = []
-    queue = deque([start_v])
-    visited[start_v]= True
+def bfs(graph, start):
+    visited = [False] * len(graph)
+    queue = deque([start])
+    visited[start] = True
     while queue:
         v = queue.popleft()
-        result.append(v)
-        for i in range(N+1):
-            if graph[v][i] == 1 and not visited[i]:
-                queue.append(i) # 연결 노드 추가
-                visited[i]= True
-                
-    return result    
-        
-dfs_ret =[]        
-def dfs(graph, visited, v):
-    visited[v] = True
-    dfs_ret.append(v)
-    for i in range(N+1):
-        if graph[v][i] == 1 and not visited[i]:
-            dfs(graph, visited, i)
+        print(v, end=' ')
+        for neighbor in sorted(graph[v]):
+            if not visited[neighbor]:
+                queue.append(neighbor)
+                visited[neighbor] = True
 
+# 입력 처리
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for _ in range(M):
-    # 두 정점의 번호, 여러 간선 o, 양방향
-    v1, v2 = map(int, input().split())
-    graph[v1][v2] = 1
-    graph[v2][v1] = 1
+# 방문 기록 초기화
+visited = [False] * (n + 1)
 
-dfs(graph, visited_dfs, V)
-#print(dfs_ret)
-#print(bfs(graph, visited_bfs, 1))
-bfs_ret = bfs(graph, visited_bfs, V)
-
-print(' '.join(map(str, dfs_ret)))
-print(' '.join(map(str, bfs_ret)))
-
-
-
-
+# DFS 및 BFS 실행
+dfs(graph, v, visited)
+print()
+bfs(graph, v)
