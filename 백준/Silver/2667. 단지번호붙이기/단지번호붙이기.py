cc959ca5520f09a5fcefe05e5a95dfb60afc6493
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-10 09:34:03 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32520 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,35 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+
+def dfs(graph, x, y, n):
+    graph[x][y] = -1  # 방문 표시
+    global count
+    count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and (graph[nx][ny] == 1):
+            dfs(graph, nx, ny, n)
+
+
 n = int(input())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().strip())))
+graph = [list(map(int, input().strip())) for _ in range(n)]
 
-home = 0
-nums = []
-def dfs(x, y):
-    global home
-    if x <0 or y <0 or x>=n or y>=n:
-        return False
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+count = 0
 
-    if graph[x][y]==1:
-        home +=1
-        graph[x][y]=0
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-
-    return False
-
+home_count = []
 for i in range(n):
     for j in range(n):
-        if dfs(i, j)==True:
-            nums.append(home)
-            home = 0
-nums.sort()
-print(len(nums))
-for i in nums:
-    print(i)
+        # 돌면서 안 돈 곳 있으면 돌아야 함
+        if graph[i][j] == 1:
+            dfs(graph, i, j, n)
+            home_count.append(count)
+            count = 0
+home_count.sort()
+print(len(home_count))
+print('\n'.join(map(str, home_count)))
