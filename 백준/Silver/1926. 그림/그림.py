f71a2459a6eb4e1927445612b9dce1f46bd15636
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-12 21:26:28 +0900

    [Silver I] Title: 그림, Time: 500 ms, Memory: 98496 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,20 +1,16 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**7)
-directions = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
-]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def dfs(graph, x, y):
     count = 1
     graph[x][y] = 2
 
-    for (dx, dy) in directions:
-        nx = x + dx
-        ny = y + dy
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] == 1:
             count += dfs(graph, nx, ny)
 
