c77760fe1238f527c1e4581c3fc263cc34e117a7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-16 21:08:54 +0900

    [Silver I] Title: 양, Time: 88 ms, Memory: 33112 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -1,61 +1,44 @@
 import sys
-from collections import deque
 
+sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-directions = [
-    (-1, 0),
-    (1, 0),
-    (0, -1),
-    (0, 1)
-]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-def bfs(graph, start_x, start_y):
-    sheep = 0
-    wolf = 0
-    queue = deque([(start_x, start_y)])
-    if graph[start_x][start_y] == 'v':
-        # 늑대
-        wolf += 1
-    elif graph[start_x][start_y] == 'o':
-        # 양
+def dfs(graph, x, y):
+    global sheep, wolf
+
+    if graph[x][y] == 'o':
         sheep += 1
-    graph[start_x][start_y] = '#'
+    elif graph[x][y] == 'v':
+        wolf += 1
 
-    while queue:
-        cx, cy = queue.popleft()
+    graph[x][y] = '#'
 
-        for (dx, dy) in directions:
-            nx = cx + dx
-            ny = cy + dy
-            if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] != '#':
-                queue.append((nx, ny))
-                if graph[nx][ny] == 'v':
-                    # 늑대
-                    wolf += 1
-                elif graph[nx][ny] == 'o':
-                    # 양
-                    sheep += 1
-                graph[nx][ny] = '#'
-            elif nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph[0]):
-                return 0, 0
-    if wolf >= sheep:
-        return 0, wolf
-    elif wolf < sheep:
-        return sheep, 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] != '#':
+            dfs(graph, nx, ny)
 
 
-r, c = map(int, input().split())  # 행 열
-
+r, c = map(int, input().split())
 graph = [list(map(str, input().strip())) for _ in range(r)]
 
-total_wolf = 0
 total_sheep = 0
+total_wolf = 0
 for i in range(r):
     for j in range(c):
-        if graph[i][j] == 'o' or graph[i][j] == 'v':
-            sheep, wolf = bfs(graph, i, j)
-            total_wolf += wolf
-            total_sheep += sheep
+        # .만 있는 경우 pass 가능하게 됨
+        if graph[i][j] == 'v' or graph[i][j] == 'o':
+            sheep = 0
+            wolf = 0
+            dfs(graph, i, j)
+            if sheep > wolf:
+                total_sheep += sheep
+            else:
+                total_wolf += wolf
+
 print(total_sheep, total_wolf)
