5576098ca1f957e128f148df43711809ea44f53c
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-16 20:36:45 +0900

    [Silver I] Title: 양, Time: 108 ms, Memory: 35004 KB -BaekjoonHub

---
+++ 백준/Silver/3184. 양/양.py
@@ -0,0 +1,65 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+directions = [
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1)
+]
+
+
+def bfs(graph, start_x, start_y):
+    sheep = 0
+    wolf = 0
+    queue = deque([(start_x, start_y)])
+    if graph[start_x][start_y] == 'v':
+        # 늑대
+        wolf += 1
+    elif graph[start_x][start_y] == 'o':
+        # 양
+        sheep += 1
+    graph[start_x][start_y] = '#'
+
+    while queue:
+        cx, cy = queue.popleft()
+
+        for (dx, dy) in directions:
+            nx = cx + dx
+            ny = cy + dy
+            if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] != '#':
+                queue.append((nx, ny))
+                if graph[nx][ny] == 'v':
+                    # 늑대
+                    wolf += 1
+                elif graph[nx][ny] == 'o':
+                    # 양
+                    sheep += 1
+                graph[nx][ny] = '#'
+            elif nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph[0]):
+                return 0, 0
+    if wolf >= sheep:
+        sheep = 0
+    elif wolf < sheep:
+        wolf = 0
+
+    return sheep, wolf
+
+
+r, c = map(int, input().split())  # 행 열
+
+graph = []
+for _ in range(r):
+    graph.append(list(input()))
+
+total_wolf = 0
+total_sheep = 0
+for i in range(r):
+    for j in range(c):
+        if graph[i][j] != '#':
+            sheep, wolf = bfs(graph, i, j)
+            total_wolf += wolf
+            total_sheep += sheep
+print(total_sheep, total_wolf)
