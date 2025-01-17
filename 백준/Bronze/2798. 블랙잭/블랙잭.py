6be3f9e118aa4c040c46742285d5e7a71cbef483
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-17 20:48:57 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,21 +1,16 @@
 import sys
-from itertools import combinations
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
-pick = list(combinations(cards, 3))
+max_sum = 0
 
-for i in range(len(pick)):
-    pick[i] = sum(pick[i])
-
-pick.sort()
-ret = 0
-for p in pick:
-    if p <= m:
-        ret = p
-    else:
-        break
-print(ret)
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            current_sum = cards[i] + cards[j] + cards[k]
+            if current_sum <= m:
+                max_sum = max(max_sum, current_sum)
+print(max_sum)
