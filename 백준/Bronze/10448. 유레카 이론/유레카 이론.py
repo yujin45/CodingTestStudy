cae327232128e8e35c7a86ec6bfa3ed8a9ef808c
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-21 20:37:49 +0900

    [Bronze I] Title: 유레카 이론, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -1,33 +1,37 @@
-import sys
-
-input = sys.stdin.readline
-
-n = 1
-
-t = int(input().strip())
-
-ret = 0
-nums = []
-while True:
-    ret = n * (n + 1) // 2
-    if ret > 1000:
-        break
-
-    nums.append(ret)
-    n += 1
-
-
-def check(k):
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            for h in range(len(nums)):
-                ksum = nums[i] + nums[j] + nums[h]
-                if ksum == k:
+def is_eureka_number(k, triangle_numbers):
+    # 3중 루프를 사용해 K를 검사
+    for i in triangle_numbers:
+        for j in triangle_numbers:
+            for h in triangle_numbers:
+                if i + j + h == k:
                     return 1
-
     return 0
 
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    T = int(data[0])  # 테스트케이스 개수
+    cases = list(map(int, data[1:]))
+    
+    # 삼각수 리스트 생성 (Tn ≤ 1000)
+    triangle_numbers = []
+    n = 1
+    while True:
+        t = n * (n + 1) // 2  # 삼각수 계산
+        if t > 1000:  # 1000을 초과하면 종료
+            break
+        triangle_numbers.append(t)
+        n += 1
+    
+    # 각 테스트케이스 처리
+    results = []
+    for k in cases:
+        results.append(is_eureka_number(k, triangle_numbers))
+    
+    # 결과 출력
+    sys.stdout.write('\n'.join(map(str, results)) + '\n')
 
-for _ in range(t):
-    k = int(input().strip())
-    print(check(k))
+if __name__ == "__main__":
+    main()
