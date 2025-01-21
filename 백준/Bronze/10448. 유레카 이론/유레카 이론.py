7128d678e5d74e2faef2d5a723ca7050293b624f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-21 20:31:47 +0900

    [Bronze I] Title: 유레카 이론, Time: 216 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+n = 1
+
+t = int(input().strip())
+
+ret = 0
+nums = []
+while True:
+    ret = n * (n + 1) // 2
+    if ret > 1000:
+        break
+
+    nums.append(ret)
+    n += 1
+
+
+def check(k):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            for h in range(len(nums)):
+                ksum = nums[i] + nums[j] + nums[h]
+                if ksum == k:
+                    return True
+
+    return False
+
+
+for _ in range(t):
+    k = int(input().strip())
+    if check(k):
+        print(1)
+    else:
+        print(00)
