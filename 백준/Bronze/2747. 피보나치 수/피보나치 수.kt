a6ca27fe625241a4525c9ab150b1ad5bbda6d10f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-23 09:46:28 +0900

    [Bronze II] Title: 피보나치 수, Time: 76 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.kt
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.kt
@@ -1,14 +1,19 @@
 package org.example
 
+fun fib(num: Int, dp: IntArray): Int {
+    if (dp[num] == -1) {
+        dp[num] = fib(num - 1, dp) + fib(num - 2, dp)
+    }
+    return dp[num]
+}
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val num = br.readLine().toInt()
-    val dp = IntArray(46)
+    val dp = IntArray(46) { -1 }
     dp[0] = 0
     dp[1] = 1
-    for (i in 2 until num + 1) {
-        dp[i] = dp[i - 1] + dp[i - 2]
-    }
-    println(dp[num])
+    println(fib(num, dp))
     br.close()
 }
+
