c9743df535da7030ae64fbf901cca8d69b059d66
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-23 11:49:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -3,35 +3,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-dp = [-1] * (n + 1)
-
-for i in range(1, n + 1):
-    temp5 = -1
-    temp3 = -1
-    if i % 5 == 0:
-        temp5 = i // 5
-    else:
-        if dp[i - 5] == -1:
-            temp5 = -1
-        else:
-            temp5 = 1 + dp[i - 5]
-
-    if i % 3 == 0:
-        temp3 = i // 3
-    else:
-        if dp[i - 3] == -1:
-            temp3 = -1
-        else:
-            temp3 = 1 + dp[i - 3]
-
-    if temp3 == -1 and temp5 != -1:
-        dp[i] = temp5
-    elif temp3 != -1 and temp5 == -1:
-        dp[i] = temp3
-    elif temp3 == -1 and temp5 == -1:
-        dp[i] = -1
-    else:
-        dp[i] = min(temp3, temp5)
-
-print(dp[n])
+count = 0
+while n >= 0:
+    if n % 5 == 0:
+        count += n // 5
+        print(count)
+        break
+    n -= 3
+    count += 1
+else:
+    print(-1)
