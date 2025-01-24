2ea58cb0d58091f644eff4e7033f6a156b19c532
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-24 19:15:31 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -5,14 +5,10 @@ input = sys.stdin.readline
 n = int(input())
 user_input = list(map(int, input().split()))
 
-# dp 배열 초기화
 dp = [0] * n
 dp[0] = user_input[0]
 max_num = dp[0]
-
-# DP 점화식 적용
 for i in range(1, n):
     dp[i] = max(dp[i - 1] + user_input[i], user_input[i])
     max_num = max(max_num, dp[i])
-
 print(max_num)
