fa420d2e9f2f6e7d26ab26ab50196efc18ae7413
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-25 14:21:15 +0900

    [Silver I] Title: 카드 구매하기, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -6,7 +6,10 @@ n = int(input())
 dp = [0] + list(map(int, input().split()))
 
 for i in range(1, n + 1):
-    for j in range(1, i // 2 + 1):
-        dp[i] = max(dp[i], dp[i - j] + dp[j])
+    if i ==1:
+        continue
+    else:
+        for j in range(1, i // 2 + 1):
+            dp[i] = max(dp[i], dp[i - j] + dp[j])
 
 print(dp[n])
