a7f38fd46fa323df2536dbe7f607d4d593e22565
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-31 14:47:34 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 53656 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -4,18 +4,18 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    // 1~n까지 자연수 중에서 중복 없이 M개 고른 수열
-    val sequence = IntArray(m)
-    val visited = BooleanArray(n + 1)
+    // 중복 X, 1~n 중 m개 선택 수열, 오름차순
+    val sequence = IntArray(m) // m개 선택한 숫자 넣어지는 용도
+    val visited = BooleanArray(n + 1) // 중복 체크용도 1~n
 
     fun backtrack(depth: Int) {
-        if (depth == m) {
+        if (depth == m) { // m개면 return
             sequence.forEach { sb.append("$it ") }
             sb.append("\n")
             return
         }
-        for (i in 1..n) {
-            if (!visited[i]) { // 중복 제거
+        for (i in 1..n) { // 1부터 n까지
+            if (!visited[i]) { // 중복 X
                 visited[i] = true
                 sequence[depth] = i
                 backtrack(depth + 1)
@@ -24,7 +24,7 @@ fun main() {
         }
     }
     backtrack(0)
+    //
     print(sb)
-
     br.close()
 }
