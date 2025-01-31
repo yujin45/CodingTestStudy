580ee2b44a2e95494b074f472ac214058dc13fdd
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-31 14:22:11 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 57156 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -2,28 +2,29 @@ package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
     val sb = StringBuilder()
-    val sequence = mutableListOf<Int>()
-    val visited = BooleanArray(n + 1) // 1부터 N까지 사용 (0은 사용 X)
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    // 1~n까지 자연수 중에서 중복 없이 M개 고른 수열
+    val sequence = IntArray(m)
+    val visited = BooleanArray(n + 1)
 
-    fun backtrack() {
-        if (sequence.size == m) {
-            sb.append("${sequence.joinToString(" ")}\n")
+    fun backtrack(depth: Int) {
+        if (depth == m) {
+            sequence.forEach { sb.append("$it ") }
+            sb.append("\n")
             return
         }
         for (i in 1..n) {
-            if (!visited[i]) {
+            if (!visited[i]) { // 중복 제거
                 visited[i] = true
-                sequence.add(i)
-                backtrack()// 다음 숫자 탐색
-                sequence.removeLast() // 백트래킹(이전 상태로 되돌리기) 1,2 하고서 1,3할 수 있게 2 제거
+                sequence[depth] = i
+                backtrack(depth + 1)
                 visited[i] = false
             }
         }
     }
-    backtrack()
+    backtrack(0)
     print(sb)
+
     br.close()
 }
-
