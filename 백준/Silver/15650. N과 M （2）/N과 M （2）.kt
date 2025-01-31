19f11a2831320c1565b3f7b184b96623d18a5c47
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-31 14:52:26 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 21028 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
@@ -4,20 +4,22 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val sequence = mutableListOf<Int>()
+    // 중복 X, 1~n 중 m개 선택 - 조합, 오름차순
+    val sequence = IntArray(m)
 
-    fun backtrack(start: Int) {
-        if (sequence.size == m) {
-            sb.append("${sequence.joinToString(" ")}\n")
+    fun backtrack(depth: Int, start: Int) {
+        if (depth == m) {
+            sequence.forEach { sb.append("$it ") }
+            sb.append("\n")
             return
         }
-        for (i in start..n) { // 오름차순 위한 start부터 탐색
-            sequence.add(i)
-            backtrack(i + 1) // 다음 숫자는 i보다 +1
-            sequence.removeLast() // 백트래킹
+        for (i in start..n) {
+            sequence[depth] = i
+            backtrack(depth + 1, i + 1)
         }
     }
-    backtrack(1)
+    backtrack(0, 1)
+    //
     print(sb)
     br.close()
 }
