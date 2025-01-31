05166143668814c20ee080b9c7e93492b43400c1
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-01-31 20:54:56 +0900

    [Silver II] Title: 부분수열의 합, Time: 128 ms, Memory: 20776 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
@@ -1,38 +1,21 @@
 package org.example
 
-import kotlin.math.absoluteValue
-
-
 fun main() {
     val br = System.`in`.bufferedReader()
     // 조합을 다 구한다
     val (n, s) = br.readLine().split(" ").map { it.toInt() }
     val numbers = br.readLine().split(" ").map { it.toInt() }
-    // 중복 숫자들 중에서 2, 3, -2, -2 가 있다고 하면 2, -2는 1번만 카운트해야 함
-    // 2, -2랑 -2, 2도 같은 것 => 조합을 구해야 함
-    // 자기 자신은 x
     var result = 0
-    fun backtrack(depth: Int, start: Int, end: Int, sequence: IntArray) {
-        if (depth == end) {
-            // 몇개 뽑을지에 따라
-            if (sequence.sum() == s) {
-                //println("$start, $end, ${sequence.contentToString()}")
-                result++
-            }
-            return
-        }
-        // -2, -2, 4 이런 경우가 있어서 lastUsed는 사용하지 않음.
-        for (i in start until n) {
-            sequence[depth] = numbers[i]
-            backtrack(depth + 1, i + 1, end, sequence)
-        }
-    }
 
-    // 1개만 뽑는 것도 s가 될 수 있음. 1부터 시작
-    for (end in 1..n) {
-        val sequence = IntArray(end)
-        backtrack(0, 0, end, sequence)
+    fun backtrack(depth: Int, start: Int, sum: Int) {
+        if (depth > 0 && sum == s) { //공집합 제외
+            result++
+        }
+        for (i in start until n) {
+            backtrack(depth + 1, i + 1, sum + numbers[i]) // 합을 체크하면서 감
+        }
     }
+    backtrack(0, 0, 0)
 
     println(result)
     br.close()
