1fc3131bd28e2acadd1a5471baf914107ad730c0
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-04 08:09:44 +0900

    [Silver IV] Title: ATM, Time: 140 ms, Memory: 24096 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.kt
+++ 백준/Silver/11399. ATM/ATM.kt
@@ -1,11 +1,16 @@
+package org.example
+
 fun main() {
     val br = System.`in`.bufferedReader()
-
     val n = br.readLine().toInt()
-    val list = br.readLine().split(" ").map{it.toInt()}.sorted()
-    var ret = 0
-    for(i in 0 until n){
-        ret += list[i]*(n-i)
+    // 1~n번
+    val p = br.readLine().split(" ").map { it.toInt() }.sorted()
+    var sum = 0
+    var result = 0
+    for (i in 0 until n) {
+        sum += p[i]
+        result += sum
     }
-    println(ret)
+    println(result)
+    br.close()
 }
