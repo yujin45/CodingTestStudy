b11849c760f98ce846911eb3b1b24b8c50469b2b
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-03 20:58:13 +0900

    [Silver IV] Title: 베스트셀러, Time: 76 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.kt
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.kt
@@ -6,11 +6,18 @@ fun main() {
     val n = br.readLine().toInt()
     // 만약 가장 많이 팔린 책이 여러 개일 경우에는 사전 순으로 가장 앞서는 제목을 출력
     val map = HashMap<String, Int>()
+    var maxCount = 0
+    var bestSeller = ""
     repeat(n) {
         val title = br.readLine()
-        map[title] = map.getOrDefault(title, 0) + 1
+        val count = map.getOrDefault(title, 0) + 1
+        map[title] = count
+        if (count > maxCount || (count == maxCount && title < bestSeller)) {
+            // maxCount보다 크거나 || 같은데 사전순이 더 앞이면
+            maxCount = count
+            bestSeller = title
+        }
     }
-    val sortedMap = map.entries.sortedWith(compareByDescending<Map.Entry<String, Int>> { it.value }.thenBy { it.key })
-    println(sortedMap.first().key)
+    println(bestSeller)
     br.close()
 }
