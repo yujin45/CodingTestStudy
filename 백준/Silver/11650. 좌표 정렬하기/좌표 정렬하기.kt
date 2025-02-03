652941badaab4f41faac3564ff9692a83771edb1
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-04 08:32:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 928 ms, Memory: 135560 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -1,15 +1,20 @@
+package org.example
+
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val n = br.readLine().toInt()
-    val list = ArrayList<Pair<Int, Int>>()
-    for(i in 0 until n){
-        val (x, y) = br.readLine().split(" ").map{it.toInt()}
-        list.add(Pair(x, y))
+    val graph = Array(n) { IntArray(2) }
+    repeat(n) {
+        val (x, y) = br.readLine().split(" ").map { it.toInt() }
+        graph[it][0] = x
+        graph[it][1] = y
     }
-    list.sortWith(compareBy({it.first}, {it.second}))
-    for(l in list){
-        sb.append("${l.first} ${l.second}\n")
+    graph.sortedWith(compareBy({ it[0] }, { it[1] })).forEach {
+        sb.append(it[0]).append(" ").append(it[1]).append("\n")
     }
-    println(sb.toString())
+    println(sb)
+
+    br.close()
 }
