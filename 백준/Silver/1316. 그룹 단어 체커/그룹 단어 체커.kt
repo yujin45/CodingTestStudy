fee03165f6fdeb902dfd62adb48d253ac6ab41a7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-03 15:04:44 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 12356 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.kt
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.kt
@@ -4,36 +4,25 @@ package org.example
 fun main() {
     val br = System.`in`.bufferedReader()
     val N = br.readLine().toInt()
-
-    var before: Char
-    var notGroupflag = false
     var count = 0
-    repeat(N) {
-        val alphabet = br.readLine()
-        val alphabetSet = HashSet<Char>()
 
-        before = alphabet[0]
-        alphabetSet.add(alphabet[0])
-        for (i in 1 until alphabet.length) {
-            if (alphabet[i] != before) {
-                // 이전과 다르면 set에 있는지 일단 확인
-                if (alphabet[i] in alphabetSet) {
-                    // 있는 경우 검사 더 필요 없음
-                    notGroupflag = true
-                    break
-                } else {
-                    // 없는 경우
-                    alphabetSet.add(alphabet[i])
-                }
-                before = alphabet[i]
+    repeat(N) {
+        val word = br.readLine()
+        val seen = BooleanArray(26) // 알파벳 26개
+        var isGroupWord = true
+
+        for (i in word.indices) {
+            if (i > 0 && word[i] != word[i - 1] && seen[word[i] - 'a']) {
+                // 2번째 알파벳이 이전과 다른데, 이미 seen에 존재하면 그룹 단어 아님
+                isGroupWord = false
+                break
             }
+            // 위에 해당하는게 아니면 단어 등장 표시하기
+            seen[word[i] - 'a'] = true
         }
-        if (!notGroupflag) {
-            count++
-        } else {
-            notGroupflag = false
-        }
+        if (isGroupWord) count++
     }
+
     println(count)
     br.close()
 }
