3a42005ac7f6eb0ef706c565c7926a0f6970dd6a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-04 08:21:41 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 70180 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,16 +1,16 @@
+package org.example
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val n = br.readLine().toInt()
-    val nSet = br.readLine().split(" ").map{it.toInt()}.toHashSet()
+    val aList = br.readLine().split(" ").map { it.toInt() }.toHashSet()
     val m = br.readLine().toInt()
-    val mList = br.readLine().split(" ").map{it.toInt()}
-    for(e in mList){
-        if(e in nSet){
-            sb.appendLine("1")
-        }else{
-            sb.appendLine("0")
-        }
+    val bList = br.readLine().split(" ").map { it.toInt() }
+    bList.forEach {
+        if (it in aList) sb.append(1) else sb.append(0)
+        sb.append("\n")
     }
-    print(sb.toString())
+    println(sb)
+    br.close()
 }
