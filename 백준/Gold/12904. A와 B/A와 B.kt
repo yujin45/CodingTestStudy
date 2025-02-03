33084fc66516c93d3b24c5d366ac7c8e7a3bf63f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-03 21:46:54 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.kt
+++ 백준/Gold/12904. A와 B/A와 B.kt
@@ -3,27 +3,17 @@ package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val T = StringBuilder()
     val S = br.readLine()
-    T.append(br.readLine())
+    val T = StringBuilder(br.readLine())
 
-    var isAble = false
-    // 아이디어!!! : 거꾸로 생각하기
-    while (T.length >= S.length) {
-        if (T.toString() == S) {
-            isAble = true
-            break
-        }
+    while (T.length > S.length) {
         if (T.last() == 'A') {
-            // 마지막이 A면 # 문자열의 뒤에 A를 추가한다.# 연산을 한 것
-            T.deleteCharAt(T.lastIndex)
-        } else if (T.last() == 'B') {
-            // 마지막이 B면 # 문자열 뒤집고 뒤에 B 추가 # 연산을 한 것
-            T.deleteCharAt(T.lastIndex)
-            T.reverse()
+            T.deleteCharAt(T.lastIndex) // dropLast() 대신 StringBuilder 사용
+        } else {
+            T.deleteCharAt(T.lastIndex).reverse() // reversed() 대신 StringBuilder 사용
         }
     }
 
-    println(if (isAble) 1 else 0)
+    println(if (S == T.toString()) 1 else 0) // 최종 비교
     br.close()
 }
