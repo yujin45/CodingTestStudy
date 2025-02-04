737d4c81e25f8494a2cb7a64905093f9c50937c7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-04 17:01:02 +0900

    [Silver I] Title: 신입 사원, Time: 768 ms, Memory: 298248 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.kt
+++ 백준/Silver/1946. 신입 사원/신입 사원.kt
@@ -1,22 +1,30 @@
 package org.example
 
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val t = br.readLine().toInt()
     val sb = StringBuilder()
     repeat(t) {
         val n = br.readLine().toInt()
-        val candidate = Array(n) { br.readLine().split(" ").map { it.toInt() } }
-            .sortedWith(compareBy({ it[0] }))
+        var st: StringTokenizer
+        val candidate = IntArray(n)
+        for (i in 0 until n) {
+            st = StringTokenizer(br.readLine())
+            // 정렬 대신 서류 순위로 인덱싱을 해둘 수도 있음!!
+            candidate[st.nextToken().toInt() - 1] = st.nextToken().toInt()
+        }
+
         // 서류 순서로 정렬하고 검사해주기
         //println(candidate)
-        var mScore = candidate[0][1] // 1위의 면접 순위를 가져오기
+        var mScore = candidate[0] // 1위의 면접 순위를 가져오기
         var count = 1
         for (i in 1 until n) {
             //println("${candidate[i][1]} - ${mScore}")
-            if (candidate[i][1] < mScore) {
+            if (candidate[i] < mScore) {
                 count++
-                mScore = candidate[i][1] // 업데이트
+                mScore = candidate[i]// 업데이트
             }
         }
         sb.append(count).append("\n")
