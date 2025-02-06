ccbe4d9b375444039004322bea17d4661a4f38e2
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-06 16:24:08 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 156 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
@@ -1,37 +1,41 @@
 package org.example
 
-import java.util.Stack
+import java.util.StringTokenizer
 
-fun main(){
+fun main() {
     val br = System.`in`.bufferedReader()
+    // 단어만 뒤집기
+    val stack = mutableListOf<Char>()
     val sb = StringBuilder()
-    val stack = Stack<Char>()
-    var flag = false
     val userInput = br.readLine()
-    for(i in 0 until userInput.length){
-        val userInputChar = userInput[i]
-        if(userInputChar == '<'){
-            while(!stack.isEmpty()){
-                sb.append(stack.pop())
+    var isOpen = false
+    for (c in userInput) {
+        if (c == '<') {
+            while (stack.isNotEmpty()) {
+                sb.append(stack.removeLast())
             }
-            sb.append(userInputChar)
-            flag = true
-        }else if(userInputChar == '>'){
-            sb.append(userInputChar)
-            flag = false
-        }else if(flag){
-            sb.append(userInputChar)
-        }else if(!flag && userInputChar == ' '){
-            while(!stack.isEmpty()){
-                sb.append(stack.pop())
+            sb.append(c)
+            isOpen = true
+
+        } else if (c == '>') {
+            sb.append(c)
+            isOpen = false
+        } else if (isOpen) {
+            sb.append(c)
+        } else if (!isOpen && c == ' ') {
+            // 닫혀있는데 공백이면
+            while (stack.isNotEmpty()) {
+                sb.append(stack.removeLast())
             }
-            sb.append(userInputChar)
-        }else{
-            stack.push(userInputChar)
+            sb.append(c)
+        } else {
+            stack.add(c)
         }
     }
-    while(!stack.isEmpty()){
-        sb.append(stack.pop())
+    while (stack.isNotEmpty()) {
+        sb.append(stack.removeLast())
     }
     println(sb)
+
+    br.close()
 }
