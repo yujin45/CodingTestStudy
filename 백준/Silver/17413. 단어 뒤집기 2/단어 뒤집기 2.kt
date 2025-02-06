3faf0f076f1c6453a8307e24d3c3c7b3bbfa6b05
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-06 16:32:17 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 148 ms, Memory: 17416 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
@@ -1,40 +1,45 @@
 package org.example
 
-import java.util.StringTokenizer
-
 fun main() {
     val br = System.`in`.bufferedReader()
-    // 단어만 뒤집기
-    val stack = mutableListOf<Char>()
     val sb = StringBuilder()
     val userInput = br.readLine()
-    var isOpen = false
+//    var isOpen = false
+    var insideTag = false // 태그인지 아닌지만 판단
+    var temp = StringBuilder()
     for (c in userInput) {
-        if (c == '<') {
-            while (stack.isNotEmpty()) {
-                sb.append(stack.removeLast())
+        when {
+            c == '<' -> {
+                sb.append(temp.reverse()) // 단어 뒤집기
+                temp.clear()
+                sb.append(c) // 현재 기호 넣기
+                insideTag = true
             }
-            sb.append(c)
-            isOpen = true
 
-        } else if (c == '>') {
-            sb.append(c)
-            isOpen = false
-        } else if (isOpen) {
-            sb.append(c)
-        } else if (!isOpen && c == ' ') {
-            // 닫혀있는데 공백이면
-            while (stack.isNotEmpty()) {
-                sb.append(stack.removeLast())
+            c == '>' -> {
+                sb.append(c)
+                insideTag = false
             }
-            sb.append(c)
-        } else {
-            stack.add(c)
+
+            insideTag -> {
+                // ★ 여기가 포인트!! 태그 내부의 문자는 그대로 출력!
+                // 그래서 when(c)로 하는게 아니라 when{}으로 하는 것!
+                sb.append(c)
+            }
+
+            c == ' ' -> {
+                sb.append(temp.reverse()) // 뒤집어서 넣기
+                temp.clear()
+                sb.append(c)
+            }
+
+            else -> {
+                temp.append(c)
+            }
         }
+
     }
-    while (stack.isNotEmpty()) {
-        sb.append(stack.removeLast())
-    }
+    sb.append(temp.reverse())
     println(sb)
 
     br.close()
