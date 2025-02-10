117cad7d6c12ec69b03e665d1527ed3be9712c01
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-10 17:52:34 +0900

    [Silver II] Title: 촌수계산, Time: 108 ms, Memory: 20760 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.kt
+++ 백준/Silver/2644. 촌수계산/촌수계산.kt
@@ -1,35 +1,37 @@
 package org.example
 
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
-
-    val n = br.readLine().toInt()
+    val n = br.readLine().toInt() // 전체 사람 수 n
     val (p1, p2) = br.readLine().split(" ").map { it.toInt() }
-    val m = br.readLine().toInt()
+    val m = br.readLine().toInt() // 관계 수 m
     val graph = Array(n + 1) { mutableListOf<Int>() }
     repeat(m) {
-        val (x, y) = br.readLine().split(" ").map { it.toInt() }
-        graph[x].add(y)
-        graph[y].add(x)
+        val st = StringTokenizer(br.readLine())
+        val a = st.nextToken().toInt()
+        val b = st.nextToken().toInt()
+        graph[a].add(b)
+        graph[b].add(a)
     }
-
     val visited = BooleanArray(n + 1)
-    var count = 0
-    println(dfs(graph, p1, p2, visited, count))
+    println(dfs(graph, p1, p2, visited, 0))
 
     br.close()
 }
 
-fun dfs(graph: Array<MutableList<Int>>, v: Int, p2: Int, visited: BooleanArray, count: Int): Int {
+fun dfs(graph: Array<MutableList<Int>>, v: Int, p2: Int, visited: BooleanArray, depth: Int): Int {
     visited[v] = true
+    //println("v $v / depth $depth / p2 $p2")
     if (v == p2) {
-        return count
+        return depth
     }
     for (neighbor in graph[v]) {
         if (!visited[neighbor]) {
-            val result = dfs(graph, neighbor, p2, visited, count + 1)
-            if (result != -1) {
-                return result
+            val ret = dfs(graph, neighbor, p2, visited, depth + 1)
+            if (ret != -1) {
+                return ret
             }
         }
     }
