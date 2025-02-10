cfce6cbc6eecbad5da2ac76dd753f64407c63ae3
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-10 20:48:18 +0900

    [Gold V] Title: 노드사이의 거리, Time: 224 ms, Memory: 27404 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.kt
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.kt
@@ -1,6 +1,5 @@
 package org.example
 
-import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
@@ -14,29 +13,24 @@ fun main() {
     val sb = StringBuilder()
     repeat(M) {
         val (p1, p2) = br.readLine().split(" ").map { it.toInt() }
-        sb.append(bfs(graph, p1, p2, N)).append("\n")
+        val visited = BooleanArray(N + 1)
+        sb.append(dfs(graph, p1, p2, visited, 0)).append("\n")
     }
     print(sb)
     br.close()
 }
 
-fun bfs(graph: Array<MutableList<Pair<Int, Int>>>, start: Int, target: Int, N: Int): Int {
-    val visited = BooleanArray(N + 1)
-    val queue = ArrayDeque<Pair<Int, Int>>() // 현재 노드, 거리
-    queue.addLast(start to 0)
-    visited[start] = true
-
-    while (queue.isNotEmpty()) {
-        val (current, currentDistance) = queue.removeLast()
-        if (current == target) {
-            return currentDistance
-        }
-        for ((neighbor, distance) in graph[current]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true
-                queue.addLast(neighbor to (currentDistance + distance)) // 지금 거리에 더 가야 하는 거리
-            }
+fun dfs(graph: Array<MutableList<Pair<Int, Int>>>, v: Int, target: Int, visited: BooleanArray, dist: Int): Int {
+    visited[v] = true
+    if (v == target) {
+        return dist
+    }
+    for ((neighbor, cost) in graph[v]) {
+        if (!visited[neighbor]) {
+            val ret = dfs(graph, neighbor, target, visited, dist + cost)
+            if (ret != -1) return ret
         }
     }
-    return -1 // 연결되지 않은 경우
+
+    return -1
 }
