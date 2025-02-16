31f13fa1a778d9f17e859b867f0eb81dccfe9b96
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-16 18:43:09 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 304 ms, Memory: 37860 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.kt
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.kt
@@ -1,28 +1,12 @@
-package org.example
+import java.util.*
+import kotlin.math.abs
 
-import java.util.StringTokenizer
-import kotlin.math.absoluteValue
-
-
-fun gcd(a: Int, b: Int): Int {
-    return if (b == 0) a else gcd(b, a % b)
-}
+fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, S) = br.readLine().split(" ").map { it.toInt() }
-    if (N == 1) {
-        println((br.readLine().toInt() - S).absoluteValue)
-        return
-    }
-    val A = IntArray(N)
-    val st = StringTokenizer(br.readLine())
-    repeat(N) {
-        A[it] = (st.nextToken().toInt() - S).absoluteValue
-    }
-    //println(A.contentToString())
-    val gcdA = A.reduce { acc, num -> gcd(acc, num) }
-    println(gcdA)
+    val A = br.readLine().split(" ").map { abs(it.toInt() - S) }
 
-    br.close()
+    println(A.reduce(::gcd))
 }
