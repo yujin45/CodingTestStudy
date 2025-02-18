5a6b1f493c644b7389ae5c1dda3a1e62badb50df
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-18 14:48:18 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.16 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/README.md
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 부족한 금액 계산하기 - 82612 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/82612?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/82612) 
 
 ### 성능 요약
 
-메모리: 60.2 MB, 시간: 0.10 ms
+메모리: 64.1 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 10:36:12
+2025년 02월 18일 14:48:16
 
 ### 문제 설명
 
--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(price: Int, money: Int, count: Int): Long {
-        var totalPrice : Long = 0
-        // N번째 이용 : price * N
-        for(n in 1..count){
-            totalPrice += price * n 
+        var total :Long = 0
+        for(i in 1..count){
+            total += i*price
         }
-        // 부족한 금액이 없으면 0, 있으면 차이를 반환
-        return if (totalPrice > money) totalPrice - money else 0L
+        //println("total: $total")
+        val need = total - money
+        return if(need >0) need else 0
     }
 }
