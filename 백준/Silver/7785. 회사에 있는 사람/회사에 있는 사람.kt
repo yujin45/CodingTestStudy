34a32f93bafc34516a3d8427107ef2baa611b917
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-18 10:33:03 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 512 ms, Memory: 54668 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
@@ -1,23 +1,24 @@
+package org.example
+
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
-
     val n = br.readLine().toInt()
-    val set = HashSet<String>()
+    val hashSet = HashSet<String>()
     repeat(n) {
-        val (name, state) = br.readLine().split(" ")
-        if (state == "enter"){
-            set.add(name)
-        }else{
-            set.remove(name)
+        val st = StringTokenizer(br.readLine())
+        val name = st.nextToken()
+        if (st.nextToken() == "enter") {
+            hashSet.add(name)
+        } else {
+            hashSet.remove(name) // O(1) 해시 테이블 사용해서
         }
     }
-
-    for(name in set.sortedDescending()){
-        bw.write(name)
-        bw.write("\n")
-    }
+    
+    val sortedSet = hashSet.toSortedSet(compareByDescending { it }) // O(N log N), 내부적으로 이진 트리 정렬 수행.
+    val sb = StringBuilder()
+    sortedSet.forEach { sb.append(it).append("\n") }
+    print(sb)
     br.close()
-    bw.flush()
-    bw.close()
 }
