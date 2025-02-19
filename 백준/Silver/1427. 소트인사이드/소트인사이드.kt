eee92a5b7485198cc8b66d59222ef1657e701e41
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-19 14:16:27 +0900

    [Silver V] Title: 소트인사이드, Time: 116 ms, Memory: 18916 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.kt
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.kt
@@ -1,9 +1,11 @@
+
 fun main() {
     val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
     val sb = StringBuilder()
-    val nums = br.readLine().map{it.digitToInt()}.sortedDescending()
-    for(n in nums){
-        sb.append("$n")
-    }
-    print(sb.toString())
+    val nums = br.readLine().toCharArray().sortedByDescending { it }.forEach { sb.append(it) }
+    bw.write(sb.toString())
+    bw.flush()
+    bw.close()
+    br.close()
 }
