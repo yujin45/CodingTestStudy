c0004677e8a8fc1cc18fa845792fdb40f3bec419
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-19 16:21:40 +0900

    [Gold V] Title: 강의실 배정, Time: 708 ms, Memory: 80148 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.kt
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.kt
@@ -1,29 +1,27 @@
 package org.example
 
-import java.util.StringTokenizer
 import java.util.PriorityQueue
+import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt()
-    // 회의실 배정 문제와 유사할 것 같음
-    // 끝나는 시간 -> 동일하면 시작 시간으로 우선 정렬하기
-    var st: StringTokenizer
-    var timeTable = Array(n) {
-        st = StringTokenizer(br.readLine())
-        (st.nextToken().toInt() to st.nextToken().toInt())
-    }.sortedBy { it.first }
-
-    val pq = PriorityQueue<Int>() // 끝나는 시간 기준 최소 힙
-
-    for ((start, end) in timeTable) {
-        // 현재 가장 빨리 끝나는 강의실이 새로운 강의 시작 전에 끝나면 재사용
-        if (pq.isNotEmpty() && pq.peek() <= start) {
-            pq.poll() // 제거하고 넣을 것
-        }
-        // 지금 강의 시작해야 하는데 아직 다 안 끝났다면 새 강의실에 새 강의 종료 시간 추가
-        pq.add(end)
+    val N = br.readLine().toInt()
+    val lecture = mutableListOf<Pair<Int, Int>>()
+    repeat(N) {
+        val st = StringTokenizer(br.readLine())
+        lecture.add(st.nextToken().toInt() to st.nextToken().toInt())
     }
-    println(pq.size)
+    lecture.sortBy { it.first } // 시작 시간 빠른 것으로
+    val room = PriorityQueue<Pair<Int, Int>>(compareBy { it.second }) // 빨리 끝나는 것을 우선으로
+    room.add(lecture[0])
+    for (i in 1 until N) {
+        val nowRoom = room.poll()
+        if (nowRoom.second > lecture[i].first) {// 시작 시간 보다 크면 다른 강의실 만들어야 함
+            room.add(nowRoom) // 재활용
+        }
+        room.add(lecture[i])
+    }
+    println(room.size)
+
     br.close()
 }
