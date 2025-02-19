79fef3487c13c515817de4ba0cf7126f2d2a7580
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-19 12:46:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1296 ms, Memory: 162020 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -1,18 +1,12 @@
-
+package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val sb = StringBuilder()
-    val n = br.readLine().toInt()
-    val list = ArrayList<Int>()
-
-    for(i in 0 until n) {
-        list.add(br.readLine().toInt())
-    }
-
-   list.sort()
-    for(num in list){
-        sb.appendLine(num)
-    }
-    println(sb.toString())
+    val bw = System.`out`.bufferedWriter()
+    val numbers = MutableList(br.readLine().toInt()){br.readLine().toInt()}
+    numbers.sort()
+    numbers.forEach{bw.write("$it\n")}
+    bw.flush()
+    bw.close()
+    br.close()
 }
