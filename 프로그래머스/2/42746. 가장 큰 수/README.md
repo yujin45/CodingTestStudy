ac2fc925f663f6b477036f88c1011cf032d83614
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-20 14:27:00 +0900

    [level 2] Title: 가장 큰 수, Time: 208.79 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 256 MB, 시간: 357.01 ms
+메모리: 100 MB, 시간: 208.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 03일 14:36:42
+2025년 02월 20일 14:26:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -1,15 +1,26 @@
+import java.util.Comparator
+
 class Solution {
     fun solution(numbers: IntArray): String {
-        // Step 1: 숫자를 문자열로 변환
-        val strNumbers = numbers.map { it.toString() }
+        // 1. 숫자 배열을 문자열로 변환
+        val strNumbers = mutableListOf<String>()
+        for(num in numbers){
+            strNumbers.add(num.toString())
+        }
 
-        // Step 2: 문자열을 3번 반복해 정렬 기준 생성
-        val sortedNumbers = strNumbers.sortedWith(compareByDescending { it.repeat(3) })
+        // 2. 정렬 기준 설정: (b+a) 가 (a+b)보다 크면 b가 앞으로 오도록 설정
+        // 3+30 > 30+3 : 이 경우 3 30 순서
+        val sortedNumbers = strNumbers.sortedWith(Comparator{
+            a, b ->
+            (b + a).compareTo(a+b) // 내림차순 정렬
+        })
 
-        // Step 3: 정렬된 문자열을 이어 붙임
-        val result = sortedNumbers.joinToString("")
-
-        // Step 4: 모든 숫자가 0일 경우 "0" 반환
-        return if (result.startsWith("0")) "0" else result
+        // 3. 정렬된 것 이어붙여 결과 생성
+        val sb = StringBuilder()
+        sortedNumbers.forEach{sb.append(it)}
+        val result = sb.toString()
+        //println(result)
+        // 4. "000" 같은 경우 "0"으로 변환
+        return if(result.startsWith("0")) "0" else result
     }
 }
