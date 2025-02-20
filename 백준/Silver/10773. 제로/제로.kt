4f674a68d8f1719768f99c2bfafc3c0106f96533
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-20 16:24:13 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 23004 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.kt
+++ 백준/Silver/10773. 제로/제로.kt
@@ -1,19 +1,17 @@
 
-import java.util.Stack
-
-fun main(){
+fun main() {
     val br = System.`in`.bufferedReader()
-    val stack = Stack<Int>()
-    val n = br.readLine().toInt()
-
-    for(i in 0 until n){
+    val K = br.readLine().toInt()
+    val stack = mutableListOf<Int>()
+    repeat(K) {
         val num = br.readLine().toInt()
-        if(num == 0){
-            stack.pop()
-        }else{
-            stack.push(num)
+        if (num == 0) {
+            stack.removeLast()
+        } else {
+            stack.add(num)
         }
     }
-
-    println(stack.sum())
+    var sum: Long = stack.fold(0) { acc, it -> acc + it }
+    print(sum)
+    br.close()
 }
