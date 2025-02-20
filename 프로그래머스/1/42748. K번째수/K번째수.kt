f473210379614aebc5e0aeb654057079812b2c63
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-20 14:04:07 +0900

    [level 1] Title: K번째수, Time: 24.45 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,11 +1,18 @@
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-        var answer = mutableListOf<Int>()
-        for(c in commands){
-            val (i, j, k) = c
-            val sliced = array.slice(i - 1 until j).sorted()
-            answer.add(sliced[k-1])
+        // 1. 인덱스 기준 i-1 ~ j-1까지 자르기
+        // 2. 1에서 나온 것 오름차순 정렬
+        // 3. 2에서 나온 배열의 인덱스 k-1 에 해당하는 숫자
+        val result = IntArray(commands.size)
+        for(cidx in 0 until commands.size){
+            val (i, j, k) = commands[cidx]
+            val temp = mutableListOf<Int>()
+            for(index in i-1..j-1){
+                temp.add(array[index])
+            }
+            result[cidx] = temp.sorted()[k-1]
         }
-        return answer.toIntArray()
+        //println(result.contentToString())
+        return result
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 66.9 MB, 시간: 26.62 ms
+메모리: 66.2 MB, 시간: 24.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 03일 13:52:01
+2025년 02월 20일 14:04:05
 
 ### 문제 설명
 
