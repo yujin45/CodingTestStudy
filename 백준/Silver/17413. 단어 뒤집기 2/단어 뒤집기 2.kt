82164269746bdc439316ebecc3a42c51d232d6a2
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-21 11:34:29 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 144 ms, Memory: 17520 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.kt
@@ -2,45 +2,29 @@ package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val sb = StringBuilder()
-    val userInput = br.readLine()
-//    var isOpen = false
-    var insideTag = false // 태그인지 아닌지만 판단
-    var temp = StringBuilder()
-    for (c in userInput) {
-        when {
-            c == '<' -> {
-                sb.append(temp.reverse()) // 단어 뒤집기
-                temp.clear()
-                sb.append(c) // 현재 기호 넣기
-                insideTag = true
-            }
-
-            c == '>' -> {
-                sb.append(c)
-                insideTag = false
-            }
-
-            insideTag -> {
-                // ★ 여기가 포인트!! 태그 내부의 문자는 그대로 출력!
-                // 그래서 when(c)로 하는게 아니라 when{}으로 하는 것!
-                sb.append(c)
-            }
-
-            c == ' ' -> {
-                sb.append(temp.reverse()) // 뒤집어서 넣기
-                temp.clear()
-                sb.append(c)
-            }
-
-            else -> {
-                temp.append(c)
-            }
+    val S = br.readLine()
+    val result = StringBuilder()
+    val temp = StringBuilder()
+    var isOpen = false
+    for (s in S) {
+        if (s == '<') {
+            result.append(temp.reverse())
+            temp.clear()
+            isOpen = true
+            temp.append(s)
+        } else if (s == '>') {
+            isOpen = false
+            temp.append(s)
+            result.append(temp)
+            temp.clear()
+        } else if (!isOpen && s == ' ') {
+            result.append(temp.reverse()).append(" ")
+            temp.clear()
+        } else {
+            temp.append(s)
         }
-
     }
-    sb.append(temp.reverse())
-    println(sb)
-
+    result.append(temp.reverse())
+    print(result)
     br.close()
 }
