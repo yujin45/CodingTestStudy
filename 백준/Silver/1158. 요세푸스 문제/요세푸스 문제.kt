840a90dc35d94ec5afab3c5346d91de8f29faafc
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-22 15:21:26 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 132 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.kt
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.kt
@@ -1,19 +1,27 @@
+package org.example
+
+import java.util.LinkedList
 
 fun main() {
     val br = System.`in`.bufferedReader()
+    val (N, K) = br.readLine().split(" ").map { it.toInt() }
     val sb = StringBuilder()
-    val result = mutableListOf<String>()
-    val (n, k) = br.readLine().split(" ").map{it.toInt()}
-    val people = mutableListOf<Int>()
-    repeat(n) {people.add(it+1)}
-   
-    var index = 0
     sb.append("<")
-    while(people.isNotEmpty()){
-        index = (index + k - 1) % people.size
-        result.add("${people.removeAt(index)}")
+    val queue = LinkedList<Int>()
+
+    for (i in 1..N) {
+        queue.addLast(i)
     }
-    sb.append(result.joinToString(", "))
-    sb.append(">")
-    print(sb)
+
+    var index = 0
+    var nextK = K - 1 // index 앞에 제거되어서 k-1
+    while (queue.size > 1) { // 0 나누지 않게
+        index = (index + nextK) % queue.size
+        sb.append(queue.removeAt(index)).append(", ")
+        // 중간 삭제가 있으므로 ArrayDeque보다 LinkedList
+    }
+    sb.append(queue[0]).append(">")
+    println(sb)
+
+    br.close()
 }
