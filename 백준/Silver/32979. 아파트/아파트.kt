f94c269e816dd34f1d1b10bb36aab750c9168df8
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-24 16:00:41 +0900

    [Silver V] Title: 아파트, Time: 104 ms, Memory: 13408 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.kt
+++ 백준/Silver/32979. 아파트/아파트.kt
@@ -1,17 +1,26 @@
 package org.example
 
-fun main(){
-    val br = System.`in`.bufferedReader()
+import java.util.StringTokenizer
 
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuilder()
     val n = br.readLine().toInt()
     val t = br.readLine().toInt()
-    val handsApartement = br.readLine().split(" ").map{it.toInt()}
-    val b_list = br.readLine().split(" ").map{it.toInt()}
-    var index = 0
-    val sb = StringBuilder()
-    for(b in b_list){
-        index = (index+(b-1)) % handsApartement.size
-        sb.append("${handsApartement[index]} ")
+    val st = StringTokenizer(br.readLine())
+    val hands = IntArray(2 * n)
+    repeat(2 * n) {
+        hands[it] = (st.nextToken().toInt())
     }
+    //println("입력된 손: 아래 -  $hands - 위")
+    val bs = StringTokenizer(br.readLine()) //t개의 b
+    var index = 0
+    repeat(t) {
+        val bIndex = bs.nextToken().toInt() - 1
+        index = (index + bIndex) % hands.size
+        sb.append(hands[index]).append(" ")
+    }
+    sb.deleteCharAt(sb.length - 1)
     println(sb)
+    br.close()
 }
