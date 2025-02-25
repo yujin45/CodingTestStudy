e5b10f709392f661ebd7364394ecbf7ce5f4feec
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 16:39:17 +0900

    [Silver I] Title: 영역 구하기, Time: 116 ms, Memory: 20796 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.kt
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.kt
@@ -1,52 +1,57 @@
 package org.example
 
+import java.util.StringTokenizer
 
-val dx = listOf(-1, 1, 0, 0)
-val dy = listOf(0, 0, -1, 1)
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (N, M, K) = br.readLine().split(" ").map { it.toInt() }
+    val graph = Array(M) { IntArray(N) }
+    repeat(K) {
+        val st = StringTokenizer(br.readLine())
+        val x1 = st.nextToken().toInt()
+        val y1 = st.nextToken().toInt()
+        val x2 = st.nextToken().toInt()
+        val y2 = st.nextToken().toInt()
+
+        for (i in x1 until x2) {
+            for (j in y1 until y2) {
+                //println("$i, $j")
+                graph[i][j] = 1 // 네모 영역 표시
+            }
+        }
+    }
+
+    val areaCount = mutableListOf<Int>()
+    for (i in graph.indices) {
+        for (j in graph[0].indices) {
+            if (graph[i][j] == 0) {
+                areaCount.add(dfs(graph, i, j))
+            }
+        }
+    }
+
+    areaCount.sort()
+    val sb = StringBuilder()
+    sb.append(areaCount.size).append("\n")
+    areaCount.forEach { sb.append(it).append(" ") }
+    sb.deleteCharAt(sb.length - 1)
+    println(sb)
+
+    br.close()
+}
 
 fun dfs(graph: Array<IntArray>, x: Int, y: Int): Int {
     var count = 1
-
-    graph[x][y] = 1 // 방문 처리
+    graph[x][y] = 2
     for (i in 0 until 4) {
         val nx = x + dx[i]
         val ny = y + dy[i]
-        if (nx in 0 until graph.size && ny in 0 until graph[0].size && graph[nx][ny] == 0) {
+        if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] == 0) {
             count += dfs(graph, nx, ny)
         }
     }
     return count
 }
-
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val sb = StringBuilder()
-    val (m, n, k) = br.readLine().split(" ").map { it.toInt() }
-    val graph = Array(m) { IntArray(n) }
-    repeat(k) {
-        val (x1, y1, x2, y2) = br.readLine().split(" ").map { it.toInt() }
-
-        for (i in y1 until y2) {
-            for (j in x1 until x2) {
-                graph[i][j] = 1
-            }
-        }
-    }
-
-    val resultPlace = mutableListOf<Int>()
-    for (i in 0 until graph.size) {
-        for (j in 0 until graph[0].size) {
-            if (graph[i][j] == 0) {
-                resultPlace.add(dfs(graph, i, j))
-            }
-        }
-    }
-    resultPlace.sort()
-    sb.append("${resultPlace.size}\n")
-    for (r in resultPlace) {
-        sb.append("${r} ")
-    }
-    println(sb)
-    br.close()
-}
-
