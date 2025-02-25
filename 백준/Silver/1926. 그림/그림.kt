42f34fc7026f59603666d948c21c56d182a58f47
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 18:24:01 +0900

    [Silver I] Title: 그림, Time: 356 ms, Memory: 79180 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.kt
+++ 백준/Silver/1926. 그림/그림.kt
@@ -1,41 +1,48 @@
 package org.example
 
-private val directions = arrayOf(
-    intArrayOf(-1, 0),
-    intArrayOf(1, 0),
-    intArrayOf(0, -1),
-    intArrayOf(0, 1),
-)
+import java.util.StringTokenizer
+import java.util.PriorityQueue
+
+// 상, 하, 좌, 우
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val graph = Array(n) { IntArray(m) }
+    for (i in 0 until n) {
+        val st = StringTokenizer(br.readLine())
+        for (j in 0 until m) {
+            graph[i][j] = st.nextToken().toInt()
+        }
+    }
+    val pq = PriorityQueue<Int>(compareByDescending { it })
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            if (graph[i][j] == 1) {
+                pq.add(dfs(graph, i, j))
+            }
+        }
+    }
+    if(pq.isEmpty()){
+        println("0\n0")
+    }else{
+        println(pq.size)
+        println(pq.poll())
+    }
+    br.close()
+}
 
 fun dfs(graph: Array<IntArray>, x: Int, y: Int): Int {
-    graph[x][y] = 2
     var count = 1
-    for ((dx, dy) in directions) {
-        val nx = x + dx
-        val ny = y + dy
+    graph[x][y] = 0
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
         if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] == 1) {
             count += dfs(graph, nx, ny)
         }
     }
     return count
-}
-
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val graph = Array(n) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
-    var resultMax = 0
-    var resultCount = 0
-
-    for (i in 0 until n) {
-        for (j in 0 until m) {
-            if (graph[i][j] == 1) {
-                resultMax = maxOf(dfs(graph, i, j), resultMax)
-                resultCount++
-            }
-        }
-    }
-    println("${resultCount}\n${resultMax}")
-
-    br.close()
 }
