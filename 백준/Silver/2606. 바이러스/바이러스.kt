3c88641e765abc1890fc42103e5a97e630813809
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 14:36:11 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 12424 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.kt
+++ 백준/Silver/2606. 바이러스/바이러스.kt
@@ -4,17 +4,18 @@ import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val computerNum = br.readLine().toInt()
-    val edge = br.readLine().toInt()
+    val computerNum = br.readLine().toInt() // 1~100
     val graph = Array(computerNum + 1) { mutableListOf<Int>() }
-    repeat(edge) {
+    val edgeNum = br.readLine().toInt()
+    repeat(edgeNum) {
         val st = StringTokenizer(br.readLine())
         val a = st.nextToken().toInt()
         val b = st.nextToken().toInt()
-        // 양방향 서로 연결
         graph[a].add(b)
         graph[b].add(a)
     }
+
+    // 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수
     val visited = BooleanArray(computerNum + 1)
     println(dfs(graph, 1, visited) - 1)
 
@@ -22,12 +23,14 @@ fun main() {
 }
 
 fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: BooleanArray): Int {
-    visited[v] = true
     var count = 1
+    visited[v] = true
+
     for (neighbor in graph[v]) {
         if (!visited[neighbor]) {
             count += dfs(graph, neighbor, visited)
         }
     }
+
     return count
 }
