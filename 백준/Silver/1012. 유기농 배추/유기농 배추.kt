d7e495e63af526cf37455947437666b248c99bd6
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 14:52:27 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 20628 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.kt
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.kt
@@ -1,56 +1,43 @@
 package org.example
 
+import java.util.StringTokenizer
+
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
 
 fun main() {
-
-    // 전역으로 두고 쓰려고 함
-    val dx = listOf(-1, 1, 0, 0)
-    val dy = listOf(0, 0, -1, 1)
-
-    fun dfs(graph: Array<IntArray>, x: Int, y: Int) {
-        graph[x][y] = 2 // 방문처리
-        for (i in 0 until 4) {
-            val nx = x + dx[i]
-            val ny = y + dy[i]
-            if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] == 1) {
-                //println("${graph.indices} ${graph[0].indices}")
-                dfs(graph, nx, ny)
-            }
-        }
-    }
-
-
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val bw = System.out.bufferedWriter()
-
-    val t = br.readLine().toInt()
-
-
-    repeat(t) {
-        val (m, n, k) = br.readLine().split(" ").map { it.toInt() }
-
-        val graph = Array(n) { IntArray(m) }
-        repeat(k) {
-            val (x, y) = br.readLine().split(" ").map { it.toInt() }
-            graph[y][x] = 1
+    val T = br.readLine().toInt()
+    repeat(T) {
+        val (M, N, K) = br.readLine().split(" ").map { it.toInt() }
+        val graph = Array(M) { BooleanArray(N) }
+        repeat(K) {
+            val st = StringTokenizer(br.readLine())
+            graph[st.nextToken().toInt()][st.nextToken().toInt()] = true
         }
-
-        var totalCount = 0
-        for (i in 0 until n) {
-            for (j in 0 until m) {
-                if (graph[i][j] == 1) {
-                    totalCount++
+        var count = 0
+        for (i in graph.indices) {
+            for (j in graph[0].indices) {
+                if (graph[i][j]) {
                     dfs(graph, i, j)
+                    count++
                 }
             }
         }
-        sb.append("$totalCount\n")
+        sb.append(count).append("\n")
     }
-
-    bw.write(sb.toString())
-    bw.flush()
-    bw.close()
+    print(sb)
     br.close()
+}
 
+fun dfs(graph: Array<BooleanArray>, x: Int, y: Int) {
+    graph[x][y] = false // 방문처리
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+        if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny]) {
+            dfs(graph, nx, ny)
+        }
+    }
 }
