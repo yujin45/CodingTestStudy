7678012e770bb52abe3c8c70e6800f95b4906431
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 17:05:26 +0900

    [Silver I] Title: 안전 영역, Time: 220 ms, Memory: 20268 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.kt
+++ 백준/Silver/2468. 안전 영역/안전 영역.kt
@@ -1,47 +1,59 @@
 package org.example
 
-private val directions = arrayOf(
-    intArrayOf(-1, 0),
-    intArrayOf(0, 1),
-    intArrayOf(1, 0),
-    intArrayOf(0, -1)
-)
+import java.util.StringTokenizer
 
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
 
-fun dfs(graph: Array<IntArray>, x: Int, y: Int, visited: Array<BooleanArray>, h: Int) {
-    if (!(x in 0 until graph.size) || !(y in 0 until graph[0].size) || visited[x][y] || graph[x][y] <= h) {
-        return
-    }
-    visited[x][y] = true
-    for (i in 0 until 4) {
-        val nx = x + directions[i][0]
-        val ny = y + directions[i][1]
-        dfs(graph, nx, ny, visited, h)
-    }
-}
-
+// 일정 높이 이하의 모든 지점은 물에 잠긴다.
+// 안전영역의 개수 중 최대 구하기
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt()
-    val graph = Array(n) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
-    val max_height = graph.flatMap { it.toList() }.max()
-    //println("Min: $min_height, Max: $max_height")
-    var ret = 1
-    for (h in 1 until max_height) {
-        val visited = Array(n) { BooleanArray(n) }
+    val N = br.readLine().toInt()
+    val graphOrigin = Array(N) { IntArray(N) }
+    var maxHeight = Int.MIN_VALUE
+    var minHeight = Int.MAX_VALUE
+    for (i in 0 until N) {
+        val st = StringTokenizer(br.readLine())
+        for (j in 0 until N) {
+            val h = st.nextToken().toInt()
+            maxHeight = maxOf(maxHeight, h)
+            minHeight = minOf(minHeight, h)
+            graphOrigin[i][j] = h
+        }
+    }
+
+    // 높이가 min ~ max인 상태
+    var result = 1 // 아무 영역도 물에 잠기지 않을 수 있다..
+    for (h in minHeight..maxHeight) {
+        val graph = Array(graphOrigin.size) { idx ->
+            graphOrigin[idx].copyOf()
+        }
         var count = 0
-        for (i in 0 until n) {
-            for (j in 0 until n) {
-                if (!visited[i][j] && graph[i][j] > h) {
-                    dfs(graph, i, j, visited, h)
+        for (i in graph.indices) {
+            for (j in graph[0].indices) {
+                if (graph[i][j] > h) {
+                    // 안전 영역일 때
+                    dfs(graph, i, j, h)
                     count++
-                    //println("h: $h - i : $i - j : $j - count : $count")
                 }
             }
         }
-        ret = maxOf(ret, count)
+        result = maxOf(result, count)
     }
-    println("$ret")
+
+    println(result)
+
     br.close()
 }
 
+fun dfs(graph: Array<IntArray>, x: Int, y: Int, h: Int) {
+    graph[x][y] = -1 // 방문 표시
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+        if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] > h) {
+            dfs(graph, nx, ny, h)
+        }
+    }
+}
