9aad666baf65a1e1e6cfa01ff13cf63108994669
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-25 21:09:28 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 13228 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.kt
+++ 백준/Gold/10026. 적록색약/적록색약.kt
@@ -1,55 +1,45 @@
 package org.example
 
-val dx = intArrayOf(-1, 1, 0, 0)
-val dy = intArrayOf(0, 0, -1, 1)
-
-
-fun dfs(graph: Array<CharArray>, x: Int, y: Int, color: String, visited: Array<BooleanArray>) {
-    visited[x][y] = true
-    for (i in 0..3) {
-        val nx = x + dx[i]
-        val ny = y + dy[i]
-        if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] in color && !visited[nx][ny]) {
-            visited[nx][ny] = true
-            dfs(graph, nx, ny, color, visited)
-        }
-    }
-
-}
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt()
-    val graph = Array(n) { br.readLine().toCharArray() }
-    var visited = Array(n) { BooleanArray(n) }
-    var normalCount = 0
-    var rgCount = 0
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            if (!visited[i][j]) {
-                if (graph[i][j] == 'R') {
-                    dfs(graph, i, j, "R", visited)
-                } else if (graph[i][j] == 'G') {
-                    dfs(graph, i, j, "G", visited)
-                } else if (graph[i][j] == 'B') {
-                    dfs(graph, i, j, "B", visited)
-                }
-                normalCount++
+    // RGB - 빨초 / 파
+    // 구역 수
+    val N = br.readLine().toInt()
+    val graph = Array(N) { br.readLine().toCharArray() }
+    val graphRG = Array(N) { idx -> graph[idx].copyOf() }
+    //
+    var totalRGB = 0
+    var totalRG = 0
+    for (i in 0 until N) {
+        for (j in 0 until N) {
+            if (graph[i][j] != '#') {
+                dfs(graph, i, j, graph[i][j], graph[i][j])
+                totalRGB++
             }
-        }
-    }
-    visited = Array(n) { BooleanArray(n) }
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            if (!visited[i][j]) {
-                if (graph[i][j] == 'B') {
-                    dfs(graph, i, j, "B", visited)
+            if (graphRG[i][j] != '#') {
+                if (graphRG[i][j] == 'B') {
+                    dfs(graphRG, i, j, 'B', 'B')
                 } else {
-                    dfs(graph, i, j, "RG", visited)
+                    dfs(graphRG, i, j, 'R', 'G')
                 }
-                rgCount++
+                totalRG++
             }
         }
     }
-    println("$normalCount $rgCount")
+    println("$totalRGB $totalRG")
+    br.close()
+}
+
+fun dfs(graph: Array<CharArray>, x: Int, y: Int, target1: Char, target2: Char) {
+    graph[x][y] = '#'
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+        if (nx in graph.indices && ny in graph[0].indices && (graph[nx][ny] == target1 || graph[nx][ny] == target2)) {
+            dfs(graph, nx, ny, target1, target2)
+        }
+    }
 }
