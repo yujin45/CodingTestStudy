518d0bc561ab3993cd7c57f3374f8b3b533a07c8
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-27 21:42:32 +0900

    [Silver V] Title: 날짜 계산, Time: 112 ms, Memory: 21168 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.kt
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.kt
@@ -1,13 +1,14 @@
 package org.example
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (e, s, m) = br.readLine().split(" ").map { it.toInt() }
+    val (E, S, M) = br.readLine().split(" ").map { it.toInt() }
     var year = 1
     while (true) {
-        if ((year - e) % 15 == 0 && (year - s) % 28 == 0 && (year - m) % 19 == 0) {
-            println("$year")
-            break
+        if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0) {
+            println(year)
+            return
         }
         year++
     }
