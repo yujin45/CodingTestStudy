77cdc839f1e2edfd9f2491d4a3503459bbdfbaaa
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-27 21:17:20 +0900

    [Silver IV] Title: 토너먼트, Time: 112 ms, Memory: 21200 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.kt
+++ 백준/Silver/1057. 토너먼트/토너먼트.kt
@@ -1,20 +1,17 @@
 package org.example
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, kim, lim) = br.readLine().split(" ").map { it.toInt() }
-    println(tornament(kim, lim))
-    br.close()
-}
-
-fun tornament(kim: Int, lim: Int): Int {
-    var roundCount = 0
-    var kim = kim
-    var lim = lim
-    while (kim != lim) {
-        kim = (kim + 1) / 2
-        lim = (lim + 1) / 2
-        roundCount++
+    var (N, a, b) = br.readLine().split(" ").map { it.toInt() }
+    // 서로 번호가 같아지면 경쟁하는 라운드
+    // 다음 라운드 번호는 (현재 i + 1) /2 로 결정됨
+    var count = 0
+    while (a != b) {
+        a = (a + 1) / 2
+        b = (b + 1) / 2
+        count++
     }
-    return roundCount
+    println(count)
+    br.close()
 }
