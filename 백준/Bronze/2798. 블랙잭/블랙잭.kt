5b521cbb718160e8f71c9104ba135546fe585b3d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-27 18:30:48 +0900

    [Bronze II] Title: 블랙잭, Time: 140 ms, Memory: 20628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.kt
+++ 백준/Bronze/2798. 블랙잭/블랙잭.kt
@@ -3,19 +3,36 @@ package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+
+    // 카드 합이 21을 넘지 않는 한도 내에서 카드의 합을 최대한 크게 만드는 게임
+    // n개의 카드 중 3장 고르기
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
     val cards = br.readLine().split(" ").map { it.toInt() }
-    var ret = 0
-    for (i in 0 until n - 2) {
-        for (j in i + 1 until n - 1) {
-            for (k in j + 1 until n) {
-                val currentSum = cards[i] + cards[j] + cards[k]
-                if (currentSum <= m) {
-                    ret = maxOf(ret, currentSum)
-                }
+    val visited = BooleanArray(N) //N개의 카드 방문 여부
+    val sequence = IntArray(3) // 3개 들어갈 것
+    var totalMax = Int.MIN_VALUE
+    fun backtrack(depth: Int) {
+        if (depth == 3) {
+            // 3개 뽑으면 확인하기
+            val total = sequence[0] + sequence[1] + sequence[2]
+            if (total <= M) {
+                totalMax = maxOf(totalMax, total)
             }
+            return
+        }
+
+        for (i in cards.indices) {
+            if (!visited[i]) {
+                visited[i] = true
+                sequence[depth] = cards[i]
+                backtrack(depth + 1)
+                visited[i] = false
+            }
+
         }
     }
-    println(ret)
+
+    backtrack(0)
+    println(totalMax)
     br.close()
 }
