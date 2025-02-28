0551ef648d5956fe26e97b0921e7db714d55e102
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-28 16:36:45 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.kt
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.kt
@@ -1,28 +1,33 @@
 package org.example
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
-
     val n = br.readLine().toInt()
-    val table = IntArray(n + 1)
-    table[0] = 0
+    val p = IntArray(n + 1) { 0 } // 계단 점수 저장
+
     for (i in 1..n) {
-        table[i] = br.readLine().toInt()
+        p[i] = br.readLine().toInt()
     }
-    val dp = IntArray(n + 1)
+
     if (n == 1) {
-        println(table[1])
+        println(p[1])
+        return
     } else if (n == 2) {
-        println(table[2] + table[1])
-    } else {
-        dp[1] = table[1]
-        dp[2] = table[2] + table[1]
-        dp[3] = maxOf(table[3] + table[1], table[3] + table[2])
-        for (i in 4..n) {
-            dp[i] = maxOf(dp[i - 2], dp[i - 3] + table[i - 1]) + table[i]
-        }
-        println(dp[n])
+        println(p[1] + p[2])
+        return
     }
+
+    val dp = IntArray(n + 1) { 0 }
+    dp[1] = p[1]
+    dp[2] = p[1] + p[2]
+    dp[3] = maxOf(p[1] + p[3], p[2] + p[3]) // 1, 3인 경우 2, 3인 경우
+
+    for (i in 4..n) {
+        // i-3, i-2, i-1, i 있을 때 현재 i 이전을 밟았는지 안 밟았는지에 따라
+        // O X O 인 경우와 O X 인 경우
+        dp[i] = maxOf(dp[i - 3] + p[i - 1], dp[i - 2]) + p[i]
+    }
+    println(dp[n])
     br.close()
 }
-
