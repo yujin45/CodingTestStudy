f0374336ea3bc0f41f07e4a332b8a8f185562d50
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-28 14:49:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.kt
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.kt
@@ -1,24 +1,23 @@
 package org.example
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    var n = br.readLine().toInt()
-    var count = 0
-    var sucess = false
-    while (n >= 0) {
-        if (n % 5 == 0) {
-            count += n / 5
-            println(count)
-            sucess = true
-            break
-        }
-        n -= 3
-        count++
-    }
-    if (!sucess) {
-        println(-1)
-    }
+    val N = br.readLine().toInt()
+    // 5a + 3b = N
+    // a+b가 최소가 되도록 해라
+    var x = N / 5 // 5kg 봉지 개수 최대로 사용
 
+    while (x >= 0) {
+        val remaining = N - (x * 5) // 남은 무게
+        if (remaining % 3 == 0) {
+            // 3kg봉지로 정확히 나눌 수 있으면
+            val y = remaining / 3
+            println(x + y)
+            return
+        }
+        x-- // 5kg 봉지 줄이고 다시 시도
+    }
+    println(-1) // 못나누는 경우
     br.close()
 }
-
