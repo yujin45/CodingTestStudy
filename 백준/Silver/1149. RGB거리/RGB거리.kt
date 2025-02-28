6a6873ee7b6a992adc89797ec20bc1aa007b2a4f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-28 18:43:36 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 19016 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.kt
+++ 백준/Silver/1149. RGB거리/RGB거리.kt
@@ -1,28 +1,22 @@
 package org.example
 
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val n = br.readLine().toInt()
-    val userInput = Array<IntArray>(n) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
-    val dp = Array<IntArray>(n) { IntArray(3) }
 
-    for (i in 0..2) {
-        dp[0][i] = userInput[0][i]
-    }
-
-    for (i in 1 until n) {
-        for (j in 0 until 3) {
-            if (j == 0) {
-                dp[i][j] = minOf(dp[i - 1][1], dp[i - 1][2]) + userInput[i][j]
-            } else if (j == 1) {
-                dp[i][j] = minOf(dp[i - 1][0], dp[i - 1][2]) + userInput[i][j]
-            } else if (j == 2) {
-                dp[i][j] = minOf(dp[i - 1][0], dp[i - 1][1]) + userInput[i][j]
-            }
-        }
+    val dp = Array(n + 1) { IntArray(3) }
+    for (i in 1..n) {
+        val st = StringTokenizer(br.readLine())
+        val r = st.nextToken().toInt()
+        val g = st.nextToken().toInt()
+        val b = st.nextToken().toInt()
+        dp[i][0] = minOf(dp[i - 1][1], dp[i - 1][2]) + r
+        dp[i][1] = minOf(dp[i - 1][0], dp[i - 1][2]) + g
+        dp[i][2] = minOf(dp[i - 1][0], dp[i - 1][1]) + b
     }
     //println(dp.contentDeepToString())
-    println(dp[n - 1].min())
+    println(dp[n].min())
     br.close()
 }
-
