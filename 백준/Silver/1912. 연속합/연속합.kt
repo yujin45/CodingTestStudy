cec3e32b809a91e316a9fae6bc3fcf1621702be9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-28 17:03:25 +0900

    [Silver II] Title: 연속합, Time: 176 ms, Memory: 22280 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.kt
+++ 백준/Silver/1912. 연속합/연속합.kt
@@ -1,17 +1,23 @@
 package org.example
 
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val n = br.readLine().toInt()
-    val userInput = br.readLine().split(" ").map { it.toInt() }
-    val dp = IntArray(n)
-    dp[0] = userInput[0]
-    var maxNum = dp[0]
-    for (i in 1 until n) {
-        dp[i] = maxOf(dp[i - 1] + userInput[i], userInput[i])
-        maxNum = maxOf(maxNum, dp[i])
+    //val numbers = br.readLine().split(" ").map{it.toInt()}
+    val dp = IntArray(n + 1)
+    val st = StringTokenizer(br.readLine())
+
+    var maxsum = Int.MIN_VALUE
+    for (i in 1..n) {
+        val num = st.nextToken().toInt()
+        // 자기 자신과 비교해서 큰 거 가져가야 함
+        //dp[i] = if (num + dp[i - 1] >= 0) num + dp[i - 1] else num
+        dp[i] = maxOf(num + dp[i - 1], num)
+        maxsum = maxOf(maxsum, dp[i])
     }
-    println(maxNum)
+    //println(dp.contentToString())
+    println(maxsum)
     br.close()
 }
-
