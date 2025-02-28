025c5d6a51a2618aa19f1ed210f88d111f6c2926
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-02-28 15:06:48 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -1,22 +1,26 @@
 package org.example
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val t = br.readLine().toInt()
+    val T = br.readLine().toInt()
     val sb = StringBuilder()
-    val dp = IntArray(11)
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
-    for (i in 4..10) {
+    val dp = IntArray(12) // n최대 11까지 -> 12로 크기 선언
+    // 1, 2, 3을 만들 수 있는 개수 초기값
+    dp[1] = 1 // 1
+    dp[2] = 2 // 1+1, 2
+    dp[3] = 4 // 1+1+1, 1+2, 2+1, 3
+
+    for (i in 4..11) {
+        // dp[n]: n을 1, 2, 3의 합으로 나타내는 방법의 수
+        // n을 만들기 위해, n-1, n-2, n-3에서 1, 2, 3을 더한 경우의 수를 더함
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
     }
 
-    repeat(t) {
-        val n = br.readLine().toInt()
-        sb.append("${dp[n]}\n")
+    repeat(T) {
+        val num = br.readLine().toInt()
+        sb.append(dp[num]).append("\n")
     }
     print(sb)
     br.close()
 }
-
