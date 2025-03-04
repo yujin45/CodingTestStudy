2271fc3613e4a71860aaf287f761d6d8585e3389
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 10:23:38 +0900

    [Silver III] Title: N과 M (6), Time: 132 ms, Memory: 24112 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.kt
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.kt
@@ -3,25 +3,24 @@ package org.example
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    // 조합
-    val numbers = br.readLine().split(" ").map { it.toInt() }.sorted()
-
-    val sequence = IntArray(m)
-
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    val nums = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val sequence = IntArray(M)
     fun backtrack(depth: Int, start: Int) {
-        if (depth == m) {
-            sequence.forEach { sb.append("$it ") }
-            sb.append("\n")
+        if (depth == M) {
+            sequence.forEach { sb.append(it).append(" ") }
+            sb.deleteCharAt(sb.length - 1).append("\n")
             return
         }
-        for (i in start until numbers.size) {
-            sequence[depth] = numbers[i]
+
+        for (i in start until N) {
+            sequence[depth] = nums[i]
             backtrack(depth + 1, i + 1)
         }
+
     }
     backtrack(0, 0)
-    //
     print(sb)
+
     br.close()
 }
