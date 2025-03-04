6ab12f10e0d1b2c93d61175ab5b999746191d041
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 09:47:44 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 20764 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
@@ -3,23 +3,34 @@ package org.example
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    // 중복 X, 1~n 중 m개 선택 - 조합, 오름차순
-    val sequence = IntArray(m)
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    // 1~N까지 자연수 중 중복 없이 M개 구하기
+    val sequence = IntArray(M) // M개 고를 배열
+    val visited = BooleanArray(N + 1) // 중복 없이, 0은 안 씀
 
     fun backtrack(depth: Int, start: Int) {
-        if (depth == m) {
-            sequence.forEach { sb.append("$it ") }
+        if (depth == M) {
+            sequence.forEach {
+                sb.append(it).append(" ")
+            }
+            sb.deleteCharAt(sb.length - 1)
             sb.append("\n")
             return
         }
-        for (i in start..n) {
-            sequence[depth] = i
-            backtrack(depth + 1, i + 1)
+
+        for (i in start..N) {
+            // 1~N까지의 수인데 고른 수열이 오름차순이 되어야 함.
+            if (!visited[i]) {
+                // 방문하지 않은 수라면
+                visited[i] = true
+                sequence[depth] = i
+                backtrack(depth + 1, i + 1)
+                visited[i] = false
+            }
         }
     }
     backtrack(0, 1)
-    //
     print(sb)
+
     br.close()
 }
