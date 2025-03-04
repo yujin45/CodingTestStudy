5a48b7618ed3b7213b0c51a19771063ba400bdeb
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 10:04:43 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 20972 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.kt
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.kt
@@ -1,24 +1,31 @@
 package org.example
 
+// 조합
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val sequence = IntArray(m)
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    // 1~N까지 자연수 중 중복 없이 M개 구하기
+    val sequence = IntArray(M) // M개 고를 배열
 
     fun backtrack(depth: Int, start: Int) {
-        if (depth == m) {
-            sequence.forEach { sb.append("$it ") }
+        if (depth == M) {
+            sequence.forEach {
+                sb.append(it).append(" ")
+            }
+            sb.deleteCharAt(sb.length - 1)
             sb.append("\n")
             return
         }
-        for (i in start..n) {
-            // i부터 탐색하고 중복 가능, 비내림차순 = 오름차순
+
+        for (i in start..N) {
+            // 1~N까지의 수인데 고른 수열이 오름차순이 되어야 함. 같은 수 골라도 됨 = visited필요x
             sequence[depth] = i
-            backtrack(depth + 1, i) // 같은 숫자 i도 다시 선택 가능
+            backtrack(depth + 1, i) // 그리고 같은 수 골라도 되니까 i+1로 다음 수부터 고르게 안해도 됨
         }
     }
     backtrack(0, 1)
     print(sb)
+
     br.close()
 }
