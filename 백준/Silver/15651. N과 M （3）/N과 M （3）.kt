91bf2e51c3b3f193c439e3cb48a1d6bbcc3e8837
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 09:53:21 +0900

    [Silver III] Title: N과 M (3), Time: 480 ms, Memory: 121668 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.kt
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.kt
@@ -2,11 +2,9 @@ package org.example
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val sb = StringBuilder()
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
-    // 1~N까지 자연수 중 중복 없이 M개 구하기
-    val sequence = IntArray(M) // M개 고를 배열
-
+    val sb = StringBuilder()
+    val sequence = IntArray(M)
     fun backtrack(depth: Int) {
         if (depth == M) {
             sequence.forEach {
@@ -16,9 +14,7 @@ fun main() {
             sb.append("\n")
             return
         }
-
         for (i in 1..N) {
-            // 같은 수를 여러번 골라도 됨 -> visited 필요 X
             sequence[depth] = i
             backtrack(depth + 1)
         }
