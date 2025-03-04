9eada81410a67383127d9f9e7fe885b072ae3302
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 10:16:38 +0900

    [Silver III] Title: N과 M (5), Time: 272 ms, Memory: 36900 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.kt
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.kt
@@ -3,29 +3,29 @@ package org.example
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    // 순열
-    val numbers = br.readLine().split(" ").map { it.toInt() }.sorted()
-
-    val sequence = IntArray(m)
-    val visited = BooleanArray(n)
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    val nums = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val visited = BooleanArray(N)
+    val sequence = IntArray(M)
     fun backtrack(depth: Int) {
-        if (depth == m) {
-            sequence.forEach { sb.append("$it ") }
-            sb.append("\n")
+        if (depth == M) {
+            sequence.forEach { sb.append(it).append(" ") }
+            sb.deleteCharAt(sb.length - 1).append("\n")
             return
         }
-        for (i in numbers.indices) {
+
+        for (i in nums.indices) {
             if (!visited[i]) {
                 visited[i] = true
-                sequence[depth] = numbers[i]
+                sequence[depth] = nums[i]
                 backtrack(depth + 1)
                 visited[i] = false
             }
         }
+
     }
     backtrack(0)
-    //
     print(sb)
+
     br.close()
 }
