be2703148fee858775746e223b445fbf657313e9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-04 11:13:21 +0900

    [Silver II] Title: 부분수열의 합, Time: 120 ms, Memory: 21000 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
@@ -1,22 +1,23 @@
 package org.example
 
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    // 조합을 다 구한다
-    val (n, s) = br.readLine().split(" ").map { it.toInt() }
-    val numbers = br.readLine().split(" ").map { it.toInt() }
-    var result = 0
-
-    fun backtrack(depth: Int, start: Int, sum: Int) {
-        if (depth > 0 && sum == s) { //공집합 제외
-            result++
+    val (N, S) = br.readLine().split(" ").map { it.toInt() }
+    val st = StringTokenizer(br.readLine())
+    val numbers = IntArray(N) { st.nextToken().toInt() }
+    var count = 0
+    fun backtrack(depth: Int, start: Int, currentSum: Int) {
+        if (depth > 0 && currentSum == S) {
+            count++
         }
-        for (i in start until n) {
-            backtrack(depth + 1, i + 1, sum + numbers[i]) // 합을 체크하면서 감
+        // 조합으로 구해야 하는데, 수열에 1, 1 이렇게 중복된 수 있을 수 있는데 이거 허용
+        for (i in start until N) { // 자기 자신 포함 x로 i+1 넘기기
+            backtrack(depth + 1, i + 1, currentSum + numbers[i])
         }
     }
     backtrack(0, 0, 0)
-
-    println(result)
+    println(count)
     br.close()
 }
