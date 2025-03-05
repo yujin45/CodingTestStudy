09a1bb7dc88726e6659754c26beab76b888df1bc
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-05 16:59:40 +0900

    [Silver IV] Title: 접미사 배열, Time: 116 ms, Memory: 18860 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.kt
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.kt
@@ -1,15 +1,18 @@
 package org.example
 
+import java.util.PriorityQueue
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val sb = StringBuilder()
     val S = br.readLine()
-    val result = mutableListOf<String>()
+    val pq = PriorityQueue<String>()
     for (i in S.indices) {
-        result.add(S.substring(i))
+        pq.add(S.substring(i))
     }
-    result.sort()
-    result.forEach { sb.append(it).append("\n") }
+    val sb = StringBuilder()
+    while (pq.isNotEmpty()) {
+        sb.append(pq.poll()).append("\n")
+    }
     print(sb)
     br.close()
 }
