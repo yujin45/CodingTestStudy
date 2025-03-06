c023452db1e88e9901a5075d972e7a48bc9814b5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-06 10:36:34 +0900

    [Gold V] Title: 행복 유치원, Time: 572 ms, Memory: 67504 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.kt
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.kt
@@ -1,21 +1,29 @@
 package org.example
 
+import java.util.StringTokenizer
+import java.util.PriorityQueue
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, K) = br.readLine().split(" ").map { it.toInt() }
-    val height = br.readLine().split(" ").map { it.toInt() }
-    // 이미 순서대로 줄 세움.
-    val diff = IntArray(N - 1)
-    // 1. 키 차이 뱅려
-    for (i in 0 until N - 1) {
-        diff[i] = (height[i + 1] - height[i])
+    // N이 3백만
+    val st = StringTokenizer(br.readLine())
+    val pq = PriorityQueue<Int>(compareByDescending { it })
+    var front = st.nextToken().toInt()
+    repeat(N - 1) {
+        val back = st.nextToken().toInt()
+        pq.add(back - front)
+        front = back
     }
-    // 2. 키 차이 정렬
-    diff.sort()
-
-    // 3. N-K만큼 합산 (N-1개 제거)
-    val minCost = diff.take(N-K).sum()
-    println(minCost)
+    // 차이 내림 차순 중 가장 큰 K-1개를 선택해서 끊어주기
+    repeat(K - 1) {
+        pq.poll()
+    }
+    var sum = 0
+    while (pq.isNotEmpty()) {
+        sum += pq.poll()
+    }
+    println(sum)
 
     br.close()
 }
