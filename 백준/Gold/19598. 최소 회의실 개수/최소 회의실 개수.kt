e8a279d8092bef52f02ff418af0c33ac39b7c8c8
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-06 14:11:36 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 600 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.kt
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.kt
@@ -3,22 +3,24 @@ package org.example
 import java.util.StringTokenizer
 import java.util.PriorityQueue
 
-fun main(args: Array<String>) {
+fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt()
-    val pq = PriorityQueue<Int>() // 회의 끝나는 시간 체크용
-    var st: StringTokenizer
-    val timeTable = Array(n) {
-        st = StringTokenizer(br.readLine())
-        (st.nextToken().toInt() to st.nextToken().toInt())
-    }.sortedBy { it.first } // 회의 시간 빠른 것을 먼저
-
-    for ((start, end) in timeTable) {
-        if (pq.isNotEmpty() && pq.peek() <= start) {
-            pq.poll()
-        }
-        pq.add(end)
+    val N = br.readLine().toInt()
+    val meetings = PriorityQueue<Pair<Int, Int>>(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })
+    repeat(N) {
+        val st = StringTokenizer(br.readLine())
+        meetings.add(st.nextToken().toInt() to st.nextToken().toInt())
     }
-    println(pq.size)
+    val room = PriorityQueue<Int>()
+    room.add(meetings.poll().second)
+    while (meetings.isNotEmpty()) {
+        val (start, end) = meetings.poll()
+        val curRoomEnd = room.poll()
+        if (start < curRoomEnd) {
+            room.add(curRoomEnd)
+        }
+        room.add(end)
+    }
+    println(room.size)
     br.close()
 }
