bde70f469a7ea92ea9aa5c0a75e06569b4142c8d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-06 14:06:14 +0900

    [Gold V] Title: 강의실, Time: 620 ms, Memory: 58832 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.kt
+++ 백준/Gold/1374. 강의실/강의실.kt
@@ -5,21 +5,26 @@ import java.util.PriorityQueue
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt()
-    val pq = PriorityQueue<Int>()
-    var st: StringTokenizer
-    val timeTable = Array(n) {
-        st = StringTokenizer(br.readLine())
-        st.nextToken() // 넘버 필요 없음
-        (st.nextToken().toInt() to st.nextToken().toInt())
-    }.sortedBy { it.first }
-    for ((start, end) in timeTable) {
-        if (pq.isNotEmpty() && pq.peek() <= start) {
-            // 재사용 가능
-            pq.poll()
-        }
-        pq.add(end)
+    val N = br.readLine().toInt()
+    // 최대한 적은 수 => 강의실 배정 문제
+    // 강의실이 여러개이기 때문에 시작 시간 빠른 것 우선으로 넣어두고 이후 종료시간 빠른 순
+    val lectures = PriorityQueue<Pair<Int, Int>>(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })
+    val room = PriorityQueue<Int>()
+    repeat(N) {
+        val st = StringTokenizer(br.readLine())
+        st.nextToken() // 강의 번호는 필요 없음
+        lectures.add(st.nextToken().toInt() to st.nextToken().toInt())
     }
-    println(pq.size)
+
+    room.add(lectures.poll().second)
+    while (lectures.isNotEmpty()) {
+        val (start, end) = lectures.poll()
+        val curRoomEnd = room.poll()
+        if (start < curRoomEnd) {
+            room.add(curRoomEnd) // 새 강의실 오픈이 필요할 때 기존 다시 넣기
+        }
+        room.add(end)
+    }
+    println(room.size)
     br.close()
 }
