2e4b41a6be7af8b249e037e16fb2ade44f9f4d98
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-07 16:49:31 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 424 ms, Memory: 50472 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.kt
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.kt
@@ -3,40 +3,38 @@ package org.example
 import java.util.StringTokenizer
 import kotlin.math.absoluteValue
 
-fun main(args: Array<String>) {
+fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
     val t = br.readLine().toInt()
     repeat(t) {
         val n = br.readLine().toInt()
-
         val numbers = IntArray(n)
         val st = StringTokenizer(br.readLine())
         for (i in 0 until n) {
             numbers[i] = st.nextToken().toInt()
         }
-        numbers.sort()
-        //println(numbers.contentToString())
-        val makeNumbers = IntArray(n)
+        numbers.sort() // 오름차순 정렬
+
+        val makeNumbers = IntArray(n) // 가장 차이 적게 배치하는 것
         var index = n - 1
         for (i in 0 until n) {
             if (i % 2 == 0) {
+                // 짝수면 앞에서부터 채워넣음
                 makeNumbers[i / 2] = numbers[i]
             } else {
                 makeNumbers[index] = numbers[i]
-                index--
+                index-- // 홀수면 뒤에서부터 채워넣음
             }
         }
-        //println(makeNumbers.contentToString())
-        // numbers 배열 재활용하기
+
+        // numbers 배열 재활용해서 차이값 넣기
         for (i in 0 until n - 1) {
             numbers[i] = (makeNumbers[i + 1] - makeNumbers[i]).absoluteValue
         }
         numbers[n - 1] = (makeNumbers[n - 1] - makeNumbers[0]).absoluteValue
-        //println(numbers.contentToString())
-        //println(numbers.max())
         sb.append(numbers.max()).append("\n")
     }
-    println(sb)
+    print(sb)
     br.close()
 }
