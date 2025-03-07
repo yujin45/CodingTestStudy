9264d66a036eb5c59b5d054949f14183ada8da72
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-07 14:48:59 +0900

    [Gold IV] Title: 도서관, Time: 124 ms, Memory: 21648 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.kt
+++ 백준/Gold/1461. 도서관/도서관.kt
@@ -1,32 +1,56 @@
 package org.example
 
-import kotlin.math.absoluteValue
+import java.util.StringTokenizer
+import java.util.PriorityQueue
+import kotlin.math.abs
 
-fun main(args: Array<String>) {
+fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val books = br.readLine().split(" ").map { it.toInt() }
+    // 가장 먼 곳은 되돌아오지 않을 곳 && 먼저 방문한다 생각하고 계산
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
 
-    // 음수와 양수 분리 후 정렬
-    val negatives = books.filter { it < 0 }.sorted()
-    val positives = books.filter { it > 0 }.sortedDescending()
+    val leftBooks = PriorityQueue<Int>(compareByDescending { it }) // 음수 값들 넣어줄 용도
+    val rightBooks = PriorityQueue<Int>(compareByDescending { it }) // 양수 값들 넣어줄 용도
 
-    var totalDistance = 0
-    val maxDistance = maxOf(negatives.firstOrNull()?.absoluteValue ?: 0, positives.firstOrNull()?.absoluteValue ?: 0)
-
-    // 음수 그룹 이동
-    for (i in negatives.indices step m) {
-        totalDistance += negatives[i].absoluteValue * 2
+    val st = StringTokenizer(br.readLine())
+    repeat(N) {
+        val book = st.nextToken().toInt()
+        if (book < 0) {
+            leftBooks.add(-book)
+        } else {
+            rightBooks.add(book)
+        }
     }
 
-    // 양수 그룹 이동
-    for (i in positives.indices step m) {
-        totalDistance += positives[i].absoluteValue * 2
+    val maxLen: Int
+    if (leftBooks.isNotEmpty() && rightBooks.isEmpty()) {
+        maxLen = leftBooks.first()
+    } else if (leftBooks.isEmpty() && rightBooks.isNotEmpty()) {
+        maxLen = rightBooks.first()
+    } else {
+        maxLen = if (leftBooks.first() > rightBooks.first()) leftBooks.first() else rightBooks.first()
     }
 
-    // 최장 거리 편도 적용
-    println(totalDistance - maxDistance)
 
+    var totalLen = -maxLen
+    var i = 0
+    while (leftBooks.isNotEmpty()) {
+        val book = leftBooks.poll()
+        if (i % M == 0) {
+            totalLen += book * 2
+            //println("book: $book - totalLen : $totalLen")
+        }
+        i++
+    }
+    i = 0
+    while (rightBooks.isNotEmpty()) {
+        val book = rightBooks.poll()
+        if (i % M == 0) {
+            totalLen += book * 2
+            //println("book: $book - totalLen : $totalLen")
+        }
+        i++
+    }
+    println(totalLen)
     br.close()
-
 }
