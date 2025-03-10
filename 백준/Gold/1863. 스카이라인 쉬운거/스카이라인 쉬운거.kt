f054d21e200a2541ac6975aa325ea4ac5dd3ec5a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-10 14:53:36 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 172 ms, Memory: 19744 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.kt
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.kt
@@ -1,28 +1,41 @@
 package org.example
 
+import java.util.StringTokenizer
+
+data class Homework(val A: Int, var T: Int)
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toInt() // 이번 학기 몇분인지
-    val stack = mutableListOf<Int>()
+    val stack = mutableListOf<Pair<Int, Int>>()
+    val n = br.readLine().toInt()
     var count = 0
     repeat(n) {
-        val (_, y) = br.readLine().split(" ").map { it.toInt() }
+        val st = StringTokenizer(br.readLine())
+        val cx = st.nextToken().toInt()
+        val cy = st.nextToken().toInt()
 
-        // 현재 높이보다 높은 모든 건물을 제거하면서 count 증가하기
-        while (stack.isNotEmpty() && stack.last() > y) {
-            stack.removeLast()
-            count++
+        while (stack.isNotEmpty()) {
+            //println(stack)
+            val (tx, ty) = stack.last() // 제일 위의 것 확인
+            if (ty >= cy) {
+                // 현재 y보다 같거나 크면 제거
+                if (ty > cy) {
+                    count++
+                }
+                stack.removeLast()
+            } else {
+                break
+            }
         }
 
-        // 스택이 비어 있거나, 현재 높이가 기존 높이랑 다르면 추가
-        if (y != 0 && (stack.isEmpty() || stack.last() != y)) {
-            stack.add(y)
-        }
-
-        //println(stack)
+        if (cy != 0) stack.add(cx to cy) // 바닥은 건물로 넣지 않음
     }
-    // 스택에 남아 있는 건물도 카운트
-    //println(stack)
-    println(count + stack.size)
+
+    while (stack.isNotEmpty()) {
+        count++
+        stack.removeLast()
+    } // 남은 건물 수 카운트
+    println(count)
+
     br.close()
 }
