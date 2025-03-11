559d245c3d0a046d567df54c28ebe8c6e6de128a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-11 13:25:54 +0900

    [Gold IV] Title: 회사 문화 1, Time: 516 ms, Memory: 91892 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.kt
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.kt
@@ -4,42 +4,43 @@ import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() } //회사 직원수 n, 최초 칭찬 횟수 m
-
-    // 1. 트리 생성
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val numOfUpPeople = StringTokenizer(br.readLine())
     val graph = Array(n + 1) { mutableListOf<Int>() }
-    val praise = IntArray(n + 1) { 0 } // 칭찬 저장 배열
-
-    // 2. 부모 -자식 관계 설정
-    val parentInfo = br.readLine().split(" ").map { it.toInt() }
-    for (i in 1 until n) {
-        // 직원번호 1부터 시작 0번 없음
-        val boss = parentInfo[i] // i+1번 직원의 상사
-        graph[boss].add(i + 1) // 부모 -> 자식 방향
-    }
-
-    // 3. 칭찬 입력(각 직원의 칭찬 수치 저장)
-    repeat(m) {
-        val (i, w) = br.readLine().split(" ").map { it.toInt() }
-        praise[i] += w // 특정 직원 i가 받은 칭찬을 저장
-    }
-
-    // 4. DFS를 이용한 칭찬 전파
-    fun dfs(node: Int) {
-        for (child in graph[node]) {
-            praise[child] += praise[node] // 부모의 칭찬을 자식에게 누적 전달
-            dfs(child)
+    var root = 0
+    for (i in 1..n) {
+        // 1번부터 n번까지 사원의 직장 상사 받아오기
+        val up = numOfUpPeople.nextToken().toInt()
+        if (up != -1) {
+            graph[up].add(i) // 상사에 나를 연결 상사 -> 직원 단방향
+        } else {
+            root = i // -1인거면 i가 사장임.
         }
     }
 
-    dfs(1) // 사장 1번부터 dfs 시작
+    // 칭찬 받은 것을 저장해두기
+    val wList = IntArray(n + 1)
+    repeat(m) {
+        val st = StringTokenizer(br.readLine())
+        wList[st.nextToken().toInt()] += st.nextToken().toInt()
+    }
+    //println(wList.contentToString())
+    dfs(graph, root, wList)
 
-    // 5. 모든 직원의 칭찬 수치 출력
     val sb = StringBuilder()
     for (i in 1..n) {
-        sb.append(praise[i]).append(" ")
+        sb.append(wList[i]).append(" ")
     }
     sb.deleteCharAt(sb.length - 1)
     println(sb)
+
     br.close()
+}
+
+fun dfs(graph: Array<MutableList<Int>>, num: Int, wList: IntArray) {
+    //println("num: $num - - wList: ${wList.contentToString()}")
+    for (neighbor in graph[num]) {
+        wList[neighbor] += wList[num] // 부모의 칭찬을 자식에게 누적 전달 ★
+        dfs(graph, neighbor, wList)
+    }
 }
