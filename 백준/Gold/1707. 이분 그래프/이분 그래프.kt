1fb1164f918539687639bcdaa151907fdfade9c3
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-11 14:01:03 +0900

    [Gold IV] Title: 이분 그래프, Time: 904 ms, Memory: 287488 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.kt
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.kt
@@ -5,53 +5,53 @@ import java.util.StringTokenizer
 fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
-    val K = br.readLine().toInt() // 테스트 케이스
+    // 입력 받기
+    val K = br.readLine().toInt()
     repeat(K) {
+        // 각 테스트 케이스
         val (V, E) = br.readLine().split(" ").map { it.toInt() }
-        // 정점 개수 V, 간선 개수 E
+        // 정점, 간선으로 그래프 구성
         val graph = Array(V + 1) { mutableListOf<Int>() }
         repeat(E) {
             val st = StringTokenizer(br.readLine())
             val a = st.nextToken().toInt()
             val b = st.nextToken().toInt()
+
             graph[a].add(b)
             graph[b].add(a)
         }
-        val visited = IntArray(V + 1) { -1 }
-        val finished = BooleanArray(V + 1)
-        var isBipartite = true
-        for (i in 1..V) {
-            if (visited[i] == -1) {
-                visited[i] = 0 // 초기값 설정
-                if (!dfs(graph, i, visited, finished)) {
-                    isBipartite = false
-                    break
-                }
+
+        // 이분 그래프가 될 수 있는 조건: 사이클이 생기지 않을 것
+        val visited = IntArray(V + 1) { 0 } // 방문한 것 색을 -1, 1으로 표시할 것임
+        var isBip = true
+        for (i in 1..V) {// 분리된 그래프 있을 수 있어서 탐색해줘야 함
+            if (visited[i] == 0) {
+                visited[i] = 1 // 그래프 탐색 시작
+                if (!dfs(graph, i, visited)) isBip = false
             }
         }
-        if (isBipartite) {
-            // 이분 그래프
+
+        if (isBip) {
             sb.append("YES\n")
         } else {
             sb.append("NO\n")
         }
-
     }
-
     print(sb)
     br.close()
 }
 
-fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: IntArray, finished: BooleanArray): Boolean {
+fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: IntArray): Boolean {
+
     for (neighbor in graph[v]) {
-        if (visited[neighbor] == -1) {
-            visited[neighbor] = 1 - visited[v]
-            if (!dfs(graph, neighbor, visited, finished)) return false
-        } else if (!finished[neighbor] && visited[v] == visited[neighbor]) {
-            // dfs 끝난 곳이 아닐 때, 이미 방문했고 홀수 사이클이면
+        if (visited[neighbor] == 0) {
+            // 아직 방문하지 않은 곳이라면 방문
+            visited[neighbor] = -visited[v] // 현재랑 반대 넣어주기
+            if (!dfs(graph, neighbor, visited)) return false
+        } else if (visited[neighbor] == visited[v]) {
+            // 만약 방문했었는데 현재랑 같은 색이었다면 사이클
             return false
         }
     }
-    finished[v] = true
     return true
 }
