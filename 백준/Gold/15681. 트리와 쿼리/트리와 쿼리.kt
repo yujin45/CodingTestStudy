521be331a90a98395d8c748fb83bc5dcbc8ad0c5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-11 13:38:52 +0900

    [Gold V] Title: 트리와 쿼리, Time: 548 ms, Memory: 91512 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.kt
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.kt
@@ -1,38 +1,47 @@
 package org.example
 
+import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    // 정점 N, 루트 번호 R, 쿼리 개수 Q
+    // 간선에 가중치와 방향성 없는 => 양방향
     val (N, R, Q) = br.readLine().split(" ").map { it.toInt() }
-    // 트리 간선 정보
-    val graph = Array(N + 1) { mutableListOf<Int>() }
+    // 정점 U를 루트로 하는 서브 트리에 속한 정점의 수
+    val tree = Array(N + 1) { mutableListOf<Int>() }
     repeat(N - 1) {
-        val (U, V) = br.readLine().split(" ").map { it.toInt() }
-        // 가중치와 방향성이 없는 트리
-        graph[U].add(V)
-        graph[V].add(U)
+        val st = StringTokenizer(br.readLine())
+        val U = st.nextToken().toInt()
+        val V = st.nextToken().toInt()
+        tree[U].add(V)
+        tree[V].add(U)
     }
-    val dp = IntArray(N + 1)
-    val visited = BooleanArray(N + 1)
 
-    dfs(graph, R, visited, dp)
+    // q줄마다 subTree 개수 탐색하면 시간 초과할 가능성 있음.
+    // 미리 subtree 다 계산해서 가지고 있기
+    val subTreeCount = IntArray(N + 1)
+    val visited = BooleanArray(N + 1)
+    dfs(tree, R, visited, subTreeCount)
+    //println(subTreeCount.contentToString())
+
+    // Q에 대한 서브트리 개수 저장
     val sb = StringBuilder()
     repeat(Q) {
         val U = br.readLine().toInt()
-        sb.append(dp[U]).append("\n")
+        sb.append(subTreeCount[U]).append("\n")
     }
     print(sb)
     br.close()
 }
 
-fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: BooleanArray, dp: IntArray): Int {
+fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: BooleanArray, subTreeCount: IntArray): Int {
+    var count = 1
     visited[v] = true
-    dp[v] = 1 // 현재 노드도 포함
+
     for (neighbor in graph[v]) {
         if (!visited[neighbor]) {
-            dp[v] += dfs(graph, neighbor, visited, dp) // 자식 노드의 서브트리 크기 합산
+            count += dfs(graph, neighbor, visited, subTreeCount)
         }
     }
-    return dp[v] // 현재 노드의 서브트리 크기 반환
+    subTreeCount[v] = count
+    return count
 }
