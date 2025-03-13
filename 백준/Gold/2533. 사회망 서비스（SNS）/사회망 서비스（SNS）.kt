755d695b9cf238beedb12ce511c2b1b3abc40f55
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-13 14:52:32 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2108 ms, Memory: 422032 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.kt
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.kt
@@ -6,33 +6,30 @@ fun main() {
     val br = System.`in`.bufferedReader()
 
     val N = br.readLine().toInt()
-    // 1~n번
     val graph = Array(N + 1) { mutableListOf<Int>() }
     repeat(N - 1) {
         val st = StringTokenizer(br.readLine())
-        val a = st.nextToken().toInt()
-        val b = st.nextToken().toInt()
-        //val (a, b) = br.readLine().split(" ").map { it.toInt() }
-        graph[a].add(b)
-        graph[b].add(a)
+        val u = st.nextToken().toInt()
+        val v = st.nextToken().toInt()
+        graph[u].add(v)
+        graph[v].add(u)
     }
     val visited = BooleanArray(N + 1)
-    val dp = Array(N + 1) { IntArray(2) { 0 } } // 선택, 미선택
-    dfs(graph, 1, visited, dp)
-    //println(dp.contentDeepToString())
+    val dp = Array(N + 1) { IntArray(2) } // 자기 선택 1, 자기선택x 0
+    dfs(graph, 1, visited, dp) // 루트를 1이라고 생각하고 진행
     println(minOf(dp[1][0], dp[1][1]))
+
     br.close()
 }
 
 fun dfs(graph: Array<MutableList<Int>>, v: Int, visited: BooleanArray, dp: Array<IntArray>) {
     visited[v] = true
-    dp[v][0] = 1 //선택한 경우
-    dp[v][1] = 0 // 선택하지 않은 경우
+    dp[v][1] = 1 // 자기 자신 선택
     for (neighbor in graph[v]) {
         if (!visited[neighbor]) {
             dfs(graph, neighbor, visited, dp)
-            dp[v][0] += minOf(dp[neighbor][0], dp[neighbor][1]) // 지금 v 선택하면 자식은 선택 작은 것으로
-            dp[v][1] += dp[neighbor][0] // 지금 v를 선택 안하면 자식은 선택해야 함
+            dp[v][1] += minOf(dp[neighbor][1], dp[neighbor][0])
+            dp[v][0] += dp[neighbor][1]
         }
     }
 }
