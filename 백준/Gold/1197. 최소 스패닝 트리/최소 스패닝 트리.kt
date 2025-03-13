c26297d73de14f0e6ba372fda24e627af9321fc1
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-13 16:22:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 488 ms, Memory: 55592 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.kt
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.kt
@@ -3,60 +3,72 @@ package org.example
 import java.util.StringTokenizer
 import java.util.PriorityQueue
 
-data class Edge(val a: Int, val b: Int, val cost: Int)
+data class Edge(val a: Int, val b: Int, val cost: Int) : Comparable<Edge> {
+    override fun compareTo(other: Edge): Int {
+        return this.cost - other.cost
+    }
+}
 
+// 유니온 파인드 : find
 fun find(parent: IntArray, x: Int): Int {
     if (parent[x] != x) {
+        // 지금 자기 자신이랑 같지 않으면 루트 값으로 맞춰주기
         parent[x] = find(parent, parent[x])
     }
     return parent[x]
 }
 
 fun union(parent: IntArray, rank: IntArray, a: Int, b: Int) {
-    val rootA = find(parent, a)
-    val rootB = find(parent, b)
+    val rootA = find(parent, a) //a의 루트 찾기
+    val rootB = find(parent, b) // b의 루트 찾기
+
     if (rootA != rootB) {
+        // 서로의 루트가 다르다면 랭크 높은 트리에 합쳐주기
         if (rank[rootA] > rank[rootB]) {
-            parent[rootB] = rootA
+            parent[rootB] = rootA // 랭크 높은 트리 합치기
         } else if (rank[rootA] < rank[rootB]) {
-            parent[rootA] = rootB
+            parent[rootA] = rootB // rootA를 rootB 아래로 두는 것
         } else {
-            parent[rootB] = rootA
-            rank[rootA]++
+            // 동일하다면
+            parent[rootB] = rootA // rootB를 rootA로 두어서 rootA가 루트가 되게 함
+            rank[rootA] += 1 // 1랭크업 잡아 먹은거라고 생각하기
         }
     }
+
 }
 
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val (V, E) = br.readLine().split(" ").map { it.toInt() }
-    val pq = PriorityQueue<Edge>(compareBy<Edge> { it.cost })
-    repeat(E) {
-        val st = StringTokenizer(br.readLine())
-        pq.add(Edge(st.nextToken().toInt(), st.nextToken().toInt(), st.nextToken().toInt()))
-    }
+fun kruskal(n: Int, edges: PriorityQueue<Edge>): Int {
+    val parent = IntArray(n + 1) { it } // 노드 n개에 대해서 부모 자기 자신으로 초기화(0번은 안 씀)
+    val rank = IntArray(n + 1) { 0 }
+    var totalCost = 0
+    var edgeCount = 0
 
-    var totalCost = 0 // 가중치 합
-    var edgeCount = 0 // N-1개 되면 스패닝 트리 완성
-
-    val parent = IntArray(V + 1) { it }
-    val rank = IntArray(V + 1) { 0 }
-
-    // 크루스칼 알고리즘으로 진행
-    while (pq.isNotEmpty()) {
-        if (edgeCount == V - 1) break
-
-        val edge = pq.poll() // 최소 가중치부터 뽑기
+    while (edges.isNotEmpty()) {
+        if (edgeCount == n - 1) break // n-1개 간선 완성되면 종료
+        val edge = edges.poll() // 정렬된 상태로 빼오기 가장 작은 cost부터
         if (find(parent, edge.a) != find(parent, edge.b)) {
-            // 같은 그룹이 아니라면 합쳐주기
+            // 사이클 방지. 자기 부모 다를 때만 합치기
             union(parent, rank, edge.a, edge.b)
             totalCost += edge.cost
             edgeCount++
         }
 
     }
-    println(totalCost)
+    return totalCost
+}
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+
+    val (V, E) = br.readLine().split(" ").map { it.toInt() }
+    val edges = PriorityQueue<Edge>()
+    repeat(E) {
+        val st = StringTokenizer(br.readLine())
+        edges.add(Edge(st.nextToken().toInt(), st.nextToken().toInt(), st.nextToken().toInt()))
+    }
+
+    println(kruskal(V, edges))
 
     br.close()
-
 }
+
