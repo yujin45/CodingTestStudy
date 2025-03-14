6a566515c8299f1ee79f4f508e6b89eed1a13d0b
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-14 14:00:28 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 148824 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.kt
+++ 백준/Gold/7576. 토마토/토마토.kt
@@ -2,59 +2,72 @@ package org.example
 
 import java.util.StringTokenizer
 
-val dx = intArrayOf(-1, 1, 0, 0)
-val dy = intArrayOf(0, 0, -1, 1)
+private val dx = intArrayOf(-1, 1, 0, 0)
+private val dy = intArrayOf(0, 0, -1, 1)
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (M, N) = br.readLine().split(" ").map { it.toInt() }
-    val graph = Array(N) { IntArray(M) }
-    val queue = ArrayDeque<Pair<Int, Int>>() // BFS 큐
 
-    var yetTomatoCount = 0
+    val (M, N) = br.readLine().split(" ").map { it.toInt() }
+
+    val graph = Array(N) { IntArray(M) }
+    var queue = ArrayDeque<Pair<Int, Int>>()
+    var notDoneTomato = 0
     for (i in 0 until N) {
         val st = StringTokenizer(br.readLine())
         for (j in 0 until M) {
-            graph[i][j] = st.nextToken().toInt()
-            if (graph[i][j] == 1) {
-                queue.add(i to j) // 익은 토마토 큐에 미리 추가
-            } else if (graph[i][j] == 0) {
-                yetTomatoCount++ // 익지 않은 토마토 개수
+            val tomato = st.nextToken().toInt()
+            if (tomato == 0) {
+                // 안 익은 토마토 개수 세어주기
+                notDoneTomato++
+            } else if (tomato == 1) {
+                // 익은 토마토 위치 큐에 삽입해주기
+                queue.add(i to j)
             }
+            graph[i][j] = tomato
         }
     }
-    // 입력 받았을 때부터 안 익은 것이 없다면 바로 0 출력 후 return
-    if (yetTomatoCount == 0) {
+
+    // 모든 토마토가 익어 있는 상태 => 안 익은 토마토가 없는 경우
+    if (notDoneTomato == 0) {
         println(0)
         return
     }
 
-    val result = bfs(graph, queue, yetTomatoCount)
-    println(result) // 최소 날짜 출력
-    br.close()
-
-}
-
-fun bfs(graph: Array<IntArray>, queue: ArrayDeque<Pair<Int, Int>>, yetTomatoCount: Int): Int {
-    var days = -1 // 날짜 카운트(처음부터 1일 차이 발생하므로 -1 시작)
-    var notDoneTomato = yetTomatoCount
+    var day = 0
+    var temp = ArrayDeque<Pair<Int, Int>>()
     while (queue.isNotEmpty()) {
-        val size = queue.size // 현재 날짜에 전파될 모든 토마토 개수
-        for (i in 0 until size) {
-            val (x, y) = queue.removeFirst()
-            for (dir in 0 until 4) {
-                val nx = x + dx[dir]
-                val ny = y + dy[dir]
+        val (cx, cy) = queue.removeLast()
+        for (i in 0 until 4) {
+            val nx = cx + dx[i]
+            val ny = cy + dy[i]
 
-                if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] == 0) {
-                    graph[nx][ny] = graph[x][y] + 1 // 날짜 증가
-                    queue.add(nx to ny) // 새로 익은 토마토를 큐에 추가
-                    notDoneTomato--
-                }
+            if (nx in graph.indices && ny in graph[0].indices && graph[nx][ny] == 0) {
+                temp.add(nx to ny) // 다음 탐색지로 지정
+                graph[nx][ny] = 1 // 토마토 있으면 익히기
+                notDoneTomato-- // 익은거 하나 제거
             }
+
         }
-        days++ // 하루 증가
+
+        if (queue.isEmpty() && temp.isNotEmpty()) {
+            // 하루 지나고 queue 교체해주기
+            day++
+            val dummy = queue
+            queue = temp
+            temp = dummy
+        }
     }
 
-    return if (notDoneTomato == 0) days else -1
+
+    // 토마토가 모두 익지 못하는 상황이면
+    if (notDoneTomato != 0) {
+        // 다 돌았는데 0이 아니면 못 익는 것이 있음
+        println(-1)
+    } else {
+        println(day)
+    }
+
+    br.close()
 }
+
