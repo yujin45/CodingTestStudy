47ddde81ace24c38cfb17a52b590ad6c886c895b
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-14 17:36:05 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 17.78 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 12.62 ms
+메모리: 99.1 MB, 시간: 17.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 15:59:21
+2025년 03월 14일 17:36:03
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.kt
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.kt
@@ -1,41 +1,55 @@
-
 class Solution {
-
-    private var oneCount = 0
-    private var zeroCount = 0
-
     fun solution(arr: Array<IntArray>): IntArray {
-        dfs(arr, 0, 0, arr.size)
-        return intArrayOf(zeroCount,oneCount)
+
+        // 0개수 1 개수
+        val n = arr.size
+        val (z, o) = dfs(arr, 0, 0, n)
+        var answer: IntArray = intArrayOf(z, o)
+
+        return answer
     }
 
-    private fun isAllSame(graph:Array<IntArray>, sx:Int, sy:Int, size:Int):Boolean{
-        val num = graph[sx][sy]
-        for(i in sx until sx+size){
-            for(j in sy until sy+size){
-                if(graph[i][j]!=num){
-                    return false
+    fun dfs(graph:Array<IntArray>, start:Int, end:Int, n:Int): Pair<Int, Int>{
+        //println("---------------dfs")
+        //println("$start, $end - $n")
+        var zero = 0
+        var one = 0
+        val initElement = graph[start][end]
+        var isSame = true
+
+        for(i in start until start+n){
+            if(!isSame) break
+            for(j in end until end+n){
+                if(graph[i][j] != initElement){
+                    // 같이 않으면 바로 멈추기
+                    //println("동일하지 않은 것 발견 - i $i, j $j - ${graph[i][j]} != $initElement")
+                    isSame = false
+                    break
                 }
             }
         }
-        return true
-    }
-    fun dfs(graph: Array<IntArray>, sx: Int, sy: Int,size: Int) {
-        if(isAllSame(graph, sx, sy, size)){
-            if(graph[sx][sy] ==1){
-                oneCount++
-            }else{
-                zeroCount++
-            }
-            // 다 같은거면 dfs 종료
-            return
-        }
 
-        val half = size/2 
-        dfs(graph, sx, sy, half) // 왼쪽 위 
-        dfs(graph, sx, sy+half, half) //  오른쪽 위
-        dfs(graph, sx+half, sy, half) // 왼쪽 아래
-        dfs(graph, sx+half, sy+half, half) // 오른쪽 아래
+        if(!isSame){
+            // 4분할 진행
+            //println("00000000000000000000000000 4분할 진행!")
+            val (z1, o1) = dfs(graph, start, end, n/2)
+            val (z2, o2) = dfs(graph, start+n/2, end, n/2)
+            val (z3, o3) = dfs(graph, start, end+n/2, n/2)
+            val (z4, o4) = dfs(graph, start+n/2, end+n/2, n/2)
+            zero = z1 + z2 + z3 + z4
+            one = o1 + o2 + o3 + o4
+        }else{
+            // 모든 값이 같으면
+            if(initElement  == 0){
+                // 0
+                zero = 1
+            }else{
+                // 1
+                one = 1
+            }
+        }
+        //println("\n\n 결과 $start, $end - $n - $zero $one \n")
+        return (zero to one)
+
     }
 }
-
