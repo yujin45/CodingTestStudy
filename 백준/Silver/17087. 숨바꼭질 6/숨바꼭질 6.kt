715770e6984daaa23706ba1fe09dff913d6406bb
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-15 13:34:26 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 292 ms, Memory: 35336 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.kt
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.kt
@@ -1,12 +1,32 @@
-import java.util.*
-import kotlin.math.abs
+package org.example
 
-fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+import java.util.StringTokenizer
+import kotlin.math.abs
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, S) = br.readLine().split(" ").map { it.toInt() }
-    val A = br.readLine().split(" ").map { abs(it.toInt() - S) }
+    if (N == 1) {
+        // 뒤의 reduce는 요소 2개 이상이어야 해서&빠른 return
+        println(abs(br.readLine().toInt() - S))
+        return
+    }
 
-    println(A.reduce(::gcd))
+    val st = StringTokenizer(br.readLine())
+    val differ = IntArray(N)
+
+    for (i in 0 until N) {
+        val now = st.nextToken().toInt()
+        differ[i] = abs(S - now)
+    }
+    //println(differ.contentToString())
+
+    //println(differ.reduce(::gcd))
+    //val gcdDiffer = differ.reduce { acc, num -> gcd(acc, num) } // differ 요소가 2개 이상이어야 함
+    val gcdDiffer = differ.fold(differ[0]) { acc, num -> gcd(acc, num) }
+    println(gcdDiffer)
+    br.close()
 }
+
+fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
