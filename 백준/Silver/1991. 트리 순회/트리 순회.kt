49d0e89451042750b50ecc1c410190df988a3617
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-15 14:11:32 +0900

    [Silver I] Title: 트리 순회, Time: 76 ms, Memory: 12424 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.kt
+++ 백준/Silver/1991. 트리 순회/트리 순회.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import java.util.StringTokenizer
+import kotlin.math.abs
 
 data class Node(
     val value: Char,
@@ -11,12 +12,14 @@ data class Node(
 fun main() {
     val br = System.`in`.bufferedReader()
     val N = br.readLine().toInt()
-    val tree = mutableMapOf<Char, Node>()
+
+    val tree = hashMapOf<Char, Node>() // value별로 node 만들어둔 것
     repeat(N) {
         val st = StringTokenizer(br.readLine())
         val parent = st.nextToken()[0]
         val left = st.nextToken()[0]
         val right = st.nextToken()[0]
+
         val parentNode = tree.getOrPut(parent) { Node(parent) }
         if (left != '.') {
             parentNode.left = tree.getOrPut(left) { Node(left) }
@@ -24,10 +27,9 @@ fun main() {
         if (right != '.') {
             parentNode.right = tree.getOrPut(right) { Node(right) }
         }
-
     }
 
-    val root = tree['A']!! //문제 조건
+    val root = tree['A']!! // 문제 조건
     val sb = StringBuilder()
     preorder(root, sb)
     sb.append("\n")
@@ -39,7 +41,6 @@ fun main() {
     br.close()
 }
 
-// 전위 순회
 fun preorder(node: Node?, sb: StringBuilder) {
     if (node == null) return
     sb.append(node.value)
@@ -47,7 +48,6 @@ fun preorder(node: Node?, sb: StringBuilder) {
     preorder(node.right, sb)
 }
 
-// 중위 순회
 fun inorder(node: Node?, sb: StringBuilder) {
     if (node == null) return
     inorder(node.left, sb)
@@ -55,7 +55,6 @@ fun inorder(node: Node?, sb: StringBuilder) {
     inorder(node.right, sb)
 }
 
-// 후위 순회
 fun postorder(node: Node?, sb: StringBuilder) {
     if (node == null) return
     postorder(node.left, sb)
