a336d621e0f2f71124e58aeb3160ae18135b08e1
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-15 15:06:28 +0900

    [Silver I] Title: 완전 이진 트리, Time: 88 ms, Memory: 12900 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.kt
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.kt
@@ -1,29 +1,45 @@
 package org.example
 
+import java.util.StringTokenizer
+import kotlin.math.roundToInt
+
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val K = br.readLine().toInt()
+    // inorder
 
-    val treeList = br.readLine().split(" ").map { it.toInt() }
-    br.close()
+    // 상근이가 순서 주어졌을 때 각 레벨에 있는 빌딩 번호 구하기
 
-    val levels = Array(K) { ArrayList<Int>() } // 깊이 별 노드 저장 리스트
-    buildTree(treeList, levels, 0, 0, treeList.size - 1)
+    val K = br.readLine().toDouble()
+    val st = StringTokenizer(br.readLine())
+    val arr = IntArray((Math.pow(2.0, K).toInt() - 1))
+    for (i in 0 until arr.size) {
+        arr[i] = st.nextToken().toInt()
+    }
 
+    //println(arr.contentToString())
+    val depthArray = Array(K.toInt()) { mutableListOf<Int>() }
+    search(K.toInt(), 0, arr.size - 1, 0, depthArray, arr)
+    //println(depthArray.contentDeepToString())
     val sb = StringBuilder()
-    for (level in levels) {
-        sb.append(level.joinToString(" ")).append("\n")
+    depthArray.forEach { item ->
+        item.forEach { node ->
+            sb.append(node).append(" ")
+        }
+        sb.deleteCharAt(sb.length - 1).append("\n")
     }
     print(sb)
+    br.close()
 }
 
-// 리스트 복사 대신 인덱스 범위 사용
-fun buildTree(treeList: List<Int>, levels: Array<ArrayList<Int>>, depth: Int, start: Int, end: Int) {
-    if (start > end) return
-
+fun search(k: Int, start: Int, end: Int, depth: Int, depthArray: Array<MutableList<Int>>, arr: IntArray) {
+    if (depth == k) {
+        //println("k도달--------------")
+        return
+    }
     val mid = (start + end) / 2
-    levels[depth].add(treeList[mid]) // 해당 depth 에 추가
-
-    buildTree(treeList, levels, depth + 1, start, mid - 1) // 왼쪽 서브트리
-    buildTree(treeList, levels, depth + 1, mid + 1, end) // 오른쪽 서브트리
+    depthArray[depth].add(arr[mid])
+    //println("start $start  - end $end  /2 -> mid $mid")
+    search(k, start, mid - 1, depth + 1, depthArray, arr) // 왼쪽
+    search(k, mid + 1, end, depth + 1, depthArray, arr) // 오른쪽
 }
