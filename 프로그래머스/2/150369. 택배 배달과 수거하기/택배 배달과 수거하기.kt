b13ae8c17ee698cf9b355d11baf75b111c4cdfd0
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-18 10:44:15 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 8.75 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun solution(cap: Int, n: Int, deliveries: IntArray, pickups: IntArray): Long {
         var answer: Long = 0
-        var remainDeliveries = 0  // 남은 배달 상자 개수
-        var remainPickups = 0     // 남은 수거 상자 개수
+        var remainDeliveries = 0
+        var remainPickups = 0
+        for(i in n-1 downTo 0){
+            // 배달 갔다가 수거해오면서 cap씩 가능
+            remainDeliveries += deliveries[i] // 배달해야 할 것 추가
+            remainPickups += pickups[i] // 수거해야 할 것 추가
 
-        for (i in n - 1 downTo 0) {
-            remainDeliveries += deliveries[i]  // 현재 위치에서 배달해야 할 상자 추가
-            remainPickups += pickups[i]        // 현재 위치에서 수거해야 할 상자 추가
-
-            while (remainDeliveries > 0 || remainPickups > 0) {
-                // 가장 멀리 가야 하는 집까지 왕복
-                answer += (i + 1) * 2L  
-
-                // cap 만큼 배달/수거 수행
-                remainDeliveries -= cap
-                remainPickups -= cap
+            while(remainDeliveries > 0 || remainPickups >0){
+                // 가장 멀리 가는 곳 왕복 
+                answer+=(i+1) *2L
+                // CAP만큼 진행
+                remainDeliveries -=cap
+                remainPickups -=cap
             }
         }
+
         return answer
     }
 }
