1bbcf76b1ac027a78fb705b52a59cc17d7267603
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-19 13:51:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.92 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 63.5 MB, 시간: 7.69 ms
+메모리: 63.5 MB, 시간: 7.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 19일 17:31:44
+2025년 03월 19일 13:51:40
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.kt
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.kt
@@ -1,40 +1,41 @@
 import java.util.PriorityQueue
 
-data class Task(val taskNum: Int, val requestTime: Int, val time: Int)
+data class Task(val taskNum:Int, val requestTime:Int, val time:Int)
 
 class Solution {
     fun solution(jobs: Array<IntArray>): Int {
+        
         var totalTurnaroundTime = 0
-        val requestQueue = PriorityQueue<Task>(compareBy { it.requestTime }) // 요청된 작업을 시간 순서대로 저장
-        val readyQueue = PriorityQueue<Task>(compareBy<Task> { it.time }
-            .thenBy { it.requestTime }
-            .thenBy { it.taskNum }) // 실행 대기 큐 (SJF 기반 우선순위)
-
-        for (i in jobs.indices) {
-            requestQueue.add(Task(i, jobs[i][0], jobs[i][1])) // 요청 시간 순으로 정렬
+        val requestQueue = PriorityQueue<Task>(compareBy<Task>{it.requestTime}) // 요청된 작업 순서대로 저장
+        val readyQueue = PriorityQueue<Task>(compareBy<Task>{it.time}
+                                            .thenBy{it.requestTime}
+                                            .thenBy{it.taskNum}) // 실행 대기 큐
+        
+        for(i in jobs.indices){
+            requestQueue.add(Task(i, jobs[i][0], jobs[i][1]))
         }
-
+        
         var currentTime = 0
         var doneCount = 0
-
-        while (doneCount < jobs.size) {
-            // 현재 시점에서 요청된 작업들을 readyQueue에 추가
-            while (requestQueue.isNotEmpty() && requestQueue.peek().requestTime <= currentTime) {
+        
+        while(doneCount<jobs.size){
+            // 현재 시점에서 요청된 작업들 readyQueue에 추가
+            while(requestQueue.isNotEmpty() && requestQueue.peek().requestTime <=currentTime){
                 readyQueue.add(requestQueue.poll())
             }
-
-            if (readyQueue.isNotEmpty()) {
-                // 가장 작업 시간이 짧은 것부터 실행
+            
+            if(readyQueue.isNotEmpty()){
+                // 가장 작업 시간 짧은 것 우선 실행
                 val task = readyQueue.poll()
                 currentTime += task.time
-                totalTurnaroundTime += (currentTime - task.requestTime) // 반환 시간 누적
-                doneCount++ // 완료된 작업 증가
-            } else {
-                // readyQueue가 비어있다면 currentTime을 가장 빠른 작업이 도착하는 시간으로 Jump
+                totalTurnaroundTime += (currentTime - task.requestTime)
+                doneCount++
+            }else{
+                // 비어있다면 currentTime가장 빠른 작업이 도착하는 시간
                 currentTime = requestQueue.peek().requestTime
             }
         }
-
-        return totalTurnaroundTime / jobs.size // 평균 반환 시간의 정수값
+        
+        return totalTurnaroundTime / jobs.size
     }
 }
