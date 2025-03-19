610d7f0a148ad75bc991e8de1fc49963ed3359c0
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-19 14:16:36 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 9.69 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.kt
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.kt
@@ -1,19 +1,8 @@
 class Solution {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
         var count = 0
-        //  만약 인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다. 
-        val backet = mutableListOf<Int>() // 바구니 
-        //printBoard(board)
-
-        // moves에 담긴 i번 위치 ->인덱스로 i-1 위치
-
-        // board를 어떻게 다룰까
-        // 1. n개의 스택으로 만들어두고 쓴다 => 뽑는 것은 쉽겠지만 공간 낭비 => PASS
-        // 2. 행 0부터 ->N-1까지 0이 아닌 순간을 찾는다 => 최악 O(N) => moves의 수가 K라면  O(K*N) -> 최악의 경우 1000 × 100 = 100,000 연산.
-        // 3. 열의 개수만큼 가장 상단 인덱스를 저장하는 배열을 만든다 
-        //     => 메모리 좀 차지하겠지만 처음 초기화 최악 O(N^2) 이후로는 탐색 O(1) 전체 복잡도: O(N² + K) -> 최악의 경우 10,000 + 1,000 = 11,000 연산.
-
-        // => 3번이 가장 효율적!
+        val backet = mutableListOf<Int>()
+        
         val topIndex = IntArray(board.size)
         for(col in board.indices){
             for(row in board.indices){
@@ -23,33 +12,25 @@ class Solution {
                 }
             }
         }
-        //println("topIndex: ${topIndex.contentToString()}")
-
+        
         for(m in moves){
             val col = m-1
             val row = topIndex[col]
-
+            
+            
             if(row < board.size){
                 val doll = board[row][col]
-                if(backet.isNotEmpty() &&backet.last() == doll){
-                    // 맨 위가 doll이랑 같으면
-                    backet.removeLast() // 뽑아버리기
+                if(backet.isNotEmpty() && backet.last()==doll){
+                    backet.removeLast()
                     count+=2
                 }else{
-                    // 위의 조건 아니면 그냥 다 넣기
                     backet.add(doll)
                 }
-                board[row][col] = 0 // 인형 뽑기 완료
-                topIndex[col]++ // 인형 뽑아서 한칸 내려감
+                board[row][col]=0
+                topIndex[col]++
             }
-            // 만약 인형 없으면 아무 일도 일어나지 않음
         }
-
-
+        
         return count
     }
-
-    // fun printBoard(board:Array<IntArray>){
-    //     board.forEach{ println(it.contentToString())}
-    // }
 }
