503cb70f320444eb98f6edf8364a937a3ef8dfbc
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-19 12:33:10 +0900

    [level 2] Title: 롤케이크 자르기, Time: 39.34 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -1,27 +1,31 @@
 class Solution {
     fun solution(topping: IntArray): Int {
-        val a = HashSet<Int>() // 좌 -> 우
-        val b = HashSet<Int>() // 좌 <- 우
-
-        val n = topping.size
-        val adp = IntArray(n)
-        val bdp = IntArray(n)
-        for(i in topping.indices){
-            a.add(topping[i])
-            b.add(topping[n-1-i])
-            adp[i] = a.size
-            bdp[n-1-i] = b.size
-        }
-        //println(adp.contentToString())
-        //println(bdp.contentToString())
-        var count =  0
-        for(i in 0 until n-1){
-            // i번째 뒤에 자르면
-            if(adp[i] == bdp[i+1]){
-                // \ 한 방향으로만 체크
-                count++
+        
+        val toppingCount = IntArray(10_001) // 토핑 개수
+        var rightCount = 0
+        var leftCount = 0
+        var result = 0
+        for(t in topping){
+            if(toppingCount[t]==0){
+                // 없던 종류이면 right의 종류 증가
+                rightCount++
             }
+            toppingCount[t] +=1 // 토핑 개수 증가
         }
-        return count
+        
+        val seen = BooleanArray(10_001) // left가 가지고 있는지 확인용
+        // left가 하나씩 종류 가져오면서 left = right 종류 개수 같으면 result ++해주기
+        for(i in topping.indices){
+            if(!seen[topping[i]]){
+                // 새로운 종류면
+                leftCount++
+                seen[topping[i]]= true
+            }
+            toppingCount[topping[i]]--// 개수 줄이기
+            if(toppingCount[topping[i]]==0) rightCount--
+            if(leftCount==rightCount) result++
+        }
+        
+        return result
     }
 }
