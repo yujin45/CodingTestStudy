6143e3b89c906ec5be5e0ddaa269f66fb77f2794
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-03-28 17:24:37 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 2.00 ms
+메모리: 9.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 15:24:11
+2025년 03월 28일 17:24:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,19 @@
+import math
 def solution(progresses, speeds):
-    # 매개변수 자체를 큐로 만들어보자
+    need = []
+    for i in range(0, len(speeds)):
+        need.append(math.ceil((100 - progresses[i]) / speeds[i]))
+    
     answer = []
-    days = 0
-    cnt = 0
-    while progresses:
-        #print(progresses)
-        # 큐가 있는 동안
-        # peek해서 확인
-        if (progresses[0] + days*speeds[0])>= 100:
-            # 100보다 크면 완료
-            progresses.pop(0)
-            speeds.pop(0)
-            cnt +=1
+    count = 1
+    cneed = need[0]
+    for i in range(1, len(need)):
+        if cneed < need[i]:
+            answer.append(count)
+            count = 1
+            cneed = need[i]
         else:
-            # 작으면
-            if cnt >0:
-                # 기능 완성된 것이 있으면
-                answer.append(cnt) 
-                cnt =0
-            days +=1 # day는 0으로 초기화 되지 않고 계속 흐르는 것!
-            # days 흐른 만큼 완성될 수 있는 애들은 위에서 다 cnt+=1 되는 것!
-    # 마지막 작업 
-    answer.append(cnt)
+            count+=1
+    answer.append(count)
+    
     return answer
