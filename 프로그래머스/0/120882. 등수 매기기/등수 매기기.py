1f5ed90cace2f6af28372b90af4f99813e5aef53
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-06 21:52:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,15 +1,18 @@
 def solution(score):
-    # 각 학생의 평균 점수를 계산합니다.
-    averages = [(s[0] + s[1]) / 2 for s in score]
-    #print(averages)
-    # 평균 점수를 내림차순으로 정렬하고, 등수를 매깁니다.
-    sorted_averages = sorted(averages, reverse=True)
-    #print(sorted_averages)
-    # 각 평균 점수에 대한 등수를 저장할 리스트를 초기화합니다.
-    ranks = []
+    # 1. 평균 계산 및 인덱스 저장
+    avg_with_index = [(i, (s[0]+s[1])/2) for i, s in enumerate(score)]
     
-    for avg in averages:
-        rank = sorted_averages.index(avg) + 1
-        ranks.append(rank)
+    # 2. 평균 기준 정렬
+    sorted_avg = sorted(avg_with_index, key = lambda x : x[1], reverse = True)
     
+    # 3. 등수 매기기
+    ranks = [0] * len(score)
+    rank = 1
+    
+    for i in range(len(sorted_avg)):
+        if i > 0 and sorted_avg[i][1] == sorted_avg[i-1][1]:
+            rank = ranks[sorted_avg[i-1][0]] # 이전 학생과 동일 등수
+        else:
+            rank = i+1 # 현재 순위 
+        ranks[sorted_avg[i][0]] = rank
     return ranks
