f737019f7167af5fc0aafe331a87983bad796a5a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-06 21:14:47 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,12 +1,2 @@
 def solution(order):
-    order = str(order)
-    answer = order.count('3') + order.count('6') + order.count('9')
-    return answer
-
-def solution1(order):
-    answer = 0
-    for s in str(order):
-        if s in ['3', '6', '9']:
-            answer+=1
-    return answer
-
+    return str(order).count('3') +str(order).count('6') +str(order).count('9')
