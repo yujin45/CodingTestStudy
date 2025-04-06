8bcb2a8da9c85f6afad2eecd89469156a7cd899f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-06 21:37:54 +0900

    [level 0] Title: A로 B 만들기, Time: 0.37 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,6 +1,3 @@
 def solution(before, after):
-    answer = 0
-    for b in before:
-        if b in after:
-            after = after[:after.find(b)] + after[after.find(b)+1:] # 확인된 것 1개만 제거
-    return 1 if len(after)==0 else 0
+    return 1 if sorted(before) == sorted(after) else 0
+    
