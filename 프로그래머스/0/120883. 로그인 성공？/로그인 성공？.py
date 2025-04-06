a4dd8f4bf39640e51093e682c75d01620b060597
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-06 21:47:19 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,36 +1,14 @@
-
 def solution(id_pw, db):
-    database = dict(db)
-    #print(database)
-    pw = database.get(id_pw[0], -1) # 키 존재하지 않으면 기본 값 리턴
-    if pw == -1:
-        return "fail"
-    elif pw == id_pw[1]:
-        return "login"
-    else:
-        return "wrong pw"
-
-def solution1(id_pw, db):
-    database = dict(db)
-    pw = database.setdefault(id_pw[0], "fail") # 없으면 키 삽입 후 디폴트 value 설정
-    if pw =="fail":
-        return "fail"
-    elif pw == id_pw[1]:
-        return "login"
-    else:
-        return "wrong pw"
     
-    return "fail"
-
-def solution2(id_pw, db):
-    answer = ''
-    for id_, pw in db:
-        if id_ == id_pw[0]:
-            # id 같으면
-            if pw == id_pw[1]:
-                # pw도 같으면
-                return "login"
-            else:
-                return "wrong pw"
-        
-    return "fail"
+    user = {}
+    for key, value in db:
+        user[key] = value
+    
+    if id_pw[0] in user:
+        if user[id_pw[0]] == id_pw[1]:
+            return "login"
+        else:
+            return "wrong pw"
+    else:
+        return "fail"
+    
