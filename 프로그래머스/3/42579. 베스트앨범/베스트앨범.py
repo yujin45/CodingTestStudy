d90a94212d8f0ec6ea055488b43af133160fd84c
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-15 16:48:50 +0900

    [level 3] Title: 베스트앨범, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,29 +1,33 @@
-from dataclasses import dataclass
 from collections import defaultdict
+from dataclasses import dataclass
 
 @dataclass
 class Song:
-    num:int
-    play:int
+    number : int 
+    play : int
 
 def solution(genres, plays):
-    total = defaultdict(int)
+    result = []
+    
+    # 기본 구조 제작
+    playcount_dict = defaultdict(int)
     songs = defaultdict(list)
-    
-    for index, (g, p) in enumerate(zip(genres, plays)):
-        total[g] += p
-        songs[g].append(Song(index, p))
-    
-    
-    sorted_total = dict(sorted(total.items(), key = lambda x:x[1], reverse = True))
-    for ge in songs:
-        songs[ge].sort(key=lambda song:song.play, reverse = True)
-    
-    
-    answer = []
-    
-    for key in sorted_total.keys():
-        for s in songs[key][:2]:
-            answer.append(s.num)
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        playcount_dict[g]+= p
+        songs[g].append(Song(i, p))
         
-    return answer
+    # songs plays 많은 순으로 정렬
+    for _, v in songs.items():
+        v.sort(key = lambda s : -s.play)
+
+    # 총 재생 수 많은 순으로 정렬
+    sorted_playcount_dict = sorted(playcount_dict.items(), key = lambda x : -x[1])
+
+    for g, _ in sorted_playcount_dict:
+        songses = songs.get(g)
+        result.append(songses[0].number)
+        if len(songses) > 1:
+            result.append(songses[1].number)
+        
+    
+    return result
