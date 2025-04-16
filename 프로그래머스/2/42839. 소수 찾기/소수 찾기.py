6a427c7ca08f7c5ff243519a2af6033becc000d7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-16 18:16:51 +0900

    [level 2] Title: 소수 찾기, Time: 6.99 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,33 +1,35 @@
-from itertools import permutations
-import math
-
-def isPrim(num):
-    if num == 0 or num == 1:
+def isPrime(num):
+    if num < 2:
         return False
-    for i in range(2, int(math.sqrt(num))+1):
-        if num % i ==0:
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
             return False
     return True
 
 def solution(numbers):
-    answer = 0
-    # 만들 수 있는 숫자의 순열 만들기
-    perm = []
-    for i in range(1, len(numbers)+1):
-        perm += permutations(numbers, i)
-    #print(perm)
-    
-    # 중복 제거
-    perm_set = set()
-    for p in perm:
-        perm_set.add(int(''.join(p)))
-        #perm_set.add(''.join(p))
-    #print(perm_set)
-    
-    # 소수인지 판단하기
-    for ps in perm_set:
-        if isPrim(ps):
-            #print(ps)
-            answer+=1
-    
-    return answer
+    n = len(numbers)
+    sequence = [''] * n
+    visited = [False] * n
+    count = 0
+    num_set = set()
+
+    def backtrack(depth):
+        nonlocal count
+        if depth > 0:
+            num = int(''.join(sequence[:depth]))  # slicing 추가로 자릿수 제어
+            if num not in num_set:
+                num_set.add(num)
+                if isPrime(num):
+                    count += 1
+
+        last_used = ''
+        for i in range(n):
+            if not visited[i] and last_used != numbers[i]:
+                visited[i] = True
+                sequence[depth] = numbers[i]
+                last_used = numbers[i]
+                backtrack(depth + 1)
+                visited[i] = False
+
+    backtrack(0)
+    return count
