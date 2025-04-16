50be257c117b675e94a2d59200d5954e73d9c14d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-16 16:30:50 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,23 +1,8 @@
 def solution(array, commands):
-    answer = []
+    result = []
     for i, j, k in commands:
-        arr = array[i-1:j]
-        arr.sort()
-        answer.append(arr[k-1])
-        
-    return answer
-
-
-
-# 채점을 시작합니다.
-# 정확성  테스트
-# 테스트 1 〉	통과 (0.00ms, 10.3MB)
-# 테스트 2 〉	통과 (0.00ms, 10.1MB)
-# 테스트 3 〉	통과 (0.01ms, 10.2MB)
-# 테스트 4 〉	통과 (0.00ms, 10.2MB)
-# 테스트 5 〉	통과 (0.01ms, 10.1MB)
-# 테스트 6 〉	통과 (0.00ms, 10.2MB)
-# 테스트 7 〉	통과 (0.01ms, 10.3MB)
-# 채점 결과
-# 정확성: 100.0
-# 합계: 100.0 / 100.0
+        sliced_array = array[i-1 : j]
+        sliced_array.sort()
+        result.append(sliced_array[k-1])
+    
+    return result
