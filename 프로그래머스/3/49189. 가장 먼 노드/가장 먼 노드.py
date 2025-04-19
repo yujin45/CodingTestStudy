a695e02465bac0e32d13107e6a31377d73de7ef9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-19 20:36:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 40.64 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,28 +1,30 @@
 from collections import deque
 
-def bfs(graph, v, distance):
-    queue = deque()
-    queue.append(v)
-    
+def bfs(graph, visited, distance):
+    queue = deque([1])
+    visited[1] = True
+
     while queue:
         current = queue.popleft()
         for neighbor in graph[current]:
-            if distance[neighbor] == -1: # 아직 방문 안한 노드
-                distance[neighbor] = distance[current] + 1 # 현재에서 +1
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                distance[neighbor] = distance[current] + 1
                 queue.append(neighbor)
-
                 
+    max_dist = max(distance)
+    
+    return distance.count(max_dist)
+
 def solution(n, edge):
-    answer = 0
     graph = [[] for _ in range(n+1)]
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
     
-    distance = [-1] * (n+1)
-    distance[1] = 0 # 1번 노드 거리는 0
-    
-    bfs(graph, 1, distance)
-    
-    max_dist = max(distance) 
-    return distance.count(max_dist)
+    visited = [False] * (n+1)
+    distance = [0] * (n+1)
+        
+    answer = bfs(graph, visited, distance)
+
+    return answer
