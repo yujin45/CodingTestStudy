a3d65341931347f0f9c254c184bdb09d8eedbd36
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-23 21:57:25 +0900

    [Silver V] Title: 막대기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,18 +1,6 @@
-# 백준 막대기
-mak = 64
-x = int(input())
-maks=[64]
+import sys
 
-while sum(maks)> x:
-
-    maks.sort()
-    maks[0] = maks[0]//2 # 가지고 있는 막대 중 길이가 가장 짧은 것을 절반으로 자른다.
-    if sum(maks)>=x:
-        #print("남아있는 것", len(maks))
-        #print(maks, sum(maks))
-        pass
-    else:
-        maks.append(maks[0])
-
-
-print(len(maks))
+input = sys.stdin.readline
+#
+X = int(input())
+print(bin(X).count('1'))
