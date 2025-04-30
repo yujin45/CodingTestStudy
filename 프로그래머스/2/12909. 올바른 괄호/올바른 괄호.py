87d943ec963d09215a8545f23e925e30f554a724
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-04-30 17:43:25 +0900

    [level 2] Title: 올바른 괄호, Time: 7.45 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,15 @@
 def solution(s):
+    if s[0] == ')':
+        return False
+    
     count = 0
-    for ps in s:
-        if ps == '(':
-            count+=1
+    for ch in s:
+        if ch == '(':
+            count += 1
         else:
-            count-=1
+            count -= 1
         
         if count < 0:
-            return False # 닫는 괄호가 먼저 오면
-        
-    if count != 0:
-        return False # 다 돌고도 짝이 안 맞으면
+            return False
     
-    return True
+    return True if count == 0 else False
