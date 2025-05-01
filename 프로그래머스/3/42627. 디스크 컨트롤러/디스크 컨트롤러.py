a5f392cc57ebdc0a44641c2c29524f56712ec950
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-01 11:12:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.76 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -4,35 +4,33 @@ from collections import deque
 
 @dataclass
 class Task:
-    number : int
-    request_time : int
+    num : int
+    req_time : int
     time : int
-
+    
 def solution(jobs):
     ready_queue = []
     
-    t_time = 0 # 반환시간 : 작업 종료 - 요청
+    t_time = 0 # 반환 시간
     n = len(jobs)
     current_time = 0
     done = 0
     
-    job_queue = deque(
-        sorted([Task(num, rt, t) for num, (rt, t) in enumerate(jobs)],
-              key = lambda x: x.request_time
-              )
-    )
+    # 요청 시간 기준 job 정렬
+    job_queue = deque(sorted([Task(num, req_time, time) for num, (req_time, time) in enumerate(jobs)], key = lambda x : x.req_time))
+    
     while done < n:
-        while job_queue and current_time >= job_queue[0].request_time:
+        while job_queue and job_queue[0].req_time <= current_time:
             task = job_queue.popleft()
-            heapq.heappush(ready_queue, (task.time, task.request_time, task.number, task))
+            heapq.heappush(ready_queue, (task.time, task.req_time, task.num, task))
         
         if ready_queue:
             _, _, _, ptask = heapq.heappop(ready_queue)
             current_time += ptask.time
-            t_time += (current_time - ptask.request_time)
-            done +=1
+            t_time += (current_time - ptask.req_time)
+            done += 1
         else:
-            # 대기 중인 작업이 없을 때는 시간을 이동시켜줘야 함!
-            current_time = job_queue[0].request_time
+            # 대기중인 작업 없을 때는 시간을 이동시켜줘야 함
+            current_time = job_queue[0].req_time
             
     return t_time // done
