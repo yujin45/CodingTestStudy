844fd4e0dccae4debefc68c00d0e8a0e952b4940
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 13:45:48 +0900

    [level 1] Title: 최소직사각형, Time: 5.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -2,6 +2,7 @@ def solution(sizes):
     w = 0
     h = 0
     for a, b in sizes:
-        w = max(w, max(a, b))
-        h = max(h, min(a, b))
+        w = max(w, min(a, b))
+        h = max(h, max(a, b))
+    
     return w*h
