8534945a5df16cea6dfcbd6ca1788e26f6279376
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 14:35:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.13 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,23 +1,24 @@
-def dfs(graph, v, visited, sub):
+def dfs(graph, v, visited, sub_tree):
+    sub_tree[v] = 1
     visited[v] = True
-    sub[v] = 1
+    
     for neighbor in graph[v]:
         if not visited[neighbor]:
-            dfs(graph, neighbor, visited, sub)
-            sub[v] += sub[neighbor]
+            dfs(graph, neighbor, visited, sub_tree)
+            sub_tree[v] += sub_tree[neighbor]
+
 
 def solution(n, wires):
-    
-    graph = [[]*(n+1) for _ in range(n+1)]
-    visited = [False] * (n+1)
+    graph = [[] for _ in range(n+1)]
     for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
     
-    sub = [0] * (n+1)
-    dfs(graph, 1, visited, sub)
+    sub_tree = [0] * (n+1)
+    visited = [False] * (n+1)
+    dfs(graph, 1, visited, sub_tree)
     
     for i in range(1, n+1):
-        sub[i] = abs(sub[i] - (n - sub[i]))
+        sub_tree[i] = abs(sub_tree[i] - (n - sub_tree[i]))
     
-    return min(sub[1:])
+    return min(sub_tree[1:])
