3585a7162015845bdb9d37fbd5f324d6bc9125ab
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 15:07:17 +0900

    [level 2] Title: 타겟 넘버, Time: 437.32 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,13 @@
-def dfs(numbers, target, index, result):
+def dfs(numbers, index, next_num, target):
     count = 0
-    if index == len(numbers)-1:
-        if result == target:
+    if len(numbers) == index:
+        if target == next_num:
             return 1
         return 0
-        
-    count += dfs(numbers, target, index + 1, result +numbers[index+1])
-    count += dfs(numbers, target, index+1, result - numbers[index+1])
+    count += dfs(numbers, index+1, next_num + numbers[index], target)
+    count += dfs(numbers, index+1, next_num - numbers[index], target)
     return count
-
-def solution(numbers, target):
-    answer =dfs(numbers, target, -1, 0)
     
-    return answer
+    
+def solution(numbers, target):
+    return dfs(numbers, 0, 0, target)
