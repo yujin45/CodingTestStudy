4716d20c70ef81bb1a25977ad8bef81e42b4ec7f
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 15:25:28 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,25 +1,26 @@
 from collections import deque
 
-def diff_one(word1, word2):
-    return sum(a != b for a, b in zip(word1, word2)) == 1
-
-def bfs(begin, words, target):
-    queue = deque([(begin, 0)])  # (현재 단어, 변환 횟수)
-    visited = set()
-    
-    while queue:
-        current, steps = queue.popleft()
-        if current == target:
-            return steps
-        
-        for word in words:
-            if word not in visited and diff_one(current, word):
-                visited.add(word)
-                queue.append((word, steps + 1))
-    
-    return 0  # 변환 불가
+def isDiffOne(current, word):
+    count = 0
+    for c, w in zip(current, word):
+        if c != w:
+            count += 1
+        if count > 1:
+            return False
+    return True if count == 1 else False
 
 def solution(begin, target, words):
-    if target not in words:
-        return 0
-    return bfs(begin, words, target)
+    queue = deque([(begin, 0)])
+    visited = set() # 방문한 단어 체크용
+    
+    while queue:
+        current, current_count = queue.popleft()
+        if current == target:
+            return current_count
+        for word in words:
+            if word not in visited:
+                if isDiffOne(current, word):
+                    visited.add(word)
+                    queue.append((word, current_count + 1))
+    
+    return 0
