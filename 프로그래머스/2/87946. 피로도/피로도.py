7ad4bc13a33829b69ccb592ac510d5b5e6d41c75
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 14:27:34 +0900

    [level 2] Title: 피로도, Time: 38.59 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,23 +1,20 @@
 from itertools import permutations
 
 def solution(k, dungeons):
-    
     n = len(dungeons)
+    
     perm = list(permutations(dungeons, n))
-    
-    
-    max_clear = 0
-    for case in perm:
-        now_k = k
-        clear_count = 0
-        for start, end in case:
-            if start <= now_k:
-                now_k -= end
-                clear_count += 1
-            else:
-                # 진입 불가
-                break
-        max_clear = max(max_clear, clear_count)
-            
 
-    return max_clear
+    max_value = 0
+    for p in perm:
+        count = 0
+        current_k = k
+        for need, d in p:
+            if current_k < need:
+                break
+            current_k -= d
+            count+=1
+        max_value = max(max_value, count)
+        
+    
+    return max_value
