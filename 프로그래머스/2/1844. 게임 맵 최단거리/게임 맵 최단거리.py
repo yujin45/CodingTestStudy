b30a41cdcdf60188aa9c1b6e9bb071e5d0a8696c
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 15:19:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.99 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,25 @@
 from collections import deque
 
-def bfs(graph, x, y):
-    queue = deque([(x, y)])
+def solution(maps):
+    n = len(maps)
+    m = len(maps[0])
+    
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
     
+    queue = deque([(0, 0)])
+    
     while queue:
-        (cx, cy) = queue.popleft()
+        cx, cy = queue.popleft()
+        if (cx, cy) == (n-1, m-1):
+            return maps[cx][cy]
         
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
-            if 0<= nx <len(graph) and 0<= ny <len(graph[0]):
-                if graph[nx][ny]!=0 and graph[nx][ny] == 1:
-                    graph[nx][ny] += graph[cx][cy]
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] += maps[cx][cy]
                     queue.append((nx, ny))
             
-    
-def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    # 벽으로 막힌 경우 바로 리턴
-    if maps[n-2][m-1] == 0 and maps[n-1][m-2] == 0:
-        return -1
-    
-    bfs(maps, 0, 0)
-    return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
+    return -1
