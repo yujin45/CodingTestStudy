ca8831f5ed50805eeab56bba85b940468c982f41
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 14:40:38 +0900

    [level 2] Title: 모음 사전, Time: 1.70 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,9 @@
 from itertools import product
 
 def solution(word):
-    # 방법 1: AEOIU를 perm 순열 -> set -> sort , 단어 찾기는 index로 찾기
-    # 방법 2: backtrack으로 순열?
-    
-    # 방법 1로 일단 진행해보기
-    alpha_dict = []
+    perm = []
     for i in range(1, 6):
-        for p in product('AEIOU', repeat=i):
-            alpha_dict.append(''.join(p))
-    alpha_dict.sort()
-    
-    return alpha_dict.index(word) + 1
+        perm += list(''.join(p) for p in product('AEIOU', repeat = i))
+    perm.sort()
+
+    return perm.index(word) + 1
