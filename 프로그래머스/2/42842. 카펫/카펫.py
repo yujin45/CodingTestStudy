19055c389f3938f5436b54c48680f0876630a1f3
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 14:22:55 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,11 @@
+
 def solution(brown, yellow):
+    size = brown + yellow
+    length = (brown + 4) // 2
+    for i in range(1, size+1):
+        if i * (length - i) == size:
+            a = i
+            b = size // a
+            return [max(a, b), min(a, b)]
     answer = []
-    total_lines = brown + yellow
-    
-    for h in range(1, int(total_lines**0.5) + 1):
-        if total_lines % h == 0:
-            # h로 나눠지면 w를 확인
-            w = total_lines // h
-            if (w-2) * (h-2) == yellow:
-                answer = [w, h]
     return answer
