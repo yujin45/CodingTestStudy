3222d9c7973defac4591261c0a215b9cf5ce308a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 14:07:43 +0900

    [level 2] Title: 소수 찾기, Time: 5.78 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,35 +1,24 @@
+from itertools import permutations
+
 def isPrime(num):
-    if num < 2:
+    if num <= 1:
         return False
-    for d in range(2, int(num**0.5) + 1):
-        if num % d == 0:
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
             return False
+
     return True
-
+    
+    
 def solution(numbers):
-    n = len(numbers)
-    sequence = [''] * n
-    visited = [False] * n
+    perm = []
+    for i in range(1, len(numbers) + 1):
+        perm.extend(set(int(''.join(p)) for p in (permutations(numbers, i))))
+
+    perm = set(perm)
     count = 0
-    num_set = set()
-
-    def backtrack(depth):
-        nonlocal count
-        if depth > 0:
-            num = int(''.join(sequence[:depth]))  # slicing 추가로 자릿수 제어
-            if num not in num_set:
-                num_set.add(num)
-                if isPrime(num):
-                    count += 1
-
-        last_used = ''
-        for i in range(n):
-            if not visited[i] and last_used != numbers[i]:
-                visited[i] = True
-                sequence[depth] = numbers[i]
-                last_used = numbers[i]
-                backtrack(depth + 1)
-                visited[i] = False
-
-    backtrack(0)
+    for p in perm:
+        if isPrime(p):
+            count+=1
+    
     return count
