59e733d8d86c81f3e7a40817a713e9a378d1411a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-05-02 19:54:04 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 63.7 MB, 시간: 9.40 ms
+메모리: 9.14 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 23:53:40
+2025년 05월 02일 19:54:02
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,32 +1,19 @@
 from collections import defaultdict
 
 def solution(tickets):
-    # 그래프 생성 및 도착지를 내림차순으로 정렬
-    routes = defaultdict(list)
-    #print(routes)
-    for start, end in tickets:
-        routes[start].append(end)
-    for start in routes:
-        routes[start].sort(reverse=True)
-    #print(routes)
-
-    # DFS 탐색
-    stack = ["ICN"]  # 시작 공항
-    path = []  # 방문한 경로를 저장할 리스트
-
-    while stack:
-        current = stack[-1]
-        #print("current : "+ (current) +" / stack: "+ str(stack))
-        # 현재 공항에서 더 갈 곳이 없으면 경로에 추가
-        if not routes[current]:
-            #print("없다 "+ current)
-            path.append(stack.pop())
-        else:
-            # 더 갈 곳이 있다면 스택에 추가하고 경로에서 제거
-            stack.append(routes[current].pop())
-        # print("stack : "+str(stack))
-        # print("path : "+str(path))
-        # print("route : "+str(routes))
-
-    # 역순으로 경로를 반환
-    return path[::-1]
+    graph = defaultdict(list)
+    
+    for a, b in sorted(tickets, reverse = True):
+        graph[a].append(b)
+    
+    route = []
+    
+    def dfs(curr):
+        while graph[curr]:
+            # 경로 있는 경우
+            next_dest = graph[curr].pop() # 뒤에서부터 
+            dfs(next_dest)
+        route.append(curr) # 막다른 지점부터 경로에 추가됨
+    
+    dfs("ICN")
+    return route[::-1] # 역순으로 완성되므로 뒤집기
