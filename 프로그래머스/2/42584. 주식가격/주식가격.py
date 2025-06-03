c2c84999c45dc0f18bf19272bb1e0e858d06750c
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-03 16:43:13 +0900

    [level 2] Title: 주식가격, Time: 32.09 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,18 @@
 def solution(prices):
     n = len(prices)
-    answer = [0] * n
-    stack = []  # index를 저장
-
+    answer = [0] * n 
+    stack = []
+    
     for i in range(n):
-        # 가격이 떨어지는 지점을 찾으면 그만큼 시간 계산
+        # 가격이 떨어지는 시점 찾으면 그만큼 시간 계산
         while stack and prices[i] < prices[stack[-1]]:
+            # 떨어지는 시점
             j = stack.pop()
-            answer[j] = i - j
+            answer[j] = i - j 
         stack.append(i)
     
-    # 남은 인덱스는 끝까지 안 떨어진 것이므로
     while stack:
         j = stack.pop()
         answer[j] = n - 1 - j
-
+        
     return answer
