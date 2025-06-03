3850cae9bd6f64cc4e104c2ac6c491c50e4a6540
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-03 16:13:56 +0900

    [level 2] Title: 올바른 괄호, Time: 7.29 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,15 @@
 def solution(s):
     if s[0] == ')':
         return False
-    
-    count = 0
-    for ch in s:
-        if ch == '(':
-            count += 1
-        else:
-            count -= 1
-        
-        if count < 0:
-            return False
-    
-    return True if count == 0 else False
+    elif s[-1] == '(':
+        return False
+    else:
+        count = 0
+        for ps in s:
+            if ps == '(':
+                count+=1
+            elif ps == ')':
+                count -=1
+            if count < 0:
+                return False
+        return True if count == 0 else False
