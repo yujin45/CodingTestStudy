107327c58f3e152e6b20364c6eb0f3fb3fb7df08
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-03 20:54:22 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,19 +1,21 @@
-from collections import defaultdict
-
 def solution(tickets):
-    graph = defaultdict(list)
-    
-    for a, b in sorted(tickets, reverse = True):
-        graph[a].append(b)
-    
+    tickets.sort() # 사전 순 정렬
+    visited = [False] * len(tickets)
     route = []
     
-    def dfs(curr):
-        while graph[curr]:
-            # 경로 있는 경우
-            next_dest = graph[curr].pop() # 뒤에서부터 
-            dfs(next_dest)
-        route.append(curr) # 막다른 지점부터 경로에 추가됨
+    def dfs(curr, depth):
+        route.append(curr)
+        if depth == len(tickets): # 모든 티켓 사용시
+            return True
+        
+        for i, (a, b) in enumerate(tickets):
+            if not visited[i] and a == curr:
+                visited[i] = True
+                if dfs(b, depth+1):
+                    return True # 모든 티켓 사용 다 되면 바로 리턴
+                visited[i] = False # 백트래킹
+        route.pop()
+        return False
     
-    dfs("ICN")
-    return route[::-1] # 역순으로 완성되므로 뒤집기
+    dfs('ICN', 0)
+    return route
