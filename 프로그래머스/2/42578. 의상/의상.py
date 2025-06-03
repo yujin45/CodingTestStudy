7276612c68259553b226e7ea30ce84048fe94f89
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-03 15:28:19 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,11 +2,13 @@ from collections import defaultdict
 
 def solution(clothes):
     clothes_dict = defaultdict(int)
+    
     for _, kind in clothes:
         clothes_dict[kind] += 1
-
-    result = 1
-    for _, value in clothes_dict.items():
-        result *= (value + 1)
     
-    return result - 1
+    ret = 1
+    
+    for count in clothes_dict.values():
+        ret *= (count + 1)
+    
+    return ret - 1
