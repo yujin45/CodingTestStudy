57e0a291425b55c1d57df9c40b363e1d84075966
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-25 16:44:13 +0900

    [level 2] Title: H-Index, Time: 0.82 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,24 +1,16 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        // citations 배열을 내림차순으로 정렬
-        List<Integer> list = Arrays.stream(citations)
-                                   .boxed() // int를 Integer로 변환
-                                   .sorted(Collections.reverseOrder())
-                                   .collect(Collectors.toList());
+        Arrays.sort(citations);
         
-        // H-Index를 계산
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i) < (i + 1)){
-                return i;
+        int n = citations.length;
+        for(int i = 0; i < n ; i++){
+            int h = n - i; // 현재 인덱스보다 뒤에 있는 논문 수 
+            if(citations[i] >= h){
+                return h;
             }
         }
-        
-        // 모든 논문이 H-Index 조건을 만족하면, 논문의 수를 반환
-        return list.size();
+        return 0;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.27 ms
+메모리: 80 MB, 시간: 0.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 12:02:08
+2025년 06월 25일 16:44:10
 
 ### 문제 설명
 
