726753d4784e254e62bae55402b4cbaf128ffac8
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-25 15:19:52 +0900

    [level 2] Title: 더 맵게, Time: 1507.36 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.util.PriorityQueue;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s :scoville){
+        
+        for(int s : scoville){
             pq.add(s);
         }
+        
         int count = 0;
         while(pq.size() > 1 && pq.peek() < K){
             int first = pq.poll();
             int second = pq.poll();
-            int newScoville = first + second * 2;
+            int newScoville = first + (second * 2);
             pq.add(newScoville);
             count++;
         }
