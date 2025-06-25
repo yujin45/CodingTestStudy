2ab858b0f64c30ec357916a6b39cbc761974a081
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-25 14:59:12 +0900

    [level 2] Title: 프로세스, Time: 2.16 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 1.68 ms
+메모리: 75.2 MB, 시간: 2.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 14:27:12
+2025년 06월 25일 14:59:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,36 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.PriorityQueue;
-import java.util.Collections;
+import java.util.*;
 
+class Task{
+    int location;
+    int priority;
+    
+    public Task(int location, int priority){
+        this.location = location;
+        this.priority = priority;
+    }
+}
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> readyQueue = new LinkedList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        ArrayDeque<Task> readyQueue = new ArrayDeque<>();
         
         for(int i = 0; i < priorities.length; i++){
-            readyQueue.add(new int[]{i, priorities[i]});
             pq.add(priorities[i]);
+            readyQueue.addLast(new Task(i, priorities[i]));
         }
         
         int count = 0;
-        while(!readyQueue.isEmpty()){
-            int[] current = readyQueue.poll();
-            if(current[1] == pq.peek()){
-                // 가장 큰 우선순위
+        while(!pq.isEmpty()){
+            Task task = readyQueue.removeFirst();
+            if(pq.peek() == task.priority){
                 pq.poll();
                 count++;
-                if(current[0] == location){
-                    return count;
-                }
+                if(task.location == location) return count;
             }else{
-                readyQueue.add(current);
+                readyQueue.addLast(task);
             }
         }
+        
         return count;
     }
 }
