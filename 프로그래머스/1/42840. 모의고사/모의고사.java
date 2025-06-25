5145f86b68df87af6e042c3ae533d962450292d4
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-25 20:43:38 +0900

    [level 1] Title: 모의고사, Time: 4.94 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        // 수포자들의 패턴
         int[][] pattern = {
-            {1, 2, 3, 4, 5}, 
+            {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
-            { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5},
         };
-        // 수포자들의 점수를 저장할 배열
-        int[] scores = new int[3];
         
-        //각 수포자의 패턴과 정답이 얼마나 일치하는지 확인
-        for(int i = 0; i <answers.length; i++){
-            for(int j = 0; j < pattern.length; j++){
-                if(answers[i] == pattern[j][i%pattern[j].length]){
-                    scores[j]++;
-                }        
+        int[] count = new int[3];
+        for(int i = 0; i < answers.length; i++){
+            for(int p = 0; p < pattern.length; p++){
+                if(pattern[p][i%pattern[p].length] == answers[i]){
+                    count[p]++;
+                }
             }
         }
-        // 가장 높은 점수 저장
-        int maxScore = Arrays.stream(scores).max().getAsInt();
-        // 가장 높은 점수를 가진 수포자들의 번호를 찾아서 리스트에 담기
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int i = 0; i<scores.length; i++){
-            if(scores[i]==maxScore){
+        
+        List<Integer> answer = new ArrayList<>();
+        int maxValue = Arrays.stream(count).max().getAsInt();
+        for(int i = 0; i < count.length; i++){
+            if(count[i] == maxValue){
                 answer.add(i+1);
             }
         }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
