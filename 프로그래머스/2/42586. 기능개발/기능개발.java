a64b513e9a6356440d6ecd7342fde456556b1ab9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-25 14:47:23 +0900

    [level 2] Title: 기능개발, Time: 8.23 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,22 +1,26 @@
 import java.util.ArrayDeque;
 
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Integer> answer = new ArrayDeque<>();
-        int cDays = -1;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        int currentDays = 0;
         int done = 0;
         for(int i = 0; i < progresses.length; i++){
-            int nowNeedDays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            if(nowNeedDays > cDays){
-                answer.addLast(done);
+            int needDays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            if(needDays > currentDays){
+                // 출하시키고 새로 넣어야 함
+                deque.addLast(done);
                 done = 1;
-                cDays = nowNeedDays;
+                currentDays = needDays;
             }else{
                 done++;
             }
         }
-        answer.removeFirst();
-        answer.addLast(done);
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        
+        // 남은 것 출하
+        deque.addLast(done);
+        deque.removeFirst();
+        return deque.stream().mapToInt(i -> i).toArray();
     }
 }
