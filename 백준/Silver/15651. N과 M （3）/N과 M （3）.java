63640a057c3751eec2ef42a840e424293e41f1b6
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 16:25:08 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67776 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	static boolean[] visited;
+	static int[] sequence;
+	static StringBuilder sb;
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+       visited = new boolean[n+1];
+       sequence = new int[m];
+       sb = new StringBuilder();
+       backtrack(0);
+       System.out.println(sb.toString());
+    }
+    
+    private static void backtrack(int depth) {
+    	if(depth == m) {
+    		for(int num : sequence) {
+    			sb.append(num).append(" ");
+    		}
+    		sb.deleteCharAt(sb.length()-1);
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for(int i = 1; i <= n; i++) {
+    			sequence[depth] = i;
+    			backtrack(depth+1);
+    	}
+    }
+    
+  
+    
+    
+}
