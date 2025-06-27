ea512ddca0f0613e79cbd916712a21dbcde162a4
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 16:35:31 +0900

    [Silver III] Title: N과 M (7), Time: 556 ms, Memory: 123204 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	static int[] numbers;
+	//static boolean[] visited;
+	static int[] sequence;
+	static StringBuilder sb;
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i <n; i++) {
+        	numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(numbers);
+        
+       //visited = new boolean[n];
+       sequence = new int[m];
+       sb = new StringBuilder();
+       backtrack(0);
+       System.out.println(sb.toString());
+    }
+    
+    private static void backtrack(int depth) {
+    	if(depth == m) {
+    		for(int num : sequence) {
+    			sb.append(num).append(" ");
+    		}
+    		sb.deleteCharAt(sb.length()-1);
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    			sequence[depth] = numbers[i];
+    			backtrack(depth+1);
+    	}
+    }
+    
+  
+    
+    
+}
