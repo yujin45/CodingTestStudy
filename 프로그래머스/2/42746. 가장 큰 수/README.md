3a05c997c8bb88740b23334580d39819d0565c88
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 22:13:30 +0900

    [level 2] Title: 가장 큰 수, Time: 160.06 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 218.92 ms
+메모리: 125 MB, 시간: 160.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 16:46:54
+2025년 06월 27일 22:13:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,19 +2,38 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] nums = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            nums[i] = String.valueOf(numbers[i]);
+        List<String> numbersList = new ArrayList<>();
+        for(int num : numbers){
+            numbersList.add(String.valueOf(num));
         }
-        
-        Arrays.sort(nums, (a, b) -> (b+a).compareTo(a+b));
-        
+        numbersList.sort((o1, o2) -> (o2+o1).compareTo(o1+o2));
         StringBuilder sb = new StringBuilder();
-        for(String num : nums){
+        for(String num : numbersList){
             sb.append(num);
         }
         
-        if(sb.charAt(0) == '0') return "0";
-        return sb.toString();
+        String answer = sb.toString();
+        return answer.startsWith("0") ? "0" : answer;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public String solution(int[] numbers) {
+//         String[] nums = new String[numbers.length];
+//         for(int i = 0; i < numbers.length; i++){
+//             nums[i] = String.valueOf(numbers[i]);
+//         }
+        
+//         Arrays.sort(nums, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+//         if(nums[0].equals("0")) return "0";
+        
+//         StringBuilder sb = new StringBuilder();
+//         for(String num : nums){
+//             sb.append(num);
+//         }
+//         return sb.toString();
+//     }
+// }
