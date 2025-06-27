2df7c8d22b910226bb0d652141ef84b1729c17b5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 15:29:50 +0900

    [level 5] Title: 방의 개수, Time: 317.94 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.java
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 class Solution {
+    
     static class Point{
         int x, y;
         
-        Point(int x, int y){
+        public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
         
-        @Override
+        @Override 
         public boolean equals(Object obj){
             if(!(obj instanceof Point)) return false;
             Point p = (Point) obj;
@@ -19,15 +21,10 @@ class Solution {
         public int hashCode(){
             return Objects.hash(x, y);
         }
-        
-        @Override
-        public String toString(){
-            return "(" + x + ", " + y + ")";
-        }
     }
     
+    
     public int solution(int[] arrows) {
-        // 행열 말고 직교 좌표계.
         int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
         int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
         
@@ -38,21 +35,17 @@ class Solution {
         visited.add(current);
         
         int rooms = 0;
-        
         for(int dir : arrows){
-            for(int step = 0; step < 2; step++){ // 교차점 만들기 위해 2번 이동
+            for(int step = 0; step < 2; step++){ // 선 겹침 위해서 2회 진행
                 Point next = new Point(current.x + dx[dir], current.y + dy[dir]);
                 
-                // 양방향 간선이 없다면 새 경로
-                graph.putIfAbsent(current, new HashSet<>());
+                graph.putIfAbsent(current, new HashSet<>()); // 없으면 생성
                 graph.putIfAbsent(next, new HashSet<>());
                 
                 if(visited.contains(next) && !graph.get(current).contains(next)){
-                    // next 방문했지만, current와 처음 연결이라면?
-                    rooms++; // 방이 새로 생김
+                    rooms++;
                 }
                 
-                // 양방향 간선 추가
                 graph.get(current).add(next);
                 graph.get(next).add(current);
                 
