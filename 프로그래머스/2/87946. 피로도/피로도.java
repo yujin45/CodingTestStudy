ba004a5f4bdb7e5543be6abf565e0e289e68a8e2
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 23:29:31 +0900

    [level 2] Title: 피로도, Time: 3.11 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -6,20 +6,49 @@ class Solution {
         visited = new boolean[dungeons.length];
         
         backtrack(0, k, dungeons);
+        
         return maxValue;
     }
     
     private void backtrack(int depth, int current, int[][] dungeons){
-        maxValue = Math.max(maxValue, depth);
+        if(depth <= dungeons.length){
+            maxValue = Math.max(maxValue, depth);
+        }
+        if(depth >= dungeons.length) return;
+        
         for(int i = 0; i < dungeons.length; i++){
-            // current가 다음 진행 가능한지 봐야 함
-            if(current >= dungeons[i][0]){
-                if(!visited[i]){
-                    visited[i] = true;
-                    backtrack(depth+1, current - dungeons[i][1], dungeons);
-                    visited[i] = false;
-                 }
+            if(!visited[i] && current >= dungeons[i][0]){
+                visited[i] = true;
+                backtrack(depth+1, current - dungeons[i][1], dungeons);
+                visited[i] = false;
             }
         }
     }
+    
 }
+
+// class Solution {
+//     int maxValue = 0;
+//     boolean[] visited;
+    
+//     public int solution(int k, int[][] dungeons) {
+//         visited = new boolean[dungeons.length];
+        
+//         backtrack(0, k, dungeons);
+//         return maxValue;
+//     }
+    
+//     private void backtrack(int depth, int current, int[][] dungeons){
+//         maxValue = Math.max(maxValue, depth);
+//         for(int i = 0; i < dungeons.length; i++){
+//             // current가 다음 진행 가능한지 봐야 함
+//             if(current >= dungeons[i][0]){
+//                 if(!visited[i]){
+//                     visited[i] = true;
+//                     backtrack(depth+1, current - dungeons[i][1], dungeons);
+//                     visited[i] = false;
+//                  }
+//             }
+//         }
+//     }
+// }
