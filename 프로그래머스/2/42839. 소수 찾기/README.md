dd62d502a7823c70051cbe6695a8446fcbc6f43a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-06-27 23:23:24 +0900

    [level 2] Title: 소수 찾기, Time: 11.89 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 24.71 ms
+메모리: 84.8 MB, 시간: 11.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 25일 20:53:56
+2025년 06월 27일 23:23:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,42 +1,87 @@
 import java.util.*;
 
 class Solution {
-    
     Set<Integer> numberSet = new HashSet<>();
     boolean[] visited;
     
     public int solution(String numbers) {
         visited = new boolean[numbers.length()];
-        backtrack("", numbers);
+        backtrack(new StringBuilder(), numbers);
+        //System.out.println(numberSet.toString());
+        int answer = 0;
         
-        int count = 0;
-        for(int num : numberSet){
-            if(isPrime(num)) count++;
+        for(int number : numberSet){
+            if(isPrime(number)) answer++;
         }
-        return count;
+        
+        return answer;
     }
     
-    // 백트래킹으로 순열 생성
-    private void backtrack(String current, String numbers){
-        if(!current.isEmpty()){
-            numberSet.add(Integer.parseInt(current));
+    private boolean isPrime(int n){
+        if(n < 2) return false;
+        for(int i = 2; i*i<= n; i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    
+    private void backtrack(StringBuilder sb, String numbers){
+        if(sb.length() > 0 && sb.length() <= numbers.length()){
+            // 길이 1 ~ numbers.length()까지
+            numberSet.add(Integer.parseInt(sb.toString()));
+            if(sb.length() >= numbers.length()) return;
         }
         
         for(int i = 0; i < numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                backtrack(current + numbers.charAt(i), numbers);
+                backtrack(sb.append(numbers.charAt(i)), numbers);
+                sb.deleteCharAt(sb.length() - 1);
                 visited[i] = false;
             }
         }
     }
-    
-    // 소수 판별
-    private boolean isPrime(int n){
-        if(n < 2) return false;
-        for(int i = 2; i*i <= n; i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
 }
+
+// import java.util.*;
+
+// class Solution {
+    
+//     Set<Integer> numberSet = new HashSet<>();
+//     boolean[] visited;
+    
+//     public int solution(String numbers) {
+//         visited = new boolean[numbers.length()];
+//         backtrack("", numbers);
+        
+//         int count = 0;
+//         for(int num : numberSet){
+//             if(isPrime(num)) count++;
+//         }
+//         return count;
+//     }
+    
+//     // 백트래킹으로 순열 생성
+//     private void backtrack(String current, String numbers){
+//         if(!current.isEmpty()){
+//             numberSet.add(Integer.parseInt(current));
+//         }
+        
+//         for(int i = 0; i < numbers.length(); i++){
+//             if(!visited[i]){
+//                 visited[i] = true;
+//                 backtrack(current + numbers.charAt(i), numbers);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
+    
+//     // 소수 판별
+//     private boolean isPrime(int n){
+//         if(n < 2) return false;
+//         for(int i = 2; i*i <= n; i++){
+//             if(n%i==0) return false;
+//         }
+//         return true;
+//     }
+// }
