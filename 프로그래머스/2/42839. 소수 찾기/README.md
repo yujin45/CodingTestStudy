90e6e8976776ad734e1223c242a5cb63875df1a9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-16 10:48:33 +0900

    [level 2] Title: 소수 찾기, Time: 19.88 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
 
 ### 성능 요약
 
-메모리: 84.8 MB, 시간: 11.89 ms
+메모리: 9.52 MB, 시간: 19.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 23:23:21
+2025년 07월 16일 10:48:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,31 @@
-from itertools import permutations
 
-def isPrime(num):
-    if num <= 1:
+def isPrime(number):
+    if number < 2:
         return False
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
             return False
-
     return True
-    
+
+
+
     
 def solution(numbers):
-    perm = []
-    for i in range(1, len(numbers) + 1):
-        perm.extend(set(int(''.join(p)) for p in (permutations(numbers, i))))
 
-    perm = set(perm)
-    count = 0
-    for p in perm:
-        if isPrime(p):
-            count+=1
+    visited = [False] * len(numbers)
+    num_set = set()
+    sequence = [""] * len(numbers)
+    def backtrack(depth):
+        if depth > 0:
+            num = int(''.join(sequence[:depth]))
+            if isPrime(num):
+                num_set.add(num)
+        for i in range(len(numbers)):
+            if not visited[i]:
+                visited[i] = True
+                sequence[depth] = numbers[i]
+                backtrack(depth+1)
+                visited[i] = False
     
-    return count
+    backtrack(0)
+    return len(num_set)
