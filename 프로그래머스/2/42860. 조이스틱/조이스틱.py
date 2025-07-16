75d9f4969d29d346e5ac32bcda74ce42cfdbf69d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-16 13:43:16 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,3 +1,4 @@
+# 다시 보기
 def solution(name):
     answer = 0
     n = len(name)
@@ -10,7 +11,9 @@ def solution(name):
         while next_idx < n and name[next_idx] == 'A':
             next_idx += 1
         
-        # 좌우 이동 최소 계산
+        # 좌우 이동 최소 계산, J A A B C가 있을 때 
+        # i = 0, next_idx = 3, n- next_idx = 2 => i + n - next_idx는 연속 A구간 제외 바꿔야 하는 곳
+        # 매 위치마다 직진하는 것이 min인지, 돌아오는 것n-next_idx가 min인지 체크해서 더하기
         min_move = min(min_move, i + n - next_idx + min(i, n - next_idx))
         
     answer += min_move
