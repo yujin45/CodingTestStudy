f17a80d5aa86f6429dac23738dc405f28edb481d
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-18 10:26:21 +0900

    [Silver III] Title: 두 수의 합, Time: 72 ms, Memory: 44956 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -9,21 +9,13 @@ arr = list(map(int, input().split()))
 x = int(input())
 
 ##
-arr.sort()
-left, right = 0, n - 1
+seen = set()
 count = 0
 
-while left < right:
-    temp = arr[left] + arr[right]
-    if temp == x:
-        # 쌍을 찾는 문제라 둘 다 이동
+for num in arr:
+    if x - num in seen:
         count += 1
-        left += 1
-        right -= 1
-    elif temp < x:
-        left += 1
-    else:
-        right -= 1
+    seen.add(num)
 
 print(count)
 
