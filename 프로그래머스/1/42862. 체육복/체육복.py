42589bfe4ee24a9e0d3ae548221e9ee677d834b4
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-21 10:06:31 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,23 +1,32 @@
 def solution(n, lost, reserve):
-    # 현재 학생 상태를 잃어버림 -1, 있음 0, 여분 있음 1로 표시
-    student = [0] * (n+1)
-    for i in range(len(lost)):
-        student[lost[i]] -= 1
-    for i in range(len(reserve)):
-        student[reserve[i]] += 1
+    arr = [0] * n
+    # 여분 있는 사람 체크
+    for r in reserve:
+        arr[r - 1] += 1
+    # 잃어버린 사람 체크
+    for l in lost:
+        arr[l - 1] -= 1
     
-    # 학생 1번~n번까지 그리디하게 -1일 때 앞뒤 보고 있는 애한테 빌리기
-    answer = 0
-    for i in range(1, n+1):
-        if student[i] == -1:
-            # 빌려야 할 때
-            if i-1 >= 1 and student[i-1] == 1:
-                student[i-1] -= 1 # 빌리기
-                student[i] += 1
-            elif i+1 <= n and student[i+1] == 1:
-                student[i+1] -= 1
-                student[i] += 1
+    # 자신의 앞 뒤를 확인하며 있는 사람 체크
+    for i in range(n):
+        if i == 0:
+            # 뒤만 확인
+            if arr[i] == -1 and arr[i+1] == 1:
+                arr[i] += 1
+                arr[i+1] -= 1
+            
+        elif i == n-1:
+            if arr[i] == -1 and arr[i-1] == 1:
+                arr[i] += 1
+                arr[i-1] -= 1
+            
+        else:
+            if arr[i] == -1 and arr[i-1] == 1:
+                arr[i] += 1
+                arr[i-1] -= 1
+            elif arr[i] == -1 and arr[i+1] == 1:
+                arr[i] += 1
+                arr[i+1] -= 1
+            
     
-    answer = sum(1 for i in range(1, n+1) if student[i] >= 0)
-    
-    return answer
+    return arr.count(0) + arr.count(1)
