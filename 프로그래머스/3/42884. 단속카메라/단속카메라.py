fe75302ef0b47210a22c417a47f0c57df80deac5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-21 11:22:58 +0900

    [level 3] Title: 단속카메라, Time: 0.98 ms, Memory: 9.69 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,18 +1,13 @@
 def solution(routes):
-    # 가장 빨리 나가는 차 기준으로 카메라 설치해야 최대한 많은 차 커버 가능
+    routes.sort(key = lambda x : x[1]) # 끝점 기준 정렬
+    # [ [0, 10], [2, 5], [6, 9] ] 이런 경우 
     
-    # 처음 차 진출점에 카메라 설치
-    # 다음 차량부터는 
-    ## 현재 위치가 그 차량 구간 안이면 pass
-    ## 아니면 새로운 카메라 설치
+    answer = 1
+    now_camera = routes[0][1]
     
-    routes.sort(key = lambda x : x[1]) # 진출 지점 기준 정렬
-    camera = -30001 # 카메라 초기 위치(최소값보다 작은 값)
-    count = 0
-    
-    for route in routes:
-        if camera < route[0]: # 현재 카메라보다 진입 지점이 왼쪽이면
-            camera = route[1] # 새 카메라 차량 끝 지점에 설치
-            count += 1
-    
-    return count
+    for start, end in routes:
+        if start > now_camera:
+            # 지금 카메라가 담지 못하는 곳이면
+            now_camera = end
+            answer += 1
+    return answer
