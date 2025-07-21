3021772e3c5ddca990d93c06f1c912bce0d11dc8
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-21 11:14:34 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,30 +1,25 @@
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    root_a = find(parent, a)
+    root_b = find(parent, b)
+    
+    if root_a != root_b:
+        parent[root_b] = root_a # root_a로 통일
+        return True
+    return False # 둘이 이미 같은 트리 내 있는 경우
+
 def solution(n, costs):
-    # 1. 각 노드는 자기 자신을 부모로 초기화
-    parent = [i for i in range(n)]
+    parent = [i for i in range(n)] # 부모는 자기 자신으로 초기화
     
-    # 2. find: x의 부모를 재귀로 찾아서 반환
-    def find(x):
-        if parent[x] != x:
-            parent[x] = find(parent[x])
-        return parent[x]
-    
-    # 3. union : 두 노드의 루트 노드가 다르면 합치고 true 반환, 같으면 false
-    def union(a, b):
-        root_a = find(a)
-        root_b = find(b)
-        if root_a != root_b:
-            parent[root_b] = root_a # 한쪽 루트를 다른 쪽 루트에 붙임
-            return True
-        return False
-    
-    # 4. 간선을 비용 기준으로 정렬
-    costs.sort(key = lambda x : x[2])
+    costs.sort(key = lambda x:x[2]) # 비용으로 정렬
     
     total = 0
-    # 5. 간선을 하나씩 보면서 MST 구성
     for a, b, cost in costs:
-        if union(a, b): # 연결 성공하면
-            total += cost # 비용 누적
+        if union(parent, a, b):
+            total += cost # 연결 가능하면 비용 누적
     
-    # 6. 최소 비용 반환
     return total
