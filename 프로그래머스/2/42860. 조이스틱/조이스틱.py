259ff2ad91b143069fffff4fc894c7efcce40e5a
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-21 10:46:48 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,20 +1,23 @@
-# 다시 보기
 def solution(name):
     answer = 0
     n = len(name)
-    min_move = n - 1
-
+    
+    # 각 자리 알파벳 맞추기 최소 이동
+    for c in name:
+        answer += min(ord(c) - ord('A'), ord('Z') - ord(c) + 1)
+    
+    # 좌우 이동 최소 구하기
+    move = n - 1  # 일단 끝까지 오른쪽으로만 간 경우
+    
     for i in range(n):
-        answer += min(ord(name[i]) - ord('A'), 26 - (ord(name[i]) - ord('A')))
-        
         next_idx = i + 1
+        # next_idx부터 연속된 A 찾기
         while next_idx < n and name[next_idx] == 'A':
             next_idx += 1
         
-        # 좌우 이동 최소 계산, J A A B C가 있을 때 
-        # i = 0, next_idx = 3, n- next_idx = 2 => i + n - next_idx는 연속 A구간 제외 바꿔야 하는 곳
-        # 매 위치마다 직진하는 것이 min인지, 돌아오는 것n-next_idx가 min인지 체크해서 더하기
-        min_move = min(min_move, i + n - next_idx + min(i, n - next_idx))
-        
-    answer += min_move
+        # 오른쪽 갔다가 왼쪽으로 돌아오기 vs 왼쪽으로 갔다가 오른쪽으로 돌아오기
+        #  i별로 다음 A 덩어리를 찾아 계산
+        move = min(move, i + i + n - next_idx, i + 2 * (n - next_idx))
+    
+    answer += move
     return answer
