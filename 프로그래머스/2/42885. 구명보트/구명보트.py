6b5260539fe1c979d17de8ffc2dac2bb3b8815e9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-21 11:07:05 +0900

    [level 2] Title: 구명보트, Time: 9.65 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,27 @@
-# 최대 2명씩 탈 수 밖에 없음
-# 가능한 최대 적게 구명보트 사용하기
 def solution(people, limit):
-    # 포인트 : 몸무게가 가장 무거운 사람과 가장 가벼운 사람을 짝지어 태우는 방식
-    # 정렬 후 투포인터
+    # 1명 이상 50000명 이하
+    if len(people) == 1:
+        return 1
+    
+    answer = 0
+    left, right = 0, len(people) - 1
+    
     people.sort()
-    i = 0 # 가장 가벼운 사람
-    j = len(people) - 1 # 가장 무거운 사람
-    boat = 0
+    while left <= right:
+        if left == right:
+            # 둘이 같아졌으면 하나로 보내기
+            answer += 1
+            break
+        if people[left] + people[right] <= limit:
+            # 탑승 가능
+            answer += 1
+            left += 1
+            right -= 1
+        else:
+            # 둘이 같이 탑승 못하면 무거운 것만 보내기
+            answer += 1
+            right -= 1
+        
     
-    while i <= j:
-        if people[i] + people[j] <= limit:
-            i += 1 # 같이 탔으니까 가벼운 쪽도 이동
-        j -= 1 # 무거운 쪽은 무조건 태움
-        boat += 1
     
-    return boat
+    return answer
