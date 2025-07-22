153e49102a93b498e372d8c4b5a5015da51187c9
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-22 10:34:28 +0900

    [level 5] Title: 방의 개수, Time: 536.60 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/README.md
+++ 프로그래머스/5/49190. 방의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 5] 방의 개수 - 49190 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=python3) 
 
 ### 성능 요약
 
-메모리: 183 MB, 시간: 317.94 ms
+메모리: 106 MB, 시간: 536.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 15:29:49
+2025년 07월 22일 10:34:26
 
 ### 문제 설명
 
--- 프로그래머스/5/49190. 방의 개수/방의 개수.py
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -1,36 +1,26 @@
 from collections import defaultdict
+# 방이 되는 기준 -> 이미 지나왔던 경로를 다시 지나갈 때 
+# 이때 a-> b 이후 다시 b -> a로 가는 경우는 a가 다시 겹치지만 방이 생성되지 않음
+# 그러므로 시작점과 도착점을 함께 봐야 함
+# 그리고 대각선 교차를 측정하기 위해서는 좌표값을 2배로 해야 가능
 
 def solution(arrows):
-    direction = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-    direction = [(x*2, y*2) for (x, y) in direction]
-    
-    # 방문한 정점과 간선 기록
-    visited_nodes = set()
-    visited_edges = set()
-    
+    # 일반 좌표계 기준으로 방향 설정 0 ~ 7
+    direct = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+    visited = defaultdict(set)
     x, y = 0, 0
-    visited_nodes.add((x, y))
-    count = 0
+    room = 0
     
     for d in arrows:
-        dx, dy = direction[d]
-        
-        # 2칸 움직이므로 중간 지점 포함해 2번 이동
-        for _ in range(2):
-            nx, ny = x + dx // 2, y + dy // 2
+        for _ in range(2): # 2배 좌표계 확장 대각선 교차도 확인해야 해서
+            dx, dy = direct[d]
+            nx, ny = x + dx, y + dy
             
-            # 간선 양 방향으로 저장
-            edge = ((x, y), (nx, ny))
-            redge = ((nx, ny), (x, y))
+            if (nx, ny) in visited and (x, y) not in visited[(nx, ny)]:
+                room += 1 # 이전 nx, ny 방문한 적은 있지만 그게 x, y에서 온 것이 아니면 방 생성
             
-            # 새 정점인데, 간선도 처음이면 방 생성
-            if (nx, ny) in visited_nodes and edge not in visited_edges:
-                count += 1
-            
-            visited_nodes.add((nx, ny))
-            visited_edges.add(edge)
-            visited_edges.add(redge)
-            
+            visited[(x, y)].add((nx, ny))
+            visited[(nx, ny)].add((x, y))
             x, y = nx, ny
-                
-    return count
+    
+    return room
