c861d7b975da6b9d6142dbf5b65f77e78dfcb2c2
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-22 10:04:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 38.40 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=python3) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 30.52 ms
+메모리: 19.4 MB, 시간: 38.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 14:35:53
+2025년 07월 22일 10:04:04
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,25 +1,25 @@
 from collections import deque
 
 def solution(n, edge):
-    
-    graph = [[] for _ in range(n+1)] # 0번은 제외
+    graph = [[] for _ in range(n+1)] # 1부터 n까지
     
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
     
+    # 각각 1로부터 얼만큼 떨어져 있는지 확인 필요
+    visited = [False] * (n+1)
+    queue = deque([1]) # 1에서 시작
+    visited[1] = True
     distance = [0] * (n+1)
-    
-    queue = deque([1])
-    
     while queue:
-        current = queue.popleft()
-        for neighbor in graph[current]:
-            if distance[neighbor] == 0:
+        curr = queue.popleft()
+        for neighbor in graph[curr]:
+            if not visited[neighbor]:
                 queue.append(neighbor)
-                distance[neighbor] = distance[current] + 1
-                
-    distance = distance[2:]
-    max_val = max(distance)
-    
-    return distance.count(max_val)
+                visited[neighbor] = True
+                distance[neighbor] = distance[curr] + 1 # 부모보다 1 증가
+
+    max_value = max(distance)
+    answer = distance.count(max_value)
+    return answer
