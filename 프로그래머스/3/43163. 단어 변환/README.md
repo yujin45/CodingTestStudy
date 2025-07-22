6274baea8c8bd72497a459396ad1b12c686b40c7
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-22 11:39:32 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.40 ms
+메모리: 9.3 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 20:01:07
+2025년 07월 22일 11:39:30
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,31 @@
 from collections import deque
 
-def isDiffOne(current, w):
+def isDiffOne(curr, word):
     count = 0
-    for a, b in zip(current, w):
-        if a != b:
-            count+=1
+    for c, w in zip(curr, word):
+        if c != w:
+            count += 1
         if count > 1:
             return False
     return True if count == 1 else False
-    
+
 def solution(begin, target, words):
+    # 변환할 수 없는 경우 초기 종료
     if target not in words:
         return 0
     
     queue = deque([(begin, 0)])
-    word_set = set()
+    visited = set()
+    visited.add(begin)
     
     while queue:
-        current, depth = queue.popleft()
-        
-        if current == target:
+        curr, depth = queue.popleft()
+        if curr == target:
             return depth
-        
-        for w in words:
-            if w not in word_set and isDiffOne(current, w):
-                queue.append((w, depth+1))
-                word_set.add(w)
-        
+        for word in words:
+            if word not in visited and isDiffOne(curr, word):
+                # 아직 방문 전이고 word랑 1개만 다르면 
+                queue.append((word, depth+1))
+                visited.add(word)
+    
     return 0
