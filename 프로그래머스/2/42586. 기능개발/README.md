176990775663eff2b83c88567ace626c7ee8e7a5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-24 17:23:31 +0900

    [level 2] Title: 기능개발, Time: 3.84 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 9.19 MB, 시간: 0.02 ms
+메모리: 74.4 MB, 시간: 3.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 15일 10:24:06
+2025년 07월 24일 17:23:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,24 @@
 import java.util.ArrayDeque;
 
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
         int currentDays = 0;
         int done = 0;
+        
         for(int i = 0; i < progresses.length; i++){
             int needDays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
             if(needDays > currentDays){
-                // 출하시키고 새로 넣어야 함
                 deque.addLast(done);
-                done = 1;
+                done = 0;
                 currentDays = needDays;
-            }else{
-                done++;
             }
+            done++;
         }
         
-        // 남은 것 출하
         deque.addLast(done);
         deque.removeFirst();
-        return deque.stream().mapToInt(i -> i).toArray();
+        return deque.stream().mapToInt(i->i).toArray();
+        
     }
 }
