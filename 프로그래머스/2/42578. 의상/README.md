903773384c819b2dcf23ccedc22670ac677ecbb5
yujin45 <113690378+yujin45@users.noreply.github.com>
2025-07-24 16:36:24 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 3.07 ms
+메모리: 80.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 20:46:08
+2025년 07월 24일 16:36:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -9,23 +9,12 @@ class Solution {
             String kind = clo[1];
             map.put(kind, map.getOrDefault(kind, 0) + 1);
         }
-        // 엔트리로 사용
-        // int total = 1;
-        // for(Map.Entry<String, Integer> entry : map.entrySet()){
-        //     total *= (entry.getValue() + 1);
-        // }
-        // return total - 1;
         
-        // value만 뽑아서 사용
-        // int total = 1;
-        // for(int count : map.values()){ // key는 map.keySet()으로 받아야 함
-        //     total *= (count+1);
-        // }
-        // return total - 1;
+        int total = 1;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            total *= (entry.getValue() + 1);
+        }
         
-        int total = map.values().stream()
-                    .mapToInt(count -> count+1)
-                    .reduce(1, (a, b) -> a*b);
         return total - 1;
     }
 }
